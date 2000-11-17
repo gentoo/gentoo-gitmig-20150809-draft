7ba871dca4ac06bc40c1daf85d160496fd7120ae
Daniel Robbins <drobbins@gentoo.org>
2000-11-17 17:59:08 +0000

    little fix

--- sys-apps/portage/files/portage.py
+++ sys-apps/portage/files/portage.py
@@ -68,7 +68,7 @@ import fchksum,types
 
 categories=("app-admin", "app-arch", "app-cdr", "app-doc", "app-editors", "app-emulation", "app-games", "app-misc", 
 			"app-office", "app-shells", "app-text", "dev-db", "dev-java", "dev-lang", "dev-libs", "dev-perl", 
-			"dev-python", "dev-util", "gnome-apps", "gnome-base", "gnome-libs", 
+			"dev-python", "dev-ruby", "dev-util", "gnome-apps", "gnome-base", "gnome-libs", 
 			"gnome-office","kde-apps", "kde-base", "kde-libs", "media-gfx", "media-libs", "media-sound", "media-video", 
 			"net-analyzer", "net-dialup", "net-fs", "net-ftp", "net-irc", "net-libs", "net-mail", "net-misc", "net-nds", 
 			"net-print", "net-www", "packages", "sys-apps", "sys-devel", "sys-kernel", "sys-libs", "x11-base", "x11-libs", 
