dfa668c24b16bb629fd3f7bf49ac04650f1dea54
Daniel Robbins <drobbins@gentoo.org>
2001-01-31 19:28:50 +0000

    new kde-i18n category

--- sys-apps/portage/files/portage.py
+++ sys-apps/portage/files/portage.py
@@ -71,7 +71,7 @@ import shutil
 categories=("app-admin", "app-arch", "app-cdr", "app-doc", "app-editors", "app-emulation", "app-games", "app-misc", 
 			"app-office", "app-shells", "app-text", "dev-db", "dev-java", "dev-lang", "dev-libs", "dev-perl", 
 			"dev-python", "dev-ruby", "dev-util", "gnome-apps", "gnome-base", "gnome-libs", 
-			"gnome-office","kde-apps", "kde-base", "kde-libs", "media-gfx", "media-libs", "media-sound", "media-video", 
+			"gnome-office","kde-apps", "kde-base", "kde-libs", "kde-i18n", "media-gfx", "media-libs", "media-sound", "media-video", 
 			"net-analyzer", "net-dialup", "net-fs", "net-ftp", "net-irc", "net-libs", "net-mail", "net-misc", "net-nds", 
 			"net-print", "net-www", "packages", "sys-apps", "sys-devel", "sys-kernel", "sys-libs", "x11-base", "x11-libs", 
 			"x11-terms", "x11-wm","virtual")
