41f9f6bbeb2f3fa7236bbceea026107ed1236db4
Daniel Robbins <drobbins@gentoo.org>
2001-04-19 05:10:54 +0000

    empty directory remerging fix (they will not disappear anymore)

--- sys-apps/portage/files/1.5/pym/portage.py
+++ sys-apps/portage/files/1.5/pym/portage.py
@@ -1665,6 +1665,8 @@ class dblink:
 					os.chown(rootfile,mystat[4],mystat[5])
 					print "<<<",rootfile+"/"
 				else:
+					#update atime/mtime so that empty dirs don't disappear on remerge
+					os.utime(rootfile,mystat[-3:-1])
 					print "---",rootfile+"/"
 				#mtime doesn't mean much for directories -- we don't store it
 				outfile.write("dir "+relfile+"\n")
