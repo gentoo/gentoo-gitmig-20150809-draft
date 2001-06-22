ab11a46db30187b2afb7bf006e91a9b6a83bdb27
Adam Bellinson <thread@gentoo.org>
2001-06-22 20:43:59 +0000

    *** empty log message ***

--- app-doc/gentoo-web/files/wiki/functions.php
+++ app-doc/gentoo-web/files/wiki/functions.php
@@ -1,7 +1,9 @@
 <?php
 	session_start();
-	mysql_connect( 'localhost', '@@USER@@', '@@PASS@@' );
-	mysql_select_db( '@@DB@@' );
+#	mysql_connect( 'localhost', '@@USER@@', '@@PASS@@' );
+#	mysql_select_db( '@@DB@@' );
+	mysql_connect( 'localhost', 'thread', 'twinkie09' );
+	mysql_select_db( 'wiki' );
 ?>
 
 <?php function main_header ( $title = 'Unknown' ) {
