398f6e61543e2f0a890cc2886c33e59e59437b82
Adam Bellinson <thread@gentoo.org>
2001-06-22 05:13:25 +0000

    *** empty log message ***

---
+++ media-gfx/qiv/files/fix_prefix.pl
@@ -0,0 +1,21 @@
+
+# this will hopefully be replaced by a leaner
+# sed method later by someone with a clue.
+#
+open MAKEFILE, "${ARGV[0]}/Makefile";
+open TMP, ">${ARGV[0]}/tmp";
+
+while ( <MAKEFILE> ) {
+	last if $_ =~ /^PREFIX/;
+	print TMP $_;
+}
+
+print TMP "PREFIX = ${ARGV[1]}\n";
+
+print TMP while <MAKEFILE>;
+
+close MAKEFILE;
+close TMP;
+
+system "cp ${ARGV[0]}/tmp ${ARGV[0]}/Makefile";
+#unlink "${ARGV[0]}/tmp";
