b2d725b11bdc2859b9b327c82a75c462e49227d1
Adam Bellinson <thread@gentoo.org>
2001-06-22 21:59:25 +0000

    *** empty log message ***

--- app-doc/gentoo-web/files/bin/wiki.pl
+++ app-doc/gentoo-web/files/bin/wiki.pl
@@ -6,14 +6,13 @@ use DBI;
 # EDITME
 $sql_user = '##USER##';
 $sql_pass = '##PASS##';
-$sql_db = '##DB##';
 
 unless ( $ARGV[0] =~ /^add$/i || $ARGV[0] =~ /^remove$/i ) {
 	print STDERR "\nGentoo Linux dev-wiki admin tool!\n\n\tUsage: wiki.pl <add|remove>\n\n";
 	exit;
 }
 
-$dbh = DBI->connect( "DBI:mysql:$sql_db", $sql_user, $sql_pass ) or die "Can't connect: $DBI::err.\n";
+$dbh = DBI->connect( "DBI:mysql:##DB##", $sql_user, $sql_pass ) or die "Can't connect: $DBI::err.\n";
 
 if ( $ARGV[0] =~ /^add$/i ) {
 	print "****************\n";
@@ -29,11 +28,22 @@ if ( $ARGV[0] =~ /^add$/i ) {
 	chomp( $password = <STDIN> );
 	die "Empty password!\n" if $password eq '';
 
-	print "\nDid I get that right: ($username/$password)? ";
+	print "\nWould you like this user to be an admin (y/n)? ";
+	chomp( $admin = <STDIN> );
+
+	if ( $admin =~ /^y/i ) {
+		$admin = 1;
+		$admin_word = "yes";
+	} else {
+		$admin = 0;
+		$admin_word = "no";
+	}
+
+	print "\nDid I get that right: ($username/$password/admin: $admin_word)? ";
 	chomp( $check = <STDIN> );
 	die "Okay, try again then.\n" unless $check =~ /^y/i;
 
-	$sth = $dbh->prepare( "insert into ##DB##.users set username='$username',password='$password'" );
+	$sth = $dbh->prepare( "insert into ##DB##.users set username='$username',password='$password',admin=$admin" );
 	$sth->execute();
 	
 	print "\nUser $username added.\n";
@@ -48,7 +58,7 @@ if ( $ARGV[0] =~ /^add$/i ) {
 	chomp( $username = <STDIN> );
 	die "Empty username!\n" if $username eq '';
 
-	$sth = $dbh->prepare( "select uid from ${sql_db}.users where username='$username'" );
+	$sth = $dbh->prepare( "select uid from ##DB##.users where username='$username'" );
 	$sth->execute();
 	( $uid ) = $sth->fetchrow_array();
 	die "$username does not exist!\n" if !$uid;
@@ -59,11 +69,11 @@ if ( $ARGV[0] =~ /^add$/i ) {
 	chomp( $check = <STDIN> );
 	die "Okaythen. No changes made.\n" unless $check =~ /^y/i;
 
-	$sth = $dbh->prepare( "delete from ${sql_db}.users where uid=$uid" );
+	$sth = $dbh->prepare( "delete from ##DB##.users where uid=$uid" );
 	$sth->execute();
-	$sth = $dbh->prepare( "delete from ${sql_db}.todos where owner=$uid" );
+	$sth = $dbh->prepare( "delete from ##DB##.todos where owner=$uid" );
 	$sth->execute();
-	$sth = $dbh->prepare( "delete from ${sql_db}.followups where uid=$uid" );
+	$sth = $dbh->prepare( "delete from ##DB##.followups where uid=$uid" );
 	$sth->execute();
 
 	print "\nWow, sucks to be $username; he's gone.\n";
