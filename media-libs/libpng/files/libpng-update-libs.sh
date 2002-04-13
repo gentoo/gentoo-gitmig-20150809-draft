49a32bbcf010a4f2e4e6faee9720dbf0369d8259
D.M.D. Ljungmark <spider@gentoo.org>
2002-04-13 00:48:03 +0000

    split scanning script to bins and libs

---
+++ media-libs/libpng/files/libpng-update-libs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ ! -f /usr/sbin/qpkg ]; then
+	echo "qpkg not found, will emerge gentoolkit"
+	emerge gentoolkit
+fi
+rm -f /tmp/pngstuff.*
+echo "Scanning libraries. do not be alarmed of error messages"
+
+find /usr/lib -type f -perm +u+x | while read LIB; do
+	ldd "${LIB}" | grep "libpng.so.2"  && /usr/sbin/qpkg -nc -f "${LIB}" >>/tmp/pngstuff.libs
+done
+cat /tmp/pngstuff.libs |sort | uniq |  sed 's:\(.*/.*\)-[0-9]\+.*:\1:g' >/tmp/pngstuff.libs.rebuild
+echo "You will now need to rebuild the following packages"
+echo "------------"
+cat /tmp/pngstuff.libs.rebuild
+echo "------------"
+# cat /tmp/pngstuff.libs.rebuild | while read PACK; do emerge ${PACK}; done
+echo "--- Done with libraries ---"
+
+
+rm -f /tmp/pngstuff.*
+
