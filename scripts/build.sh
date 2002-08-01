55aa48c615f08cc702ca246a71a32015ef3ddd8c
Bart Verwilst <verwilst@gentoo.org>
2002-08-01 18:55:42 +0000

    updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-ver=${1:-1.0-gcc3}
+
+if [ "${1}" ]; then
+	ver=${1}
+else
+	ver=x86-2.0
+fi
+
 for x in `cat /usr/portage/profiles/default-${ver}/packages.build`
 do
 	grep -E "${x}(-[^[:space:]]*)?[[:space:]]*$" /usr/portage/profiles/default-${ver}/packages | grep -v '^#' | sed -e 's:^\*::' | cat 
