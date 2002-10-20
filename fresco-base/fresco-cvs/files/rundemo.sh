d2a77fe9f1bd18418cdd9f24474085853d14abf5
Tilman Klar <phoenix@gentoo.org>
2002-10-20 10:48:00 +0000

    Initial import of fresco-cvs

---
+++ fresco-base/fresco-cvs/files/rundemo.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+TMPFILE="/tmp/orbref"
+if([ -e ${TMPFILE} ]) then
+	echo "${TMPFILE} already exists. Delete it please."
+	exit
+fi
+
+pushd /opt/fresco &> /dev/null
+
+echo "Starting up the server..."
+bin/server -R ior &> ${TMPFILE} &
+sleep 2
+
+REF=`head -1 ${TMPFILE} | cut -d " " -f 3`
+rm ${TMPFILE}
+
+echo "Starting the demo..."
+bin/demo -ORBInitRef ${REF}
+
+popd &> /dev/null
