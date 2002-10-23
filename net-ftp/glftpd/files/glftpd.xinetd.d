26196ac205541d7077b80155347488a6f7244510
Mike Frysinger <vapier@gentoo.org>
2002-10-23 15:51:43 +0000

    Initial ebuild

---
+++ net-ftp/glftpd/files/glftpd.xinetd.d
@@ -0,0 +1,12 @@
+service glftpd
+{
+    disable         = no
+    flags           = REUSE NAMEINARGS
+    socket_type     = stream
+    protocol        = tcp
+    wait            = no
+    user            = root
+    server          = /usr/sbin/tcpd
+    server_args     = /opt/glftpd/bin/glftpd -l -i -o -r /opt/glftpd/glftpd.conf -s/opt/glftpd/bin/glstrings.bin 
+    only_from       = 0.0.0.0
+}
