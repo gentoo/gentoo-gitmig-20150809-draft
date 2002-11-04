731139b45a70ce967bbbb6789c1616aa7f1e6159
Matthew Kennedy <mkennedy@gentoo.org>
2002-11-04 16:26:39 +0000

    initial

---
+++ app-emacs/apache-mode/files/50apache-mode-gentoo.el
@@ -0,0 +1,10 @@
+
+;;; apache-mode site-lisp configuration
+
+(setq load-path (cons "@SITELISP@" load-path))
+
+(autoload 'apache-mode "apache-mode" "autoloaded" t)
+(add-to-list 'auto-mode-alist '("\\.htaccess$"   . apache-mode))
+(add-to-list 'auto-mode-alist '("httpd\\.conf$"  . apache-mode))
+(add-to-list 'auto-mode-alist '("srm\\.conf$"    . apache-mode))
+(add-to-list 'auto-mode-alist '("access\\.conf$" . apache-mode))
