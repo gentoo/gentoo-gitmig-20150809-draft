9ad0bb215df46ae19e65d49e79db62f81e443bf0
Martin Schlemmer <azarah@gentoo.org>
2002-12-26 03:29:41 +0000

    fix unneeded --login

--- x11-base/xfree/files/4.2.1-r2/startDM.sh
+++ x11-base/xfree/files/4.2.1-r2/startDM.sh
@@ -1,8 +1,8 @@
-#!/bin/bash --login
+#!/bin/bash
 # Copyright 1999-2002 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License, v2 or later
 # Author: Martin Schlemmer <azarah@gentoo.org>
-# $Header: /var/cvsroot/gentoo-x86/x11-base/xfree/files/4.2.1-r2/startDM.sh,v 1.2 2002/12/25 18:51:06 azarah Exp $
+# $Header: /var/cvsroot/gentoo-x86/x11-base/xfree/files/4.2.1-r2/startDM.sh,v 1.3 2002/12/26 03:29:41 azarah Exp $
 
 source /etc/init.d/functions.sh
 
