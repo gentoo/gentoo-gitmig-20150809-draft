f3634e1998bf0b9d235f8ecb4aac869a391dfef2
Jose Alberto Suarez Lopez <bass@gentoo.org>
2003-01-11 01:42:39 +0000

    new version next generation :)

---
+++ app-emulation/win4lin/files/registerme.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo 'Your license evaluation code is:'
+echo '    1w4e053x-n0aaf8-7tw9-587j-h47j-d6'
+echo
+echo 'Remember you need do this like root.'
+echo 'copy it and press a key'
+
+read
+sh /opt/win4lin/bin/ask_license.sh
