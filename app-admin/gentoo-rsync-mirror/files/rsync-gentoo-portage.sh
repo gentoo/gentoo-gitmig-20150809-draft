d899fdc9f48663101adfed5823e33caebe0591b5
Michael Imhof <tantive@gentoo.org>
2003-02-10 21:55:16 +0000

    New ebuild, makes the setup of a gentoo rsync mirror very easy.

---
+++ app-admin/gentoo-rsync-mirror/files/rsync-gentoo-portage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RSYNC="/usr/bin/rsync"
+OPTS="--quiet --recursive --links --perms --times --devices --compress --delete --timeout=600"
+#Uncomment the following line only if you have been granted access to rsync1.us.gentoo.org
+#SRC="rsync://rsync1.us.gentoo.org/gentoo-portage"
+#If you are waiting for access to our master mirror, select one of our mirrors to mirror from:
+SRC="rsync://rsync.gentoo.org/gentoo-portage"
+DST="/opt/gentoo-rsync/portage/"
+
+echo "Started update at" `date` >> $0.log 2>&1
+logger -t rsync "re-rsyncing the gentoo-portage tree"
+${RSYNC} ${OPTS} ${SRC} ${DST} >> $0.log 2>&1
+
+echo "End: "`date` >> $0.log 2>&1 
+
