6e1db95770d2e11ed2115b8fe33cf3209e7cfaaf
Mike Frysinger <vapier@gentoo.org>
2003-02-18 00:14:03 +0000

    initial ebuild #14124

---
+++ media-gfx/wings/files/wings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Wing3D run script
+
+ESDL_ROOT="/usr/lib/erlang/lib/esdl"
+WINGS_ROOT="/usr/lib/erlang/lib/wings"
+
+erl -pa $ESDL_ROOT/ebin $WINGS_ROOT/ebin -run wings_start start_halt
