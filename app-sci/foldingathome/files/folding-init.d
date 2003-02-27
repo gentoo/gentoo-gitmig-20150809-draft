c0bc73bb940ac4f4a2b0f230b14284f034fd9b2b
Daniel Ahlberg <aliz@gentoo.org>
2003-02-27 10:23:31 +0000

    Version bump and closing of #12950.

--- app-sci/foldingathome/files/folding-init.d
+++ app-sci/foldingathome/files/folding-init.d
@@ -1,10 +1,13 @@
 #!/sbin/runscript
+# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/app-sci/foldingathome/files/folding-init.d,v 1.2 2003/02/27 10:23:31 aliz Exp $
 
 start() {
 
 	ebegin "Starting Folding@home"
 	cd /opt/foldingathome
-	./foldingathome >&/dev/null&
+	nice -n 19 ./foldingathome >&/dev/null&
 	eend $?
 }
 
