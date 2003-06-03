5fc4693fadbf6dddea0ea47cddea1922272a280e
Jose Alberto Suarez Lopez <bass@gentoo.org>
2003-06-03 22:29:28 +0000

    New version

--- app-emulation/win4lin/files/registerme.sh
+++ app-emulation/win4lin/files/registerme.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 echo 'Your license evaluation code is:'
+echo '-> Win4Lin 4.x:'
 echo '    1w4e053x-n0aaf8-7tw9-587j-h47j-d6'
+echo '-> Win4Lin 5.x'
+echo '	  1w5e055x-n0ca34-xjhx-xx3c-4c3c-73'
 echo
-echo 'Remember you need do this like root.'
-echo 'copy it and press a key'
+echo 'Remember you need do this in root mode.'
+echo 'copy it and press a key to start the reg.'
 
 read
 sh /opt/win4lin/bin/ask_license.sh
