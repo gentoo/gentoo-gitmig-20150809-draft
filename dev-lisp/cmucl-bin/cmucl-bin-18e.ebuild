64a994f9717b229acdadc2b0c389f773c356a8af
Matthew Kennedy <mkennedy@gentoo.org>
2003-06-07 19:34:41 +0000

    minor version bump and port to common lisp controller infrastructure

---
+++ dev-lisp/cmucl-bin/cmucl-bin-18e.ebuild
@@ -0,0 +1,74 @@
+# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/dev-lisp/cmucl-bin/cmucl-bin-18e.ebuild,v 1.1 2003/06/07 19:34:37 mkennedy Exp $
+
+DESCRIPTION="CMUCL Lisp. This conforms to the ANSI Common Lisp Standard"
+HOMEPAGE="http://www.cons.org/cmucl/"
+LICENSE="public-domain"
+DEPEND="dev-lisp/common-lisp-controller"
+IUSE=""
+SLOT="0"
+KEYWORDS="~x86"
+SRC_URI="ftp://ftp.cn.freebsd.org/pub/cmucl/release/${PV}/cmucl-${PV}-x86-linux.tar.bz2
+	ftp://ftp.cn.freebsd.org/pub/cmucl/release/${PV}/cmucl-${PV}-x86-linux.extra.tar.bz2
+	ftp://ftp.cn.freebsd.org/pub/cmucl/release/${PV}/cmucl-${PV}.source.tar.bz2
+	ftp://ftp.cn.freebsd.org/pub/cmucl/release/${PV}/cmucl-${PV}.documents.tar.bz2"
+
+S=${WORKDIR}/${P}
+
+src_unpack() {
+	mkdir ${P}
+	cd ${P}
+	unpack cmucl-${PV}-x86-linux.tar.bz2
+	unpack cmucl-${PV}-x86-linux.extra.tar.bz2
+	unpack cmucl-${PV}.documents.tar.bz2
+	unpack cmucl-${PV}.source.tar.bz2
+}
+
+src_install() {
+	dobin bin/*
+	cp -a lib ${D}/usr
+
+# 	dodir /usr/share/common-lisp/
+# 	cp -a src ${D}/usr/share/common-lisp/source
+
+	insinto /etc/common-lisp/cmucl
+	doins ${FILESDIR}/site-init.lisp
+	dosym /etc/common-lisp/cmucl/site-init.lisp /usr/lib/cmucl/lib/site-init.lisp
+
+	doman man/man1/*.1
+	dodoc doc/cmucl/README doc/cmucl/release-*.txt
+	dodoc `find ${S}/package/doc/cmucl/ -type f \( -name \*.ps -o -name \*.pdf \) -print`
+	dodoc ${FILESDIR}/${PV}/README.Gentoo
+
+	[ -f /etc/lisp-config.lisp ] || touch ${D}/etc/lisp-config.lisp
+
+ 	dodir /etc/env.d
+ 	cat >${D}/etc/env.d/50cmucl <<EOF
+CMUCLLIB=/usr/lib/cmucl
+EOF
+	mv ${D}/usr/lib/cmucl/lib/lisp.core ${D}/usr/lib/cmucl/lib/lisp-dist.core
+	dosym /usr/lib/cmucl/lib/lisp-dist.core /usr/lib/cmucl/lisp-dist.core
+
+	insinto /usr/lib/cmucl
+	doins ${FILESDIR}/install-clc.lisp
+
+	exeinto /usr/lib/common-lisp/bin
+	cp ${FILESDIR}/cmucl-script.sh cmucl.sh
+	doexe cmucl.sh
+}
+
+pkg_postinst() {
+	einfo ">>> Fixing permissions for executables and directories..."
+	find /usr/share/common-lisp/source -type d -o \( -type f -perm +111 \) \
+		|xargs chmod 755
+	einfo ">>> fix permissions for non-executable files..."
+	find /usr/share/common-lisp/source -type f ! -perm -111 \
+		|xargs chmod 644
+
+	/usr/sbin/register-common-lisp-implementation cmucl
+}
+
+pkg_postrm() {
+	/usr/sbin/unregister-common-lisp-implementation cmucl
+}
---
+++ dev-lisp/cmucl-bin/files/site-init.lisp
@@ -0,0 +1,100 @@
+;;; -*- Mode: Lisp; Package: System -*-
+;;;
+;;; **********************************************************************
+;;; This code was written as part of the CMU Common Lisp project at
+;;; Carnegie Mellon University, and has been placed in the public domain.
+;;;
+
+;;; Heavy modifications by Peter Van Eynde
+
+(in-package "SYSTEM")
+
+(if (probe-file "/etc/lisp-config.lisp")
+    (load "/etc/lisp-config.lisp")
+  (format t "~%;;; Hey: there is no /etc/lisp-config.lisp file, please run \"dpkg-reconfigure common-lisp-controller\" as root"))
+
+;;; If you have sources installed on your system, un-comment the following form
+;;; and change it to point to the source location.  This will allow the Hemlock
+;;; "Edit Definition" command and the debugger to find sources for functions in
+;;; the core.
+(setf (ext:search-list "target:")
+      '(
+        "/usr/share/common-lisp/source/cmucl/"      ; object dir
+        ))
+
+(setf (ext:search-list "library:") '("/usr/lib/cmucl/"))
+;;; for safety...
+
+;;; optional extentions to the lisp image: delete if you
+;;; don't like them :-).
+(in-package :common-lisp-user)
+
+;;; newbie functions, delete if you don't like them
+
+#-hemlock
+(defun ed (&rest rest)
+  (multiple-value-bind (return errorp)
+    (ignore-errors (require :cmucl-hemlock))
+   (if errorp
+     (error "Sorry, cannot find hemlock, please install and use ilisp. Reason: ~S" errorp)
+    (apply #'ed rest))))
+
+(defun help ()
+  (format t "~
+Welcome to CMUCL for Linux.
+
+If you aren't running this with ilisp in emacs,
+or aren't intending to use hemlock then you
+deserve to lose. :-)
+
+Read the documentation in /usr/share/doc/cmucl.
+
+(quit) is to quit.
+(ed) starts hemlock (if installed)
+(demo) shows a list of demos
+(describe 'progn) gives information about progn for 
+ example.
+(inspect '*a*) interactively inspects *a* for example.
+"))
+
+(defun demo ()
+  (format t "Some demos are in the source package, some in the
+normal package.
+
+General demos:
+CLX demos:
+ if you have installed cmucl-source you can do:
+ (require :cmucl-clx)
+ (load \"cl-library:cmucl-clx;demo;hello\")
+ (xlib::hello-world \"\")
+ (load \"cl-library:;cmucl-clx;demo;menu\")
+ (xlib::just-say-lisp \"\")
+ (xlib::pop-up \"\"
+	       '(\"Linux\" \"FreeBSD\" \"OpenBSD\"))
+  exit by pressing control+C followed by a keypress.
+
+Clue demos:
+ if you have installed the clue package you can do:
+ (require :clue)
+ (load \"cl-library:;clue;examples;menu\")
+ (clue-examples::beatlemenuia \"\")
+ (clue-examples::pick-one \"\"
+			  \"One\"
+			  \"Two\"
+			  \"Three\")
+ (clue-examples::just-say-lisp \"\")
+ 
+ or you can use the Clio demos:
+ (require :clio)
+ (require :clio-examples)
+ (clio-examples::sketch :host  "")
+
+Pictures demos:
+ (require :pictures)
+ (load \"cl-library:;pictures;examples;road-demo\")
+ (pictures::road-demo)
+ press control-a to animate
+
+"))
+
+
