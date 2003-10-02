32dd976137a9c14c58b5306c9e16e015d8e71e22
Mamoru Komachi <usata@gentoo.org>
2003-10-02 20:26:18 +0000

    Changed jde to autoload (It'll reduce startup time)

--- app-emacs/jde/files/70jde-gentoo.el
+++ app-emacs/jde/files/70jde-gentoo.el
@@ -2,5 +2,5 @@
 ;;; JDE site-lisp configuration
 
 (setq load-path (cons "@SITELISP@/lisp" load-path))
-(require 'jde)
-
+(autoload 'jde-mode "jde" "Java Development Environment Emacs" t)
+(setq auto-mode-alist (cons '("\\.java$" . jde-mode) auto-mode-alist))
