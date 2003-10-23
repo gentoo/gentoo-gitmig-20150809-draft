bb4656fd3ce93e1a092b75a551de4869c73e0604
Mike Gardiner <obz@gentoo.org>
2003-10-23 08:53:00 +0000

    Fixed site-lisp file, and revision bumped the ebuild

--- app-emacs/psgml/files/50psgml-gentoo.el
+++ app-emacs/psgml/files/50psgml-gentoo.el
@@ -4,4 +4,4 @@
 (autoload 'sgml-mode "psgml" "Major mode to edit SGML files." t)
 (autoload 'xml-mode "psgml" "Major mode to edit XML files." t)
 
-(setq sgml-catalog-files "/etc/sgml/catalog")
+(setq sgml-catalog-files '("CATALOG" "/etc/sgml/catalog"))
