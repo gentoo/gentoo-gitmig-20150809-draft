e6c0f75cce37cb173f82bfc7a075077e9b180cda
Matthew Kennedy <mkennedy@gentoo.org>
2003-11-16 19:38:33 +0000

    refine defaults

--- app-emacs/semantic/files/60semantic-gentoo.el
+++ app-emacs/semantic/files/60semantic-gentoo.el
@@ -3,5 +3,28 @@
 
 (setq load-path (cons "@SITELISP@" load-path))
 (require 'semantic-util)
-(setq semantic-load-turn-everything-on t)
+
+;; To determine what should be done here every semantic release, check
+;; semantic-load.el.  Here we replicate the (when ... sexp twoards the
+;; end of the source.
+
+;; We turn on everything except foor `global-semantic-stickyfunc-mode'
+;; as it seems to be a source of consternation for new and experienced
+;; users alike.
+
+(setq semantic-load-turn-everything-on nil
+      semantic-load-turn-useful-things-on nil)
 (require 'semantic-load)
+
+(global-semantic-show-dirty-mode 1)
+(global-senator-minor-mode 1)
+(global-semantic-show-unmatched-syntax-mode 1)
+(global-semantic-auto-parse-mode 1)
+(global-semanticdb-minor-mode 1)
+(global-semantic-summary-mode 1)
+;; (global-semantic-stickyfunc-mode 1)
+
+(when (and (eq window-system 'x)
+	   (locate-library "imenu"))
+  (add-hook 'semantic-init-hooks (lambda ()
+				   (imenu-add-to-menubar "TOKENS"))))
---
+++ app-emacs/semantic/semantic-1.4.4-r1.ebuild
@@ -0,0 +1,31 @@
+# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/app-emacs/semantic/semantic-1.4.4-r1.ebuild,v 1.1 2003/11/16 19:38:30 mkennedy Exp $
+
+inherit elisp
+
+IUSE=""
+
+DESCRIPTION="The Semantic Bovinator is a lexer, parser-generator, and parser written in Emacs Lisp"
+HOMEPAGE="http://cedet.sourceforge.net/semantic.shtml"
+SRC_URI="mirror://sourceforge/cedet/${P}.tar.gz"
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="~x86"
+
+DEPEND="virtual/emacs
+	>=app-emacs/speedbar-0.14_beta4
+	>=app-emacs/eieio-0.17"
+
+S="${WORKDIR}/${P}"
+
+src_compile() {
+	make LOADPATH="${SITELISP}/speedbar ${SITELISP}/eieio" || die
+}
+
+src_install() {
+	elisp-install ${PN} *.el *.elc *.bnf
+	elisp-site-file-install ${FILESDIR}/60semantic-gentoo.el
+	dodoc ChangeLog NEWS INSTALL
+	doinfo semantic.info*
+}
