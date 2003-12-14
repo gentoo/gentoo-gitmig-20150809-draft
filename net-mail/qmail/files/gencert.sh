39cfa11ceafe2cadacade93e2299919aa4126da7
D.M.D. Ljungmark <spider@gentoo.org>
2003-12-14 23:05:08 +0000

    a few more cases of chown user.group -> user:group,  twice inside the patch, and once inside the gencert.sh

--- net-mail/qmail/files/gencert.sh
+++ net-mail/qmail/files/gencert.sh
@@ -1,4 +1,4 @@
 umask 0077 ; 
 /usr/bin/openssl genrsa -out /var/qmail/control/rsa512.new 512 >/dev/null 2>&1 && \
-chown qmaild.qmail /var/qmail/control/rsa512.new && \
+chown qmaild:qmail /var/qmail/control/rsa512.new && \
 /bin/mv -f /var/qmail/control/rsa512.new /var/qmail/control/rsa512.pem 
