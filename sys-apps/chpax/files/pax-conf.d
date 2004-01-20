e045bcf8d7f9026d51f1146bc9435f0929d2420a
Ned Ludd <solar@gentoo.org>
2004-01-20 09:33:44 +0000

    marking 0.6.1 stable on x86. Added a few more entrys to pageexec/segmexec exempt

--- sys-apps/chpax/files/pax-conf.d
+++ sys-apps/chpax/files/pax-conf.d
@@ -1,5 +1,5 @@
 ####################################################################
-#         Copyright 1999-2003 Gentoo Technologies, Inc.            #
+#         Copyright 1999-2004 Gentoo Technologies, Inc.            #
 # Distributed under the terms of the GNU General Public License v2 #
 ####################################################################
 #
@@ -9,13 +9,20 @@
 # m	do not restrict mprotect()
 # s	do not enforce segmentation based non-executable pages
 # x	do not randomize ET_EXEC base [ELF only]
+# z	zero flag mask
 
-PAGEEXEC_EXEMPT="/usr/X11R6/bin/xinit /usr/X11R6/bin/XFree86 /opt/blackdown-jdk-*/jre/bin/[a-z]* /opt/blackdown-jdk-*/jre/[a-z]* /usr/bin/xmms /usr/bin/mplayer /usr/lib/wine/bin/wine /usr/bin/blender /usr/bin/gxine /usr/bin/totem /usr/bin/acme"
+JAVA=/opt/blackdown-jdk-*/jre/{java,java_vm,keytool,kinit,klist,ktab,orbd,policytool,rmid,rmiregistry,servertool,tnameserv}
+WINE=/usr/lib/wine/bin/wine/{wine,winebuild,wineclipsrv,winedump,winegcc,wineserver,winewrap}
+
+# most things that need pageexec need segmexec and or vice versa so we set both.
+PAGEEXEC_EXEMPT="/usr/X11R6/bin/XFree86 /usr/bin/xmms /usr/bin/mplayer /opt/OpenOffice*/program/soffice.bin \
+ /usr/bin/blender /usr/bin/gxine /usr/bin/totem /usr/bin/acme $JAVA $WINE"
+
 TRAMPOLINE_EXEMPT=""
 MPROTECT_EXEMPT=""
 RANDMMAP_EXEMPT=""
 SEGMEXEC_EXEMPT="${PAGEEXEC_EXEMPT}"
-RANDEXEC_EXEMPT="/opt/blackdown-jdk-*/jre/bin/[a-z]* /opt/blackdown-jdk-*/jre/[a-z]*"
+RANDEXEC_EXEMPT="${JAVA}"
 
 # when zero flag mask is set to "yes" it will remove all pax flags from all files on reboot/stop
-ZERO_FLAG_MASK=no
+ZERO_FLAG_MASK=yes
