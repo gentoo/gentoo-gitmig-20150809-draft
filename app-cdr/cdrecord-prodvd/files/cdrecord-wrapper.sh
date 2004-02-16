b77c3272cae3a5d02a81d837732f638bbe17d049
Lars Weiler <pylon@gentoo.org>
2004-02-16 18:52:08 +0000

    Updated the wrapper-script as the free key changed

--- app-cdr/cdrecord-prodvd/files/cdrecord-wrapper.sh
+++ app-cdr/cdrecord-prodvd/files/cdrecord-wrapper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-CDR_SECURITY=8:dvd,clone:sparc-sun-solaris2,i386-pc-solaris2,i586-pc-linux,i686-pc-linux,powerpc-apple,hppa,powerpc-ibm-aix,i386-unknown-freebsd,i386-unknown-openbsd,i386-unknown-netbsd,powerpc-apple-netbsd,i386-pc-bsdi,mips-sgi-irix,i386-pc-sco:1.11::1076000000:::private/research/educational_non-commercial_use:5kiG6nW75l/om7C5dFGBTglXcWO1xJr2ZPcXcVjv1XK7Hk/rd4GtmxgLrhN
+#	Key Expires ---> 2004 Aug 20 13:06:40
+#
+CDR_SECURITY=8:dvd,clone:sparc-sun-solaris2,i386-pc-solaris2,i586-pc-linux,powerpc-apple,hppa,powerpc-ibm-aix,i386-unknown-freebsd,i386-unknown-openbsd,i386-unknown-netbsd,powerpc-apple-netbsd,i386-pc-bsdi,mips-sgi-irix,i386-pc-sco:1.11::1093000000:::private/research/educational_non-commercial_use:9vl2T2kP6w6O4h.bXuet8hP1Z3H5erm3qWmxhbcr.fHvuN8ZJbhQUWBzjAc
 export CDR_SECURITY
 exec cdrecord-ProDVD "$@"
