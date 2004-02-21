7ce5834a2e8fe4f021d5cf0ed303be1222996836
Mike Frysinger <vapier@gentoo.org>
2004-02-21 13:15:08 +0000

    exec in wrapper scripts

--- media-gfx/wings/files/wings.sh
+++ media-gfx/wings/files/wings.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Wing3D run script
-
 ESDL_ROOT="/usr/lib/erlang/lib/esdl"
 WINGS_ROOT="/usr/lib/erlang/lib/wings"
-
-erl -noshell -pa $ESDL_ROOT/ebin $WINGS_ROOT/ebin -run wings_start start_halt
+exec erl -noshell -pa $ESDL_ROOT/ebin $WINGS_ROOT/ebin -run wings_start start_halt
