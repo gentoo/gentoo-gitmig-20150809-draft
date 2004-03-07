b98af6d9e4ff0e8591f74404a58ce806d716b93b
Tim Yamin <plasmaroo@gentoo.org>
2004-03-07 11:38:42 +0000

    Added "before netmount" to the initialization script so that network filesystems that require IPSec would be properly mounted - closes bug #39359.

--- net-firewall/ipsec-tools/files/racoon.init.d
+++ net-firewall/ipsec-tools/files/racoon.init.d
@@ -1,8 +1,9 @@
 #!/sbin/runscript
-# Copyright 1999-2002 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
 
 depend() {
+	before netmount
 	use net
 }
 
