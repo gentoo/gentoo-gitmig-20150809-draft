4a7f37c820e7dfa5616621201872e955edc162f9
Jeremy Huddleston <eradicator@gentoo.org>
2004-04-13 18:16:33 +0000

    Fix nas init.d file to work with 1.5

--- media-libs/nas/files/nas.init.d
+++ media-libs/nas/files/nas.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2004 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/media-libs/nas/files/nas.init.d,v 1.1 2004/03/26 17:04:51 eradicator Exp $
+# $Header: /var/cvsroot/gentoo-x86/media-libs/nas/files/nas.init.d,v 1.2 2004/04/13 18:16:33 eradicator Exp $
 
 depend() {
 	need net
@@ -10,12 +10,13 @@ depend() {
 
 start() {
 	ebegin "Starting nas"
-	start-stop-daemon --start --quiet --exec /usr/X11R6/bin/nasd -- -b $NAS_OPTIONS
+	start-stop-daemon --start --quiet --exec /usr/X11R6/bin/nasd --background \
+		--pidfile /var/run/nasd.pid --make-pidfile -- $NAS_OPTIONS
 	eend $?
 }
 
 stop() {
 	ebegin "Stopping nas"
-	start-stop-daemon --stop --quiet --exec /usr/X11R6/bin/nasd
+	start-stop-daemon --stop --quiet --pidfile /var/run/nasd.pid
 	eend $?
 }
