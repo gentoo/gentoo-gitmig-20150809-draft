e187b7f551729e7ceb71b29d4b147e944af7bd77
Mike Frysinger <vapier@gentoo.org>
2004-04-18 08:44:01 +0000

    remove hardcoded path to q3ded #47970

--- games-fps/quake3/files/q3ded.conf.d
+++ games-fps/quake3/files/q3ded.conf.d
@@ -1,6 +1,6 @@
-# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/games-fps/quake3/files/q3ded.conf.d,v 1.1 2003/12/09 21:34:48 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/games-fps/quake3/files/q3ded.conf.d,v 1.2 2004/04/18 08:44:01 vapier Exp $
 
 q3_OPTS="+set com_hunkmegs 24 +set dedicated 1 +set net_port 27960 +map q3tourney2"
 
