5d057b8359fb8708af897e4b5c62cc6eb92bfddc
Martin Holzer <mholzer@gentoo.org>
2004-05-02 22:45:02 +0000

    updating copyright info

--- net-misc/rsync/files/rsyncd.conf.d
+++ net-misc/rsync/files/rsyncd.conf.d
@@ -1,6 +1,6 @@
-# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/rsync/files/rsyncd.conf.d,v 1.1 2003/02/22 22:41:09 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-misc/rsync/files/rsyncd.conf.d,v 1.2 2004/05/02 22:45:02 mholzer Exp $
 
 # Config file for /etc/init.d/rsyncd
 
