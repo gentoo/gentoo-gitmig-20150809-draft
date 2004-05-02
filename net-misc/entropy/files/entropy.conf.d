9e1f19d13a101817dc120a7d9d34760755c7b989
Mike Frysinger <vapier@gentoo.org>
2004-05-02 07:09:35 +0000

    ver bump #49473

---
+++ net-misc/entropy/files/entropy.conf.d
@@ -0,0 +1,6 @@
+# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/net-misc/entropy/files/entropy.conf.d,v 1.1 2004/05/02 07:09:35 vapier Exp $
+
+# The user to run entropy as
+ENTROPY_USER="entropy"
