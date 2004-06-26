cbedc0b541a5a96dd6d179184020803cbc81fe5d
Matthew Kennedy <mkennedy@gentoo.org>
2004-06-26 19:31:55 +0000

    *** empty log message ***

--- app-emacs/sawfish/files/50sawfish-gentoo.el
+++ app-emacs/sawfish/files/50sawfish-gentoo.el
@@ -2,9 +2,9 @@
 ;;; elscreen site-lisp configuration 
 
 (add-to-list 'load-path "@SITELISP@")
-(autoload 'sawfish-mode "sawfish" "sawfish-mode" t)
+(autoload 'sawfish-mode "sawfish" "Autoload for sawfish-mode" t)
+(autoload 'sawfish-interaction "sawfish" "Autoload for sawfish-interaction" t)
+(autoload 'sawfish-console "sawfish" "Autoload for sawfish-console" t)
 (setq auto-mode-alist (cons '("\\.sawfishrc$"  . sawfish-mode) auto-mode-alist)
       auto-mode-alist (cons '("\\.jl$"         . sawfish-mode) auto-mode-alist)
       auto-mode-alist (cons '("\\.sawfish/rc$" . sawfish-mode) auto-mode-alist))
-
-
