70b4bd48c001fa687afc1eb9f8aaadc03b3ea92d
Jon Hood <squinky86@gentoo.org>
2004-06-29 16:34:12 +0000

    better startup script (#52900)

--- net-p2p/xmule/files/xmule.sh
+++ net-p2p/xmule/files/xmule.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-p2p/xmule/files/xmule.sh,v 1.1 2004/06/28 16:20:57 squinky86 Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-p2p/xmule/files/xmule.sh,v 1.2 2004/06/29 16:34:12 squinky86 Exp $
 
-if [ ! -e ~/.xMule ]; then
-	echo "Creating ~/.xMule..."
-	mkdir ~/.xMule
-	echo "Copying required files to ~/.xMule..."
-	mkdir ~/.xMule/resource
-	cp /usr/share/xmule/*.pm ~/.xMule/resource/
-	echo "Starting xMule..."
-	xmule-bin
-else
-	xmule-bin
+if [ ! -d ~/.xMule ]; then
+        echo "Creating ~/.xMule..."
+        mkdir ~/.xMule || exit 1
 fi
+if [ ! -d ~/.xMule/resource ]; then
+        echo "Copying required files to ~/.xMule..."
+        mkdir ~/.xMule/resource || exit 1
+        cp /usr/share/xmule/*.pm ~/.xMule/resource/ || exit 1
+        echo "Starting xMule..."
+fi
+exec xmule-bin
