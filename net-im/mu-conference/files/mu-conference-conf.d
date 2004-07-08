f4d04f83e43526725d0658a034b0e194f551f576
Gustavo Felisberto <humpback@gentoo.org>
2004-07-08 17:35:01 +0000

    small typo

--- net-im/mu-conference/files/mu-conference-conf.d
+++ net-im/mu-conference/files/mu-conference-conf.d
@@ -1,8 +1,8 @@
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License, v2 or later
-# $Header: /var/cvsroot/gentoo-x86/net-im/mu-conference/files/mu-conference-conf.d,v 1.3 2004/07/08 16:31:40 humpback Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-im/mu-conference/files/mu-conference-conf.d,v 1.4 2004/07/08 17:35:01 humpback Exp $
 
-CONFIG= "/etc/jabber/muctrans.xml"
+CONFIG="/etc/jabber/muctrans.xml"
 
 #Need to find a way to fix the "double" expansion 
 #PIDFILE= grep pid ${CONFIG} | sed -e 's/<[^>]*>//g' | sed s/' '//g
