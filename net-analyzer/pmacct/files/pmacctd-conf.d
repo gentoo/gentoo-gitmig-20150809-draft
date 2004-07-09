b726b34f08c2f5df409627644d0ad6212835b433
Eldad Zack <eldad@gentoo.org>
2004-07-09 14:11:02 +0000

    initial ebuild, closes #49912

---
+++ net-analyzer/pmacct/files/pmacctd-conf.d
@@ -0,0 +1,10 @@
+# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/net-analyzer/pmacct/files/pmacctd-conf.d,v 1.1 2004/07/09 14:11:02 eldad Exp $
+
+# Interface to listen on
+INTERFACE="eth0"
+
+# Other options to pass to pmacctd
+#OPTS=""
+
---
+++ net-analyzer/pmacct/files/pmacctd-init.d
@@ -0,0 +1,35 @@
+#!/sbin/runscript
+# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/net-analyzer/pmacct/files/pmacctd-init.d,v 1.1 2004/07/09 14:11:02 eldad Exp $
+
+depend() {
+	need net
+}
+
+chekconfig() {
+	if [ ! -e /etc/pmacctd.conf ] ; then
+		eerror "You need an /etc/pmacctd.conf file to run pmacctd"
+		return 1
+	fi
+}
+
+start() {
+	chekconfig || return 1
+	ebegin "Starting pmacctd"
+	start-stop-daemon --start --exec /usr/sbin/pmacctd \
+		-- -D -f /etc/pmacctd.conf -i ${INTERFACE} ${OPTS}
+
+	# pmacctd apparently always returns a value > 0 when launched,
+	# and start-stop-daemon sees that an error an therfore always
+	# return 1. So we have to assume everything whent as expected.
+	
+	eend
+}
+
+stop() {
+	ebegin "Stopping pmacctd"
+	start-stop-daemon --stop --exec /usr/sbin/pmacctd
+	eend $?
+}
+
