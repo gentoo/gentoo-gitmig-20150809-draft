db3d5fc2c8ab0a94c05478ce25279100db5d4fb8
Sven Wegener <swegener@gentoo.org>
2004-07-10 23:15:03 +0000

    typo fix

--- net-irc/ptlink-opm/files/ptlink-opm.init.d
+++ net-irc/ptlink-opm/files/ptlink-opm.init.d
@@ -1,12 +1,11 @@
 #!/sbin/runscript
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-irc/ptlink-opm/files/ptlink-opm.init.d,v 1.2 2004/07/10 23:12:10 swegener Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-irc/ptlink-opm/files/ptlink-opm.init.d,v 1.3 2004/07/10 23:13:18 swegener Exp $
 
 depend() {
 	need net
-	use dns
-	provide ircd
+	use dns ircd
 }
 
 start() {
