83fa4a0d9b366dadf813fd1b2ebd5d15352c2565
Aron Griffis <agriffis@gentoo.org>
2004-07-14 22:17:00 +0000

    Gentoo Technologies -> Gentoo Foundation

--- games-rpg/egoboo/files/egoboo-2.22.sh
+++ games-rpg/egoboo/files/egoboo-2.22.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/games-rpg/egoboo/files/egoboo-2.22.sh,v 1.2 2004/01/08 11:13:13 mr_bones_ Exp $
+# $Header: /var/cvsroot/gentoo-x86/games-rpg/egoboo/files/egoboo-2.22.sh,v 1.3 2004/07/14 22:17:00 agriffis Exp $
 
 cd GENTOODIR/egoboo
 exec ./egoboo $@
