6598cfb6bebb82b1324160aa669dd71a119678d1
Aron Griffis <agriffis@gentoo.org>
2004-07-14 21:22:44 +0000

    Gentoo Technologies -> Gentoo Foundation

--- app-admin/hddtemp/files/hddtemp-conf.d
+++ app-admin/hddtemp/files/hddtemp-conf.d
@@ -1,6 +1,6 @@
-# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/app-admin/hddtemp/files/hddtemp-conf.d,v 1.1 2004/02/11 15:37:04 aliz Exp $
+# $Header: /var/cvsroot/gentoo-x86/app-admin/hddtemp/files/hddtemp-conf.d,v 1.2 2004/07/14 21:14:10 agriffis Exp $
 
 # the hddtemp executable
 HDDTEMP_EXEC=/usr/sbin/hddtemp
