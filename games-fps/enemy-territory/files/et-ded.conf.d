4393e238f003edd24f7c9205852f6e972460e823
Aron Griffis <agriffis@gentoo.org>
2004-07-14 22:16:39 +0000

    Gentoo Technologies -> Gentoo Foundation

--- games-fps/enemy-territory/files/et-ded.conf.d
+++ games-fps/enemy-territory/files/et-ded.conf.d
@@ -1,5 +1,5 @@
-# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/games-fps/enemy-territory/files/et-ded.conf.d,v 1.1 2003/12/09 22:06:38 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/games-fps/enemy-territory/files/et-ded.conf.d,v 1.2 2004/07/14 22:15:25 agriffis Exp $
 
 et_OPTS="+set com_hunkmegs 64 +set sv_maxrate 9000 +set com_zonemegs 32 +set dedicated 2 +map battery"
--- games-fps/quake3/files/q3ded.conf.d
+++ games-fps/quake3/files/q3ded.conf.d
@@ -1,6 +1,6 @@
-# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/games-fps/quake3/files/q3ded.conf.d,v 1.2 2004/04/18 08:44:01 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/games-fps/quake3/files/q3ded.conf.d,v 1.3 2004/07/14 22:15:46 agriffis Exp $
 
 q3_OPTS="+set com_hunkmegs 24 +set dedicated 1 +set net_port 27960 +map q3tourney2"
 
