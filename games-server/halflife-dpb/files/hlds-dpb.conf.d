6748eb45f97092ba02e34d71f9c34660521bcd1f
Aron Griffis <agriffis@gentoo.org>
2004-07-14 22:24:38 +0000

    Gentoo Technologies -> Gentoo Foundation

--- games-server/halflife-dpb/files/hlds-dpb.conf.d
+++ games-server/halflife-dpb/files/hlds-dpb.conf.d
@@ -1,6 +1,6 @@
-# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/games-server/halflife-dpb/files/hlds-dpb.conf.d,v 1.1 2003/09/10 05:51:11 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/games-server/halflife-dpb/files/hlds-dpb.conf.d,v 1.2 2004/07/14 22:17:40 agriffis Exp $
 
 # to find out more options, read these 2 files:
 # GENTOO_DIR/dpb/readme.txt
