5dc73afd43d186bf375b8ae5a08e8fe3e0dcc570
Aron Griffis <agriffis@gentoo.org>
2004-07-14 23:46:09 +0000

    Gentoo Technologies -> Gentoo Foundation

--- net-im/jabberd/files/jabber-conf.d
+++ net-im/jabberd/files/jabber-conf.d
@@ -1,6 +1,6 @@
-# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License, v2 or later
-# $Header: /var/cvsroot/gentoo-x86/net-im/jabberd/files/jabber-conf.d,v 1.3 2004/06/24 00:33:16 humpback Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-im/jabberd/files/jabber-conf.d,v 1.4 2004/07/14 23:45:05 agriffis Exp $
 
 # Configure which is the default jabberd server. Any value that is not
 # jabberd14 and jabberd2 will be started
