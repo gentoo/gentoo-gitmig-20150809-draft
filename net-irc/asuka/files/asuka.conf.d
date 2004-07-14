01936ae68e566d3b0e4a0cf55de1753830ab330c
Aron Griffis <agriffis@gentoo.org>
2004-07-14 23:48:05 +0000

    Gentoo Technologies -> Gentoo Foundation

--- net-irc/asuka/files/asuka.conf.d
+++ net-irc/asuka/files/asuka.conf.d
@@ -1,6 +1,6 @@
-# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-irc/asuka/files/asuka.conf.d,v 1.2 2004/01/02 13:52:35 zul Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-irc/asuka/files/asuka.conf.d,v 1.3 2004/07/14 23:46:29 agriffis Exp $
 
 # Config file for /etc/init.d/asuka
 
