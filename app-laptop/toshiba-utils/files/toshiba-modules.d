cbb81c316e0feb0ab76300c2a5ac20884a03aca4
Daniel Black <dragonheart@gentoo.org>
2004-07-18 02:34:58 +0000

    Manifest fix - files/toshiba-modules.d size mismatch

--- app-laptop/toshiba-utils/files/toshiba-modules.d
+++ app-laptop/toshiba-utils/files/toshiba-modules.d
@@ -1,2 +1,2 @@
-# $Header: /var/cvsroot/gentoo-x86/app-laptop/toshiba-utils/files/toshiba-modules.d,v 1.1 2004/03/08 05:52:22 latexer Exp $
+# $Header: /var/cvsroot/gentoo-x86/app-laptop/toshiba-utils/files/toshiba-modules.d,v 1.2 2004/07/18 02:34:58 dragonheart Exp $
 alias char-major-10-181 toshiba
