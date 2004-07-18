f0076b4992823508d225b4ffff35143f7654f151
Daniel Black <dragonheart@gentoo.org>
2004-07-18 03:27:44 +0000

    Manifest fix - files/1.03-r3/run-qmailpop3d size mismatch, files/1.03-r3/run-qmailqmqpd size mismatch, files/1.03-r3/run-qmailqmtpd size mismatch, files/1.03-r3/run-qmailsmtpd size mismatch, files/1.03-r3/qmail-genrsacert.sh size mismatch, files/1.03-r3/conf-pop3d size mismatch, files/1.03-r3/conf-qmqpd size mismatch, files/1.03-r3/conf-qmtpd size mismatch, files/1.03-r3/servercert.cnf size mismatch, files/1.03-r3/conf-smtpd size mismatch, files/1.03-r3/mkservercert size mismatch, files/1.03-r3/conf-common size mismatch, files/1.03-r3/config-sanity-check size mismatch, files/1.03-r4/run-qmailpop3d size mismatch, files/1.03-r4/run-qmailqmqpd size mismatch, files/1.03-r4/run-qmailqmtpd size mismatch, files/1.03-r4/run-qmailsmtpd size mismatch, files/1.03-r4/qmail-genrsacert.sh size mismatch, files/1.03-r4/conf-pop3d size mismatch, files/1.03-r4/conf-qmqpd size mismatch, files/1.03-r4/conf-qmtpd size mismatch, files/1.03-r4/servercert.cnf size mismatch, files/1.03-r4/conf-smtpd size mismatch, files/1.03-r4/mkservercert size mismatch, files/1.03-r4/conf-common size mismatch, files/1.03-r4/config-sanity-check size mismatch

--- mail-mta/qmail-ldap/files/1.03-r4/qmail-genrsacert.sh
+++ mail-mta/qmail-ldap/files/1.03-r4/qmail-genrsacert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Header: /var/cvsroot/gentoo-x86/mail-mta/qmail-ldap/files/1.03-r4/qmail-genrsacert.sh,v 1.1 2004/05/30 10:52:45 robbat2 Exp $
+# $Header: /var/cvsroot/gentoo-x86/mail-mta/qmail-ldap/files/1.03-r4/qmail-genrsacert.sh,v 1.2 2004/07/18 03:27:44 dragonheart Exp $
 # Robin H. Johnson <robbat2@gentoo.org> - October 17, 2003
 # This file generates the static temporary RSA keys needed for qmail to encrypt messages
 # It should be run from a crontab, once a day is ok on low load machines, but
