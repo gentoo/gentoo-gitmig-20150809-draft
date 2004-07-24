1c79dfce5ccd92c49074d5bb306a4250cf469d79
Jodok Batlogg <batlogg@gentoo.org>
2004-07-24 22:26:25 +0000

    cleaning up

--- net-zope/zope/files/custom_zodb.py
+++
@@ -1,7 +0,0 @@
-import ZEO.ClientStorage, os, string     
-
-host=os.environ.get('ZEO_SERVER_NAME', '')
-port=string.atoi(os.environ['ZEO_SERVER_PORT'])    
-
-Storage=ZEO.ClientStorage.ClientStorage(
-        (host, port), name='ZEO Demo')
