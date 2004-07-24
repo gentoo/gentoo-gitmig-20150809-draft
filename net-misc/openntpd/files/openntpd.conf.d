4e0db7e0d4772e42570121991f985b3ae2b5fec6
Mike Frysinger <vapier@gentoo.org>
2004-07-24 05:33:08 +0000

    rename init.d scripts #57869

--- net-misc/openntpd/files/openntpd.conf.d
+++ net-misc/openntpd/files/openntpd.conf.d
@@ -1,5 +1,5 @@
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/openntpd/files/ntpd.conf.d,v 1.1 2004/07/20 23:44:31 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-misc/openntpd/files/openntpd.conf.d,v 1.1 2004/07/24 05:33:08 vapier Exp $
 
 NTPD_HOME=/var/empty
