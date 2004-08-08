593dacfbb4d13d10a37c2890a2d192176e871ed2
Stuart Herbert <stuart@gentoo.org>
2004-08-08 17:52:39 +0000

    Moving to www-servers/orion

--- net-www/orion/files/2.0.1/stop_orion.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# $Header: /var/cvsroot/gentoo-x86/net-www/orion/files/2.0.1/stop_orion.sh,v 1.2 2004/07/18 04:26:56 dragonheart Exp $
-ps auxww | grep orion.jar | awk '{print $2}' | xargs kill &> /dev/null
