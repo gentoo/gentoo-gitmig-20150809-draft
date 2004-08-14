67c1a6b0d227164601123f44aa922ce4c55c55dd
Sven Wegener <swegener@gentoo.org>
2004-08-14 22:35:49 +0000

    Fixed init script.

--- net-irc/ircservices/files/ircservices.init.d
+++ net-irc/ircservices/files/ircservices.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License, v2 or later
-# $Header: /var/cvsroot/gentoo-x86/net-irc/ircservices/files/ircservices.init.d,v 1.1 2004/08/14 21:38:26 swegener Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-irc/ircservices/files/ircservices.init.d,v 1.2 2004/08/14 22:35:49 swegener Exp $
 
 depend() {
 	need net
@@ -19,7 +19,7 @@ start() {
 
 stop() {
 	ebegin "Stopping IRC Services"
-	kill <$(/var/lib/ircservices/ircservices.pid)
+	start-stop-daemon --stop --quiet --pidfile /var/lib/ircservices/ircservices.pid
 	eend $?
 	rm -f /var/lib/ircservices/ircservices.pid
 }
