a1b296cd8ffa109d17e9bb0b630d74f024348999
Sven Wegener <swegener@gentoo.org>
2004-08-14 21:38:26 +0000

    Initial import, ebuild written by me.

---
+++ net-irc/ircservices/files/ircservices.init.d
@@ -0,0 +1,25 @@
+#!/sbin/runscript
+# Copyright 1999-2004 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License, v2 or later
+# $Header: /var/cvsroot/gentoo-x86/net-irc/ircservices/files/ircservices.init.d,v 1.1 2004/08/14 21:38:26 swegener Exp $
+
+depend() {
+	need net
+	use ircd
+}
+
+start() {
+	ebegin "Starting IRC Services"
+	/usr/bin/ircservices \
+		-dir=/var/lib/ircservices \
+		-log=/var/log/ircservices/ircservices.log \
+		&>/dev/null
+	eend $?
+}
+
+stop() {
+	ebegin "Stopping IRC Services"
+	kill <$(/var/lib/ircservices/ircservices.pid)
+	eend $?
+	rm -f /var/lib/ircservices/ircservices.pid
+}
