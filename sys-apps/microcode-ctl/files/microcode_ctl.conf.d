bcca0cbc55145ddf66d23a0032cd9339a956dc34
Mike Frysinger <vapier@gentoo.org>
2004-09-01 22:04:20 +0000

    default to the sysfs naming version

--- sys-apps/microcode-ctl/files/microcode_ctl.conf.d
+++ sys-apps/microcode-ctl/files/microcode_ctl.conf.d
@@ -1,5 +1,5 @@
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-apps/microcode-ctl/files/microcode_ctl.conf.d,v 1.1 2004/09/01 01:16:46 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-apps/microcode-ctl/files/microcode_ctl.conf.d,v 1.2 2004/09/01 22:04:20 vapier Exp $
 
-MICROCODE_DEV="/dev/cpu/microcode"
+MICROCODE_DEV="/dev/microcode"
