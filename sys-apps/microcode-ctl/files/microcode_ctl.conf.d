5bed4d7129138692b0a22f066c005a6a26f29c7e
Mike Frysinger <vapier@gentoo.org>
2004-09-01 01:16:46 +0000

    ver bump #60396

---
+++ sys-apps/microcode-ctl/files/microcode_ctl.conf.d
@@ -0,0 +1,5 @@
+# Copyright 1999-2004 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/sys-apps/microcode-ctl/files/microcode_ctl.conf.d,v 1.1 2004/09/01 01:16:46 vapier Exp $
+
+MICROCODE_DEV="/dev/cpu/microcode"
