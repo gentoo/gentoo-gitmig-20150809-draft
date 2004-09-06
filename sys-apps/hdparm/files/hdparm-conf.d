f9dc193b53d13694164037c83b3a4c1ce533455c
Sven Wegener <swegener@gentoo.org>
2004-09-06 02:17:08 +0000

    Fixed typos in conf.d files. Thanks to Redshift <gentoo@halffull.org> and Steve Dibb <sdibb@myway.com> in bug #62275.

--- sys-apps/hdparm/files/hdparm-conf.d
+++ sys-apps/hdparm/files/hdparm-conf.d
@@ -1,11 +1,11 @@
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-apps/hdparm/files/hdparm-conf.d,v 1.3 2004/07/15 00:43:00 agriffis Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-apps/hdparm/files/hdparm-conf.d,v 1.4 2004/09/06 02:17:08 swegener Exp $
 
 # You can either set hdparm arguments for each drive using disc*_args and cdrom*_args..
 # eg.
 # disc0_args="-d1 -X66"
-# disc1_args"-d1"
+# disc1_args="-d1"
 # cdrom0_args="-d1"
 
 # Or, you can set hdparm options for ALL drives using all_args..
