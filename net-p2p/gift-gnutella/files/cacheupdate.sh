1e5030fa5ceae81c0492c23964fb410bd7b19888
Jon Hood <squinky86@gentoo.org>
2004-09-17 18:37:54 +0000

    Applying makeup because this was uglier than a drunken bearded hermaphrodite on prom night.

--- net-p2p/gift-gnutella/files/cacheupdate.sh
+++ net-p2p/gift-gnutella/files/cacheupdate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-p2p/gift-gnutella/files/cacheupdate.sh,v 1.2 2004/09/13 10:29:07 squinky86 Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-p2p/gift-gnutella/files/cacheupdate.sh,v 1.3 2004/09/17 18:37:54 squinky86 Exp $
 
 if [ -d ~/.giFT/Gnutella/ ]; then
 	cd ~/.giFT/Gnutella
-	wget http://gwebcache.squinky.gotdns.com/perlgcache.cgi?get=1\&hostfile=1\&net=gnutella2\&client=GEN2\&version=0.1 -O gwebcaches.new || die
+	wget http://gwebcache.squinky.gotdns.com/perlgcache.cgi?get=1\&hostfile=1\&net=gnutella2\&client=GEN2\&version=0.1 -O gwebcaches.new || die "Unable to retrieve new caches."
 	grep "u|" gwebcaches.new > gwebcaches.new1
 	sed -i -e 's:u|::g' gwebcaches.new1
 	sed -i -e 's:|.*::g' gwebcaches.new1
@@ -15,6 +15,7 @@ if [ -d ~/.giFT/Gnutella/ ]; then
 	sed -i -e 's:|.*::g' nodes.new
 	mv nodes.new nodes
 	rm gwebcaches.new
+	echo "Update complete!"
 else
 	echo "Please emerge gift-gnutella and run gift-setup."
 fi
