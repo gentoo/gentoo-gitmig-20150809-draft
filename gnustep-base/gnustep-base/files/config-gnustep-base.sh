412650657acc41fbd6335eb01c740bd1db6603ad
Sven Wegener <swegener@gentoo.org>
2004-09-28 00:51:18 +0000

    Gentoo Technologies, Inc. -> Gentoo Foundation

--- gnustep-base/gnustep-base/files/config-gnustep-base.sh
+++ gnustep-base/gnustep-base/files/config-gnustep-base.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/gnustep-base/gnustep-base/files/config-gnustep-base.sh,v 1.1 2004/09/24 01:04:11 fafhrd Exp $
+# $Header: /var/cvsroot/gentoo-x86/gnustep-base/gnustep-base/files/config-gnustep-base.sh,v 1.2 2004/09/28 00:51:18 swegener Exp $
 
 TIME_ZONE="America/New_York"
 LANGUAGE="English"
