841ea098490845b44f9f5c00d3e2e344cd3b322e
Sven Wegener <swegener@gentoo.org>
2004-09-28 00:50:15 +0000

    Gentoo Technologies, Inc. -> Gentoo Foundation

--- gnustep-apps/aclock/files/config-aclock.sh
+++ gnustep-apps/aclock/files/config-aclock.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/gnustep-apps/aclock/files/config-aclock.sh,v 1.1 2004/09/24 01:07:05 fafhrd Exp $
+# $Header: /var/cvsroot/gentoo-x86/gnustep-apps/aclock/files/config-aclock.sh,v 1.2 2004/09/28 00:50:15 swegener Exp $
 
 echo "AClock can draw with a smooth second hand or normal.  Please choose one."
 read -p "'smooth' or 'normal': " style
