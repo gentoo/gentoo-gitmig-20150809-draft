1b85604d5ff08d51c796567979f9e09f612c587b
Sven Wegener <swegener@gentoo.org>
2004-09-28 01:57:58 +0000

    Gentoo Technologies, Inc. -> Gentoo Foundation

--- sys-devel/gcc-config/files/wrapper.c
+++ sys-devel/gcc-config/files/wrapper.c
@@ -1,8 +1,8 @@
 /*
- * Copyright 1999-2003 Gentoo Technologies, Inc.
+ * Copyright 1999-2004 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
  * Author: Martin Schlemmer <azarah@gentoo.org>
- * $Header: /var/cvsroot/gentoo-x86/sys-devel/gcc-config/files/wrapper.c,v 1.6 2004/07/18 04:44:54 dragonheart Exp $
+ * $Header: /var/cvsroot/gentoo-x86/sys-devel/gcc-config/files/wrapper.c,v 1.7 2004/09/28 01:57:58 swegener Exp $
  */
 
 #define _REENTRANT
