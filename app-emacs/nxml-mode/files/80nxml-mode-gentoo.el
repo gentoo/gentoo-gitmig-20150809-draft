d788e66f549a83a087b7cb0f8d80545fe7a2e3fb
Mamoru Komachi <usata@gentoo.org>
2004-10-06 07:19:50 +0000

    Added .xsd to auto-mode-alist; bug #65836.

--- app-emacs/nxml-mode/files/80nxml-mode-gentoo.el
+++ app-emacs/nxml-mode/files/80nxml-mode-gentoo.el
@@ -1,9 +1,9 @@
 
 ;;; nxml site-lisp configuration
 
-(setq load-path (cons "@SITELISP@" load-path))
+(add-to-list 'load-path "@SITELISP@")
 (load "@SITELISP@/rng-auto.el")
 
 (setq auto-mode-alist
-      (cons '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\)\\'" . nxml-mode)
+      (cons '("\\.\\(xml\\|xsl\\|xsd\\|rng\\|xhtml\\)\\'" . nxml-mode)
 	        auto-mode-alist))
