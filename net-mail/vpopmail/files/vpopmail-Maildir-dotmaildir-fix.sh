eb73fc5e7728247eef66476307b5b5f5ab5300fd
Robin H. Johnson <robbat2@gentoo.org>
2004-10-19 17:55:24 +0000

    fix bug #59362.

--- net-mail/vpopmail/files/vpopmail-Maildir-dotmaildir-fix.sh
+++ net-mail/vpopmail/files/vpopmail-Maildir-dotmaildir-fix.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+# Copyright 1999-2004 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/net-mail/vpopmail/files/vpopmail-Maildir-dotmaildir-fix.sh,v 1.2 2004/10/19 17:55:24 robbat2 Exp $
+# Written by Robin H. Johnson, robbat2@gentoo.org
+
 OLDNAME='Maildir'
 NEWNAME='.maildir'
 SEARCHPATH=/var/vpopmail/domains/
+MINDEPTH=3
+# If you have a very large vpopmail deployment, you may need to increase MAXDEPTH.
+MAXDEPTH=6
 
 if [ "${1}" == '--revert' ]; then
     SEARCHNAME="${NEWNAME}"
@@ -12,10 +20,12 @@ else
 fi
 
 echo "Doing '${SEARCHNAME}' '${REPLACENAME}' changeover"
-for i in `find ${SEARCHPATH} -name "${SEARCHNAME}" -maxdepth 3 -mindepth 3 -type d`; do
-    foundname=${i}
-    todoname=${foundname/${SEARCHNAME}/${REPLACENAME}}
+echo find ${SEARCHPATH} -name "${SEARCHNAME}" -maxdepth $MAXDEPTH -mindepth $MINDEPTH -type d
+for i in `find ${SEARCHPATH} -name "${SEARCHNAME}" -maxdepth $MAXDEPTH -mindepth $MINDEPTH -type d`; do
+    foundname=${i/${SEARCHNAME}*}${SEARCHNAME}
     base="`dirname $i`"
+    todoname=${base}/${REPLACENAME}
+	#echo "$foundname -> $todoname"
     echo "Fixing `echo $base | sed -e "s|${SEARCHPATH}||g"`"
     chmod +t $base
     if [ -L ${todoname} ]; then
