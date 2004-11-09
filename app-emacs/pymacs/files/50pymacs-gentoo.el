e50c4436fd8f12ebcd6c91741167be84e8a8811c
Mamoru Komachi <usata@gentoo.org>
2004-11-09 18:33:47 +0000

    Initial import. Ebuild submitted by Nick Savchenko <rust@voliacable.com>; bug #69323.

---
+++ app-emacs/pymacs/files/50pymacs-gentoo.el
@@ -0,0 +1,6 @@
+
+;;; emacs site-lisp configuration for pymacs
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
