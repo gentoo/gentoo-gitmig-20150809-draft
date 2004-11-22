b4ea80eff3fb50ab1d54551125b3542c077d577a
Mike Frysinger <vapier@gentoo.org>
2004-11-22 15:27:51 +0000

    support passing options

--- net-misc/openntpd/files/openntpd.conf.d
+++ net-misc/openntpd/files/openntpd.conf.d
@@ -1,5 +1,7 @@
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/openntpd/files/openntpd.conf.d,v 1.1 2004/07/24 05:33:08 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-misc/openntpd/files/openntpd.conf.d,v 1.2 2004/11/22 15:27:51 vapier Exp $
 
 NTPD_HOME=/var/empty
+
+NTPD_OPTS=""
