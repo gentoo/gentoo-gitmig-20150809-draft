daa4b882e8dbdcf15bd28f9b82bd5c1dd32af088
Olivier Fisette <ribosome@gentoo.org>
2004-12-28 05:30:36 +0000

    Moved from app-sci/gimps to sci-mathematics/gimps.

---
+++ sci-mathematics/gimps/files/gimps-conf.d
@@ -0,0 +1,12 @@
+# Config file for /etc/init.d/gimps
+
+# the number of CPUs GIMPS will use
+GIMPS_CPUS="1"
+
+# set up any options you want for GIMPS
+# for more info, `mprime -h`
+# GIMPS_OPTIONS=""
+
+# this is the directory where setiathome's run-time
+# data files will be stored
+GIMPS_DIR=/var/lib/gimps
