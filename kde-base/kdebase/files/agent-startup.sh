bc8afdfb10aa6a4370e50316a6af3aa03a6184e3
Gregorio Guidi <greg_g@gentoo.org>
2005-01-16 00:40:36 +0000

    Fix agent-startup.sh.

--- kde-base/kdebase/files/agent-startup.sh
+++ kde-base/kdebase/files/agent-startup.sh
@@ -1,14 +1,14 @@
 # Agents startup file
 #
 # This file is sourced at kde startup, so that
-Ã# the environment variablesset here are available
+# the environment variables set here are available
 # throughout the session.
 
-# Uncomment the following lines to start the gpg-agent
+# Uncomment the following lines to start gpg-agent
 # and/or ssh-agent at kde startup.
 # If you do so, do not forget to uncomment the respective
-# lines in KDEDIR/shutdown to properly kill the
-# agents when the session ends.
+# lines in KDEDIR/shutdown/agent-shutdown.sh to
+# properly kill the agents when the session ends.
 
 #if [ -x /usr/bin/gpg-agent ]; then
 #  eval "$(/usr/bin/gpg-agent --daemon)"
