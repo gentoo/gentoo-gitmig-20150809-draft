f2d621b948553f27d79f304fce839cb9479ab8d4
Sven Wegener <swegener@gentoo.org>
2005-01-27 23:26:11 +0000

    Removed old vulnerable ebuilds. Package-Manager: portage-2.0.51-r15

--- net-irc/ngircd/files/ngircd.init.d
+++ net-irc/ngircd/files/ngircd.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
-# Copyright 1999-2004 Gentoo Foundation
+# Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-irc/ngircd/files/ngircd.init.d,v 1.1 2004/07/08 22:59:54 swegener Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-irc/ngircd/files/ngircd.init.d,v 1.2 2005/01/27 23:26:11 swegener Exp $
 
 depend() {
 	need net
