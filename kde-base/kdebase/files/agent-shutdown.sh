ec9f9891fd733b5d02ad2b10b3c4756388d3213c
Gregorio Guidi <greg_g@gentoo.org>
2005-02-07 19:03:35 +0000

    Cosmetic changes, not affecting functionality.

--- kde-base/kdebase/files/agent-shutdown.sh
+++ kde-base/kdebase/files/agent-shutdown.sh
@@ -4,10 +4,10 @@
 # Uncomment the following lines to kill the agents
 # that were started at session startup.
 
-#if [ "${GPG_AGENT_INFO}" ]; then
+#if [ -n "${GPG_AGENT_INFO}" ]; then
 #  kill $(echo ${GPG_AGENT_INFO} | cut -d':' -f 2) >/dev/null 2>&1
 #fi
-#
-#if [ "${SSH_AGENT_PID}" ]; then
-#  ssh-agent -k
+
+#if [ -n "${SSH_AGENT_PID}" ]; then
+#  eval "$(ssh-agent -k)"
 #fi
