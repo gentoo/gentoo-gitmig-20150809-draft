99c3578cc62be3ad5d5020cae6038439f5a8bca4
Henrik Brix Andersen <brix@gentoo.org>
2005-02-14 08:25:29 +0000

    Version bump. Package-Manager: portage-2.0.51-r15

---
+++ net-wireless/wpa_supplicant/files/wpa_supplicant-0.3.8-conf.d
@@ -0,0 +1,12 @@
+# Copyright 1999-2004 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/net-wireless/wpa_supplicant/files/wpa_supplicant-0.3.8-conf.d,v 1.1 2005/02/14 08:25:29 brix Exp $
+
+# List of interfaces
+INTERFACES="eth1"
+
+# Common arguments to all wpa_supplicant instances
+ARGS="-w"
+
+# Interface specific arguments
+#ARGS_eth1="-Dipw"
