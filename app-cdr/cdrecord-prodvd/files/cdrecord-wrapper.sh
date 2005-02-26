31f54199b653e7e86dfb330268eecb93ed73563f
Jeremy Huddleston <eradicator@gentoo.org>
2005-02-26 11:36:39 +0000

    Updated the key. Package-Manager: portage-2.0.51.16

--- app-cdr/cdrecord-prodvd/files/cdrecord-wrapper.sh
+++ app-cdr/cdrecord-prodvd/files/cdrecord-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#	Key Expires ---> 2005 Mar  5 06:20:00
+#	Key Expires ---> 2005 Oct 22 18:53:20
 #
-CDR_SECURITY=8:dvd,clone:sparc-sun-solaris2,i386-pc-solaris2,i586-pc-linux,powerpc-apple,hppa,powerpc-ibm-aix,i386-unknown-freebsd,i386-unknown-openbsd,i386-unknown-netbsd,powerpc-apple-netbsd,i386-pc-bsdi,mips-sgi-irix,i386-pc-sco:1.11::1110000000:::private/research/educational_non-commercial_use:1p8anqnRQ3jtiMgniHZLFfWSyfs2L.m4ab/OAUHU95PYL.SF7W2NAIDkJ/A
+CDR_SECURITY=8:dvd,clone:sparc-sun-solaris2,i386-pc-solaris2,i586-pc-linux,powerpc-apple,hppa,powerpc-ibm-aix,i386-unknown-freebsd,i386-unknown-openbsd,i386-unknown-netbsd,powerpc-apple-netbsd,i386-pc-bsdi,mips-sgi-irix,i386-pc-sco,i586-pc-cygwin:1.11::1130000000:::private/research/educational_non-commercial_use:4Yrzy07SJ6rmjHWclTIMRj9IPRFm1dbW9E3QNK/HtzKg6Sr0h7Qj88980Fo
 export CDR_SECURITY
 exec cdrecord-ProDVD "$@"
