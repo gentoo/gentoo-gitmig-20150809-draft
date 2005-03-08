68e2e0d79015a3e1ba5a75a841d372e1e4260f34
Gustavo Felisberto <humpback@gentoo.org>
2005-03-08 22:23:36 +0000

    deleting older versions and adding -r1 that has better deps Package-Manager: portage-2.0.51.19

--- net-im/mercury-bin/files/mercury.sh
+++ net-im/mercury-bin/files/mercury.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 MERCURY_INSTALL_PATH="/opt/mercury-bin"
-JDICTRAY_INSTALL_PATH="/opt/jdictrayapi/lib/"
 cd ${MERCURY_INSTALL_PATH}
-java -Djava.library.path=${JDICTRAY_INSTALL_PATH} -classpath $(java-config -p jdom-1.0_beta6,jdictrayapi,jmf-bin,mercury-bin) com.dMSN.Main
+java -Djava.library.path=$(java-config -i jdictrayapi) -classpath $(java-config -p jdom-1.0_beta6,jdictrayapi,jmf-bin,mercury-bin,xpp3,jgoodies-looks-1.3) com.dMSN.Main
