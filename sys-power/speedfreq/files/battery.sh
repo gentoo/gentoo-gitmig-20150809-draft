31c69657f5b21833cc13f8131e16b8aceafd8ec0
Ciaran McCreesh <ciaranm@gentoo.org>
2005-03-14 21:57:10 +0000

    Moved from sys-apps/speedfreq to sys-power/speedfreq.

---
+++ sys-power/speedfreq/files/battery.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Battery acpi script that set different speedfreq modes depending on AC adapter
+# Can only handle one battery at the time, will add support for more when I get time
+#
+# To use this script add the following lines to /etc/acpi/events/default
+#  event=battery.*
+#  action=/etc/acpi/battery.sh %e
+
+set $*
+source /etc/conf.d/speedfreq
+[ "${SPEEDFREQ_SPEED}" != "auto" ] && exit 0
+
+case "$2" in
+	BAT1)
+		if [ $4 -eq 00000001 ] && [ -f /proc/acpi/battery/BAT1/state ] ; then
+			action="`cat /proc/acpi/battery/BAT1/state | grep charging | cut -c 26-`"
+			case $action in
+				charging)	/usr/bin/speedfreq -p performance
+						logger "A/C adapter plugged in"
+						;;
+				discharging)	/usr/bin/speedfreq -p powersave
+						logger "A/C adapter plugged out"
+						;;
+			esac
+		fi
+		;;
+esac
