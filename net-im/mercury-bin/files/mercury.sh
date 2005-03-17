082aed4e608a4f1ca784a5a9d27c0f2a6d226fa4
Thomas Matthijs <axxo@gentoo.org>
2005-03-17 18:41:20 +0000

    version bump Package-Manager: portage-2.0.51.19

--- net-im/mercury-bin/files/mercury.sh
+++ net-im/mercury-bin/files/mercury.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 MERCURY_INSTALL_PATH="/opt/mercury-bin"
 cd ${MERCURY_INSTALL_PATH}
-java -Djava.library.path=$(java-config -i jdictrayapi) -classpath $(java-config -p jdom-1.0_beta6,jdictrayapi,jmf-bin,mercury-bin,xpp3,jgoodies-looks-1.3) com.dMSN.Main
+java -Djava.library.path=$(java-config -i jdictrayapi) -classpath $(java-config -p jdom-1.0,jdictrayapi,jmf-bin,mercury-bin,xpp3,jgoodies-looks-1.3) com.dMSN.Main
