b054125977fe5ddbd6182b783ca72a62a5cb6b8a
Matthew Kennedy <mkennedy@gentoo.org>
2005-04-03 20:03:21 +0000

    Package-Manager: portage-2.0.51.19

--- dev-lang/gnu-smalltalk/files/50gnu-smalltalk-gentoo.el
+++ dev-lang/gnu-smalltalk/files/50gnu-smalltalk-gentoo.el
@@ -3,4 +3,5 @@
 
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'smalltalk-mode "smalltalk-mode" "Autoload for smalltalk-mode" t)
+(autoload 'gst "gst-mode" "Autoload for gst" t)
 (add-to-list 'auto-mode-alist '("\\.st\\'" . smalltalk-mode))
