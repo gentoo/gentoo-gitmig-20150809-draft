59955be870268f5dad1165cd3fe5a7764a4eba11
Mike Frysinger <vapier@gentoo.org>
2005-05-31 23:05:07 +0000

    make people enable with RC_BOOTLOG=yes Package-Manager: portage-2.0.51.22-r1

--- app-admin/showconsole/files/bootlogger.sh
+++ app-admin/showconsole/files/bootlogger.sh
@@ -1,11 +1,13 @@
 # /lib/rcscripts/addons/bootlogger.sh:  Handle logging of output at boot
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/app-admin/showconsole/files/bootlogger.sh,v 1.1 2005/05/31 22:54:29 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/app-admin/showconsole/files/bootlogger.sh,v 1.2 2005/05/31 23:05:07 vapier Exp $
 
 if [[ -x /sbin/blogd ]] ; then
 
 bootlog() {
+	[[ ${RC_BOOTLOG} == "yes" ]] || return 0
+
 	local opt=$1
 	shift
 	case ${opt} in
