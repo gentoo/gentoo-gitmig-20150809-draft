86356be81d11c57561fa82de5605548d25d4626f
Roy Marples <uberlord@gentoo.org>
2005-06-07 18:34:17 +0000

    init.d script now checks for listen=YES or listen_ipv6=YES - fixes #54228 xinetd USE flag added to make support of it optional - fixes #75892 USERID removed from xinetd files - fixes #78764 init.d script now does a "killall vsftpd" when stopping - fixes #81819 cleaner ebuild - fixes #84733 Package-Manager: portage-2.0.51.19

--- net-ftp/vsftpd/files/vsftpd.init.d
+++ net-ftp/vsftpd/files/vsftpd.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 2003-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License, v2
-# $Header: /var/cvsroot/gentoo-x86/net-ftp/vsftpd/files/vsftpd.init.d,v 1.4 2004/10/01 03:16:13 jforman Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-ftp/vsftpd/files/vsftpd.init.d,v 1.5 2005/06/07 18:34:17 uberlord Exp $
 
 depend() {
 	need net
@@ -9,29 +9,33 @@ depend() {
 }
 
 checkconfig() {
-	if [ ! -e ${VSFTPD_CONF} ] ; then
+	if [[ ! -e ${VSFTPD_CONF} ]] ; then
 		eerror "Please setup ${VSFTPD_CONF} before starting vsftpd"
 		eerror "There are sample configurations in /usr/share/doc/vsftpd"
 		return 1
-	else
-		( grep -q "^background=YES" ${VSFTPD_CONF}  && grep -q "^listen=YES" ${VSFTPD_CONF} )  || {
-		eerror "${VSFTPD_CONF} must contain background=YES and listen=YES"
-		eerror "in order to start vsftpd from /etc/init.d/vsftpd"
-		return 2
-	}
 	fi
+
+	if grep -q "^background=YES" ${VSFTPD_CONF} ; then
+		local c=$( grep -c "^\(listen\|listen_ipv6\)=YES" "${VSFTPD_CONF}" )
+		[[ ${c} == "1" ]] && return 0
+	fi
+		
+	eerror "${VSFTPD_CONF} must contain background=YES and either"
+	eerror "listen=YES or listen_ipv6=YES (but not both)"
+	eerror "in order to start vsftpd from /etc/init.d/vsftpd"
+	return 1
 }
 
 start() {
 	checkconfig || return 1
 	ebegin "Starting vsftpd"
-	start-stop-daemon --start --quiet \
+	start-stop-daemon --start \
 		--exec /usr/sbin/vsftpd ${VSFTPD_CONF}
 	eend $?
 }
 
 stop() {
 	ebegin "Stopping vsftpd"
-	start-stop-daemon --stop --quiet --exec /usr/sbin/vsftpd
+	killall vsftpd
 	eend $?
 }
