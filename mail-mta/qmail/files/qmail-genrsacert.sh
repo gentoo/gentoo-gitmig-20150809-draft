39a525cd0c891cc682b3300fece06aeb3c68ef6f
Michael Hanselmann <hansmi@gentoo.org>
2005-06-12 19:35:43 +0000

    The cronjob now also generated the Diffie-Hellman Parameters. Package-Manager: portage-2.0.51.22-r1

--- mail-mta/qmail/files/qmail-genrsacert.sh
+++ mail-mta/qmail/files/qmail-genrsacert.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# $Header: /var/cvsroot/gentoo-x86/mail-mta/qmail/files/qmail-genrsacert.sh,v 1.3 2005/06/08 19:23:06 hansmi Exp $
+# $Header: /var/cvsroot/gentoo-x86/mail-mta/qmail/files/qmail-genrsacert.sh,v 1.4 2005/06/12 19:35:43 hansmi Exp $
 # Robin H. Johnson <robbat2@gentoo.org> - October 17, 2003
-# This file generates the static temporary RSA keys needed for qmail to encrypt messages
-# It should be run from a crontab, once a day is ok on low load machines, but
-# if you do lots of mail, once per hour is more reasonable
-# if you do NOT create the rsa512.pem, qmail will generate it on the fly for
-# each connection, which can be VERY slow.
+#
+# This file generates the static temporary RSA keys needed for qmail to encrypt
+# messages. It should be run from a crontab, once a day is ok on low load
+# machines, but if you do lots of mail, once per hour is more reasonable if you
+# do NOT create the rsa512.pem, qmail will generate it on the fly for each
+# connection, which can be VERY slow.
 
 if [ -z "${ROOT}" -o "${ROOT}" = "/" ]; then
 	confdir=/var/qmail/control
@@ -23,8 +24,7 @@ umask ${umaskvalue}
 
 # This is a list with bits of the generated keys. They should
 # be a power of 2 ideally and must be more than 64.
-# Sample: 128 256 512 1024
-keys="512"
+keys="512 1024"
 
 for bits in ${keys}
 do
@@ -35,4 +35,11 @@ do
 	/usr/bin/openssl genrsa -out ${tmpfile} ${bits} 2>/dev/null && \
 	/bin/chown ${uid}:${gid} ${tmpfile} && \
 	/bin/mv -f ${tmpfile} ${pemfile} || exit 1
+
+	dhfile="${confdir}/dh${bits}.pem"
+	dtmpfile="${confdir}/dh${bits}.pem.tmp"
+
+	/usr/bin/openssl dhparam -2 -out ${dtmpfile} ${bits} 2>/dev/null && \
+	/bin/chown ${uid}:${gid} ${dtmpfile} && \
+	/bin/mv -f ${dtmpfile} ${dhfile} || exit 1
 done
