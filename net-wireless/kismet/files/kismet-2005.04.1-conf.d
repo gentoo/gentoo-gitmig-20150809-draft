c355ebf0cb82bda1c90ec5a1aaeabf82fe541b62
Henrik Brix Andersen <brix@gentoo.org>
2005-06-13 10:38:41 +0000

    Fixed bug #94205 (init script fails to stop), bug #94387 (Add curses interface support in kismet) and bug #95539 (bad default configuration). Also updated the Ethereal version, fixed econf parameters and removed call to ifconfig from within the init script. Package-Manager: portage-2.0.51.22-r1

--- net-wireless/kismet/files/kismet-2005.04.1-conf.d
+++ net-wireless/kismet/files/kismet-2005.04.1-conf.d
@@ -1,27 +1,14 @@
-# Copyright 1999-2004 Gentoo Foundation
+# Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-wireless/kismet/files/kismet-2005.04.1-conf.d,v 1.1 2005/04/03 11:06:39 brix Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-wireless/kismet/files/kismet-2005.04.1-conf.d,v 1.2 2005/06/13 10:38:41 brix Exp $
 
-# Config file for kismet server
+# Kismet configuration is done in /etc/kismet.conf
 
-# ATTENTION: most of the kismet configuration is still done in
-# /etc/kismet.conf
 # To use the kismet init script, you must have "logtemplate" set to a location
 # that is writable by the user assigned by "suiduser".
 # e.g.
 # suiduser=foo
 # logtemplate=%h/kismet_log/%n-%d-%i.%l
 
-
-# Set WIFI_DEV to the device to be used by the kismet server.
-# This device must have the ability to do monitor mode
-
-
-WIFI_DEV=""
-
-# WIFI_DEV="wlan0"
-# WIFI_DEV="eth1"
-
-# Options to pass to the hopper/monitor/server
-KISMET_MONITOR_OPTS=""
-KISMET_SERVER_OPTS=""
+# Options to pass to kismet_server, see `kismet_server --help`
+KISMET_SERVER_OPTIONS=""
