0e8b37353617441f6d2990f1a895b2f00abd6382
Renat Lumpau <rl03@gentoo.org>
2005-06-21 11:09:49 +0000

    Fixed initscript. Package-Manager: portage-2.0.51.22-r1

--- www-apps/rt/files/3.4.2/rt.init.d
+++ www-apps/rt/files/3.4.2/rt.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/www-apps/rt/files/3.4.2/rt.init.d,v 1.1 2005/06/21 02:12:38 rl03 Exp $
+# $Header: /var/cvsroot/gentoo-x86/www-apps/rt/files/3.4.2/rt.init.d,v 1.2 2005/06/21 11:09:49 rl03 Exp $
 
 depend() {
 	use mysql postgresql lighttpd
@@ -14,16 +14,17 @@ start() {
 		/sbin/start-stop-daemon -o --quiet --start \
 		--startas ${RTPATH}/bin/mason_handler.fcgi \
 		--pidfile ${PIDFILE} -c ${RTUSER} -g ${RTGROUP} -b -d ${RTPATH}
-	# if you experience problems, comment out the line above
-	# and uncomment the line below
-	# --pidfile ${PIDFILE} -c ${RTUSER} -g ${RTGROUP}  -d ${RTPATH}
+	# mason_handler.fcgi doesn't bg itself, so have start-stop-demon do that
+	# if you experience problems, delete the -b flag above
+	# and check if you get any errors
 
-	eend $?
+	# make sure lighttpd can use the socket
 	while true; do
 		[[ -a ${FCGI_SOCKET_PATH} ]] && break
 		sleep 1
 	done
 	chmod g+rwx ${FCGI_SOCKET_PATH}
+	eend $?
 }
 
 stop() {
