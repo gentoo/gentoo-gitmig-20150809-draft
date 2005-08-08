ae0f89760036d36c6082bf59f9c577a88225fae4
Michael Cummings <mcummings@gentoo.org>
2005-08-08 09:38:32 +0000

    Corrected apache2-*.pl Package-Manager: portage-2.0.51.22-r1

--- www-apache/mod_perl/files/apache2-mod_perl-startup.pl
+++ www-apache/mod_perl/files/apache2-mod_perl-startup.pl
@@ -3,10 +3,7 @@ use Apache2 ();
 use lib qw(/home/httpd/perl);
 
 # enable if the mod_perl 1.0 compatibility is needed
-use Apache::compat ();
-# preload all mp2 modules
-use ModPerl::MethodLookup;
-ModPerl::MethodLookup::preload_all_modules();
+# #use Apache::compat ();
 
 use ModPerl::Util (); #for CORE::GLOBAL::exit
 
@@ -14,7 +11,7 @@ use Apache::RequestRec ();
 use Apache::RequestIO ();
 use Apache::RequestUtil ();
 
-use Apache::ServerRec ();
+use Apache::Server ();
 use Apache::ServerUtil ();
 use Apache::Connection ();
 use Apache::Log ();
@@ -25,17 +22,5 @@ use ModPerl::Registry ();
 
 use Apache::Const -compile => ':common';
 use APR::Const -compile => ':common';
-
-# end of mod_perl2 stuff
-
-#use Apache::Session ();
-use CGI ();
-use CGI::Cookie ();
-use DBI;
-#use Apache::Perldoc ();
-use Apache::Status ();
-use HTML::Mason ();
-use HTML::Mason::ApacheHandler ();
-use MasonX::Apache2Handler ();
 
 1;
