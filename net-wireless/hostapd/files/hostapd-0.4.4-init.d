ddcb6f0fa76625c985ff56a6c8b0452fceba30b1
Henrik Brix Andersen <brix@gentoo.org>
2005-08-22 20:35:18 +0000

    Install more example configuration files, use SIGHUP for reloading configuration in init script. Package-Manager: portage-2.0.51.22-r2

--- net-wireless/hostapd/files/hostapd-0.4.4-init.d
+++ net-wireless/hostapd/files/hostapd-0.4.4-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-wireless/hostapd/files/hostapd-0.4.4-init.d,v 1.1 2005/08/22 12:51:11 brix Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-wireless/hostapd/files/hostapd-0.4.4-init.d,v 1.2 2005/08/22 20:35:18 brix Exp $
 
 opts="start stop reload"
 
@@ -45,6 +45,6 @@ reload() {
 	checkconfig || return 1
 
 	ebegin "Reloading hostapd configuration"
-	start-stop-daemon --stop --signal 1 --quiet --exec /usr/sbin/hostapd
+	start-stop-daemon --stop --quiet --exec /usr/sbin/hostapd --signal HUP
 	eend ${?}
 }
