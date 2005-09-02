055f3292b08cdc6dc31cd3c61fe4678d944498a0
Henrik Brix Andersen <brix@gentoo.org>
2005-09-02 15:48:25 +0000

    Added 'use logger' to init script. Package-Manager: portage-2.0.51.22-r2

--- sys-power/cpufreqd/files/cpufreqd-2.0.0_beta4-init.d
+++ sys-power/cpufreqd/files/cpufreqd-2.0.0_beta4-init.d
@@ -1,12 +1,13 @@
 #!/sbin/runscript
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-power/cpufreqd/files/cpufreqd-2.0.0_beta4-init.d,v 1.1 2005/08/26 12:06:06 brix Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-power/cpufreqd/files/cpufreqd-2.0.0_beta4-init.d,v 1.2 2005/09/02 15:48:25 brix Exp $
 
 CONFIGFILE=/etc/cpufreqd.conf
 
 depend() {
 	need localmount
+	use logger
 }
 
 checkconfig() {
