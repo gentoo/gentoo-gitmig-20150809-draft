727880c772483ef5a6c3e3aa405f0d037b653402
Robin H. Johnson <robbat2@gentoo.org>
2005-09-23 05:55:28 +0000

    Add debug support, and improve init script to properly stop iscsid. Package-Manager: portage-2.0.51.22-r2

--- sys-block/open-iscsi/files/iscsid-init.d
+++ sys-block/open-iscsi/files/iscsid-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2005 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License, v2 or later
-# $Header: /var/cvsroot/gentoo-x86/sys-block/open-iscsi/files/iscsid-init.d,v 1.2 2005/09/20 08:00:38 robbat2 Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-block/open-iscsi/files/iscsid-init.d,v 1.3 2005/09/23 05:55:28 robbat2 Exp $
 
 PID_FILE=/var/run/iscsid.pid
 CONFIG_FILE=/etc/iscsid.conf
@@ -16,28 +16,35 @@ depend() {
 }
 
 checkconfig() {
-	[ ! -f $CONFIG_FILE ] && die "Config file $CONFIG_FILE does not exist!"
-	if [ ! -f $INITIATORNAME ]; then
+	if [ ! -f $CONFIG_FILE ]; then
+		eerror "Config file $CONFIG_FILE does not exist!"
+		return 1
+	fi
+	if [ ! -f $INITIATORNAME -o -z "$(egrep '^InitiatorName=' "${INITIATORNAME}")" ]; then
 		eerror "$INITIATORNAME should contain a string with your initiatior name, eg:"
-		errror "InitiatorName=iqn.2005-09.tld.domainname.hostname:initiator-name"
-		die "Initiator name file does not exist!"
+		eerror "InitiatorName=iqn.2005-09.tld.domainname.hostname:initiator-name"
+		eerror "Initiator name file does not exist!"
+		return 1
 	fi
 }
 
 start() {
-	checkconfig
-	ebegin "Starting iSCSI initiator service"
-	modprobe scsi_transport_iscsi
-	modprobe iscsi_tcp
-	start-stop-daemon --start --exec $DAEMON --quiet
+	checkconfig || return 1
+	ebegin "Loading iSCSI modules"
+	modprobe scsi_transport_iscsi && modprobe iscsi_tcp
 	ret=$?
 	eend $ret
 	[ $ret -gt 0 ] && return $ret
+	ebegin "Starting iSCSI initiator service"
+	start-stop-daemon --start --exec $DAEMON --quiet
+	ret=$?
+	eend $ret
+	return $ret
 }
 	
 stop() {
 	ebegin "Stopping iSCSI initiator service"
-	start-stop-daemon --stop --quiet --exec $DAEMON --pidfile $PID_FILE
+	start-stop-daemon --signal HUP --stop --quiet --exec $DAEMON #--pidfile $PID_FILE
 	eend $?
 
 	# ugly, but pid file is not removed by iscsid
@@ -48,7 +55,7 @@ stop() {
 	modprobe -r scsi_transport_iscsi
 	ret=$?
 	eend $ret
-	[ $ret -gt 0 ] && return $ret
+	return $ret
 }
 
 opts="${opts} dump"
