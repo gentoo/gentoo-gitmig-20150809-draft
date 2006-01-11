36683a65941840987ea88550ab0c33c8650a0729
Gunnar Wrobel <wrobel@gentoo.org>
2006-01-11 18:16:00 +0000

    Added the emacs muse-mode to portage. Package-Manager: portage-2.0.53

---
+++ app-emacs/muse/files/50muse-mode-gentoo.el
@@ -0,0 +1,14 @@
+
+;;; muse-mode site-lisp configuration
+
+
+(require 'muse-mode)     ; load authoring mode
+
+(require 'muse-project)
+
+(require 'muse-html)     ; load publishing styles I use
+(require 'muse-latex)
+(require 'muse-texinfo)
+(require 'muse-docbook)
+
+
