8b3985a219aea0cfd8cbc94db2be6b33a9146f56
Marcelo Góes <vanquirius@gentoo.org>
2006-01-15 18:34:36 +0000

    # Copyright 1999-2006 Gentoo Foundation Package-Manager: portage-2.0.53

--- net-analyzer/ipcad/files/ipcad.conf.d
+++ net-analyzer/ipcad/files/ipcad.conf.d
@@ -1,3 +1,7 @@
+# Copyright 1999-2006 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/net-analyzer/ipcad/files/ipcad.conf.d,v 1.2 2006/01/15 18:34:36 vanquirius Exp $
+
 # Config file for /etc/init.d/pptpd
 
 # Any extra options you want to pass to pptpd
