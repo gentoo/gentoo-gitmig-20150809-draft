76a499b3f99444865d609c7da881248d3198e50a
Mike Frysinger <vapier@gentoo.org>
2006-01-17 03:31:01 +0000

    dont copyright config files Package-Manager: portage-2.1_pre3-r1

--- net-ftp/vsftpd/files/vsftpd.conf.d
+++ net-ftp/vsftpd/files/vsftpd.conf.d
@@ -1,7 +1,3 @@
-# Copyright 2003-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License, v2
-# $Header: /var/cvsroot/gentoo-x86/net-ftp/vsftpd/files/vsftpd.conf.d,v 1.2 2004/07/14 23:44:02 agriffis Exp $
-
-# Config file for /etc/init.d/vsftpd
+# /etc/conf.d/vsftpd: config file for /etc/init.d/vsftpd
 
 VSFTPD_CONF=/etc/vsftpd/vsftpd.conf
