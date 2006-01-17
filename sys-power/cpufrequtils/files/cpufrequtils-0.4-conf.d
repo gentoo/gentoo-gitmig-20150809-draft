285e2ed7bc1f72d3ce1f1f59ef79eb224fa4496f
Mike Frysinger <vapier@gentoo.org>
2006-01-17 03:37:29 +0000

    dont copyright config files Package-Manager: portage-2.1_pre3-r1

--- sys-power/cpufrequtils/files/cpufrequtils-0.4-conf.d
+++ sys-power/cpufrequtils/files/cpufrequtils-0.4-conf.d
@@ -1,6 +1,4 @@
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-power/cpufrequtils/files/cpufrequtils-0.4-conf.d,v 1.1 2005/12/18 17:07:17 brix Exp $
+# /etc/conf.d/cpufrequtils: config file for /etc/init.d/cpufrequtils
 
 # Which governor to use. Must be one of the governors listed in:
 #   cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
