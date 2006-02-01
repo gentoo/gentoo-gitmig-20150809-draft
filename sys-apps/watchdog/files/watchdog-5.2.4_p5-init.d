41b6a2b3028a863277a52f21923e25768bbd02c3
Henrik Brix Andersen <brix@gentoo.org>
2006-02-01 15:37:43 +0000

    Add retry timeout of 5 seconds to stop() function in init script, bug #120044. Package-Manager: portage-2.0.54

--- sys-apps/watchdog/files/watchdog-5.2.4_p5-init.d
+++ sys-apps/watchdog/files/watchdog-5.2.4_p5-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
-# Copyright 1999-2005 Gentoo Foundation
+# Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-apps/watchdog/files/watchdog-5.2.4_p5-init.d,v 1.1 2005/11/27 10:14:24 brix Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-apps/watchdog/files/watchdog-5.2.4_p5-init.d,v 1.2 2006/02/01 15:37:43 brix Exp $
 
 depend() {
 	need localmount
@@ -17,6 +17,6 @@ start() {
 
 stop() {
 	ebegin "Stopping watchdog"
-	start-stop-daemon --stop --quiet --exec /usr/sbin/watchdog
+	start-stop-daemon --stop --retry 5 --quiet --exec /usr/sbin/watchdog
 	eend ${?}
 }
