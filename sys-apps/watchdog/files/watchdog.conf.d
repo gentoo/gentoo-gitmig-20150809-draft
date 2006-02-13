cf21bef880fe28040dd3733d3a1bb5cd935fb266
Henrik Brix Andersen <brix@gentoo.org>
2006-02-13 15:40:10 +0000

    Pruned old ebuild. Package-Manager: portage-2.0.54

--- sys-apps/watchdog/files/watchdog.conf.d
+++
@@ -1,6 +0,0 @@
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-apps/watchdog/files/watchdog.conf.d,v 1.1 2005/11/27 10:07:03 brix Exp $
-
-# for more info, see watchdog(8)
-WATCHDOG_OPTS=""
