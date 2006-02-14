b4216ae1235ab9923722f4005366243d59fb1774
Henrik Brix Andersen <brix@gentoo.org>
2006-02-14 13:34:50 +0000

    Do not use --quiet with start-stop-daemon. Package-Manager: portage-2.0.54

--- sys-power/cpufreqd/files/cpufreqd-2.0.0-init.d
+++ sys-power/cpufreqd/files/cpufreqd-2.0.0-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-power/cpufreqd/files/cpufreqd-2.0.0-init.d,v 1.1 2005/11/27 09:46:06 brix Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-power/cpufreqd/files/cpufreqd-2.0.0-init.d,v 1.2 2006/02/14 13:34:50 brix Exp $
 
 CONFIGFILE=/etc/cpufreqd.conf
 
@@ -26,13 +26,13 @@ start() {
 	checkconfig || return 1
 
 	ebegin "Starting CPU Frequency Daemon"
-	start-stop-daemon --start --quiet --exec /usr/sbin/cpufreqd -- \
+	start-stop-daemon --start --exec /usr/sbin/cpufreqd -- \
 		-f ${CONFIGFILE}
 	eend ${?}
 }
 
 stop() {
 	ebegin "Stopping CPU Frequency Daemon"
-	start-stop-daemon --stop --quiet --exec /usr/sbin/cpufreqd
+	start-stop-daemon --stop --exec /usr/sbin/cpufreqd
 	eend ${?}
 }
