de877baf41a28309819e15898ee77647573dfa63
Michael Sterrett <mr_bones_@gentoo.org>
2006-03-14 23:21:27 +0000

    armagetron has become armagetronad

--- games-action/armagetron/files/armagetron-0.2.6.1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! -e ~/.armagetron ]; then
-	mkdir ~/.armagetron{,/config,/log,/var}
-	ln -s "DATADIR"/config/{{default,aiplayers}.cfg,master.srv} ~/.armagetron/config/
-	cp "DATADIR"/config/settings.cfg ~/.armagetron/config/
-	ln -s "DATADIR"/{arenas,models,moviesounds,textures,language,moviepack,sound} ~/.armagetron/
-fi
-cd ~/.armagetron
-exec "LIBDIR"/armagetron "$@"
