78fdca3bf126fb5dd4929d04252c02bdc05b2687
Joerg Bornkessel <hd_brummy@gentoo.org>
2006-04-03 18:46:25 +0000

    version bump; fixed problem with spaces in *.iso file names Package-Manager: portage-2.0.54

--- media-tv/vdr-dvd-scripts/files/0.0.1/dvdchanger_writedvd_real.sh
+++ media-tv/vdr-dvd-scripts/files/0.0.1/dvdchanger_writedvd_real.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 #
-# source: http://www.hoochvdr.info
-#
-# $1 = filename of the iso file
+# $Header: /var/cvsroot/gentoo-x86/media-tv/vdr-dvd-scripts/files/0.0.1/dvdchanger_writedvd_real.sh,v 1.2 2006/04/03 18:46:25 hd_brummy Exp $
 #
 # 04 Mar 2006; Joerg Bornkessel <hd_brummy@gentoo.org>
 # addaptded to gentoo-vdr-scripts
 
 [[ -e /etc/conf.d/vdr.cd-dvd ]] && source /etc/conf.d/vdr.cd-dvd
 
+ISO_FILE=`echo ${@} | sed 's:\ IMAGE::g'`
+
+#logger -t burnscript burn ${ISO_FILE} --
+
 : ${VDR_DVDWRITER:=/dev/dvd}
 
 DVD_RECORDCMD="growisofs"
-DVDPLUS_RECORD_OPTS="-dvd-compat -Z"
+DVDPLUS_RECORD_OPTS="-use-the-force-luke=tty -dvd-compat -Z"
 
 unset SUDO_COMMAND
 svdrpsend.pl -d localhost "MESG DVD burn initiated"
-$DVD_RECORDCMD $DVDPLUS_RECORD_OPTS $VDR_DVDWRITER=$1
+$DVD_RECORDCMD $DVDPLUS_RECORD_OPTS $VDR_DVDWRITER="${ISO_FILE}"
 svdrpsend.pl -d localhost "MESG DVD burn completed"
