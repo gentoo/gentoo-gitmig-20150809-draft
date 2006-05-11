92db344ccc726c329373115915ebbe8ced064f43
Matthias Schwarzott <zzam@gentoo.org>
2006-05-11 19:24:58 +0000

    Added CVS-patch for compilation on amd64. Added installation of shared-memory-module. Package-Manager: portage-2.1_pre10-r5

---
+++ media-plugins/vdr-softdevice/files/rc-addon.sh
@@ -0,0 +1,16 @@
+# plugin-config-addon for softdevice
+plugin_pre_vdr_start() {
+	if [[ -n "${SOFTDEVICE_VIDEO_OUT}" ]]; then
+		add_plugin_param "-vo ${SOFTDEVICE_VIDEO_OUT}:${SOFTDEVICE_VIDEO_OUT_SUBOPTS}"
+	fi
+
+	if [[ -n "${SOFTDEVICE_AUDIO_OUT}" ]]; then
+		add_plugin_param "-ao ${SOFTDEVICE_AUDIO_OUT}:${SOFTDEVICE_AUDIO_OUT_SUBOPTS}"
+	fi
+}
+
+# for compatibility
+if [[ ${SCRIPT_API:-1} -lt 2 ]]; then
+        plugin_pre_vdr_start
+fi
+
