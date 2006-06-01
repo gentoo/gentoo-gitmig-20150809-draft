5577f4e631bfe93daee017a4bbd017b67e911a2a
Markus Ullmann <jokey@gentoo.org>
2006-06-01 21:13:58 +0000

    Adjusting conf.d file, thanks to Ricardo Cordeiro in bug #135085 Package-Manager: portage-2.1_rc1-r3

--- net-analyzer/ipcad/files/ipcad.conf.d
+++ net-analyzer/ipcad/files/ipcad.conf.d
@@ -1,9 +1,9 @@
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-analyzer/ipcad/files/ipcad.conf.d,v 1.2 2006/01/15 18:34:36 vanquirius Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-analyzer/ipcad/files/ipcad.conf.d,v 1.3 2006/06/01 21:13:58 jokey Exp $
 
-# Config file for /etc/init.d/pptpd
+# Config file for /etc/init.d/ipcad
 
-# Any extra options you want to pass to pptpd
+# Any extra options you want to pass to ipcad
 # on start-up should be put here.
 IPCAD_OPTS="-drs"
