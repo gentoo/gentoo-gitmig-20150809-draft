a1ace9ded442b15314819f11f12d948e58099054
Mike Frysinger <vapier@gentoo.org>
2006-07-01 00:48:30 +0000

    Use cooler bash syntax. Package-Manager: portage-2.1.1_pre1-r5

--- x11-apps/xinit/files/startDM.sh
+++ x11-apps/xinit/files/startDM.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Copyright 1999-2004 Gentoo Foundation
+# Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License, v2
+# $Header: /var/cvsroot/gentoo-x86/x11-apps/xinit/files/startDM.sh,v 1.2 2006/07/01 00:48:30 vapier Exp $
+#
 # Author: Martin Schlemmer <azarah@gentoo.org>
-# $Header: /var/cvsroot/gentoo-x86/x11-apps/xinit/files/startDM.sh,v 1.1 2005/08/14 00:12:39 spyderous Exp $
 
 source /etc/init.d/functions.sh
 
@@ -14,24 +15,21 @@ source /etc/profile
 # We enable this by default
 export GDK_USE_XFT=1
 
-if [ -e ${svcdir}/options/xdm/service ]
-then
+if [[ -e ${svcdir}/options/xdm/service ]] ; then
 	retval=0
-	EXE="`cat ${svcdir}/options/xdm/service`"
+	EXE=$(<"${svcdir}"/options/xdm/service)
 
 	/sbin/start-stop-daemon --start --quiet --exec ${EXE}
 	retval=$?
 	# Fix bug #26125 for slower systems
 	wait; sleep 2
-	
-	if [ "${retval}" -ne 0 ]
-	then
+
+	if [[ ${retval} -ne 0 ]] ; then
 		# there was a error running the DM
 		einfo "ERROR: could not start the Display Manager..."
 		# make sure we do not have a misbehaving DM
 		killall -9 ${EXE##*/}
 	fi
 fi
-
 
 # vim:ts=4
