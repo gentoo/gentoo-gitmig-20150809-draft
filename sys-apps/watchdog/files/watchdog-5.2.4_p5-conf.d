ed1f54c1ae2c8e2d7764a92d9cb2fe58096dbf7f
Christian Heim <phreak@gentoo.org>
2006-07-13 17:22:09 +0000

    cvs died in the middle of the last commit, redoing Package-Manager: portage-2.1.1_pre2-r8

--- sys-apps/watchdog/files/watchdog-5.2.4_p5-conf.d
+++
@@ -1,6 +0,0 @@
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-apps/watchdog/files/watchdog-5.2.4_p5-conf.d,v 1.1 2005/11/27 10:14:24 brix Exp $
-
-# for more info, see watchdog(8)
-WATCHDOG_OPTS=""
--- sys-apps/watchdog/files/watchdog-5.2.6_p1-conf.d
+++
@@ -1,6 +0,0 @@
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-apps/watchdog/files/watchdog-5.2.6_p1-conf.d,v 1.1 2006/06/25 10:50:21 brix Exp $
-
-# for more info, see watchdog(8)
-WATCHDOG_OPTS=""
