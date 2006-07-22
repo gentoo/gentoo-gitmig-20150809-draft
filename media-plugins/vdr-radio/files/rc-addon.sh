16688aa49f44b90de1deebaf2ababfc8e51ec27d
Joerg Bornkessel <hd_brummy@gentoo.org>
2006-07-22 19:57:21 +0000

    version bump; set maintainer gentoo-vdr-project; cleanup old version Package-Manager: portage-2.1-r1

--- media-plugins/vdr-radio/files/rc-addon.sh
+++ media-plugins/vdr-radio/files/rc-addon.sh
@@ -1,8 +1,10 @@
-# $Header: /var/cvsroot/gentoo-x86/media-plugins/vdr-radio/files/rc-addon.sh,v 1.1 2006/01/09 21:45:42 hd_brummy Exp $
+# $Header: /var/cvsroot/gentoo-x86/media-plugins/vdr-radio/files/rc-addon.sh,v 1.2 2006/07/22 19:57:21 hd_brummy Exp $
 #
 # rc-addon plugin-startup-skript for vdr-radio
 # 
+# This sript is called by gentoo-vdr-scripts on start of VDR
 
+# Set default DIR to the background picture
 RADIO_BACKGROUND_DIR=/usr/share/vdr/radio
 
 plugin_pre_vdr_start() {
