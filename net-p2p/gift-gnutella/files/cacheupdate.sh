7383eb110f93817242b16166e4113fef62326a95
Jon Hood <squinky86@gentoo.org>
2006-09-02 14:20:39 +0000

    Fix cache updating script to an active gnutella cache. Package-Manager: portage-2.1.1_rc1-r2

--- net-p2p/gift-gnutella/files/cacheupdate.sh
+++ net-p2p/gift-gnutella/files/cacheupdate.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-p2p/gift-gnutella/files/cacheupdate.sh,v 1.7 2004/11/27 18:21:00 squinky86 Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-p2p/gift-gnutella/files/cacheupdate.sh,v 1.8 2006/09/02 14:20:39 squinky86 Exp $
+
+CACHE=http://loot.alumnigroup.org/
 
 if [ -d ~/.giFT/Gnutella/ ]; then
 	cd ~/.giFT/Gnutella
-	wget http://crab.bishopston.net:3558/?urlfile=1\&client=GEN2\&version=0.2 -O gwebcaches.new || die "Unable to retrieve new caches."
+	wget ${CACHE}?urlfile=1\&client=GEN2\&version=0.2 -O gwebcaches.new || die "Unable to retrieve new caches."
 	if [ "`grep ERROR gwebcaches.new`" ]; then
 		cat gwebcaches.new
 	else
 		mv gwebcaches.new gwebcaches
 	fi
-	wget http://crab.bishopston.net:3558/?hostfile=1\&client=GEN2\&version=0.2 -O nodes.new || die "Unable to retrieve new hosts."
+	wget ${CACHE}?hostfile=1\&client=GEN2\&version=0.2 -O nodes.new || die "Unable to retrieve new hosts."
 	if [ "`grep ERROR nodes.new`" ]; then
 		cat nodes.new
 	else
