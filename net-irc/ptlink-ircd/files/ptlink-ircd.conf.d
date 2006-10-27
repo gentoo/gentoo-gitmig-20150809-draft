d7ecfbd44e83beb551ef278c58ae28a48eb0e228
Markus Ullmann <jokey@gentoo.org>
2006-10-27 07:11:58 +0000

    Version bump and cleanup Package-Manager: portage-2.1.2_pre3-r9

--- net-irc/ptlink-ircd/files/ptlink-ircd.conf.d
+++
@@ -1,6 +0,0 @@
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-irc/ptlink-ircd/files/ptlink-ircd.conf.d,v 1.2 2005/01/30 12:49:18 swegener Exp $
-
-# User to run ptlink-ircd as
-PTLINKIRCD_USER="ptlink-ircd"
