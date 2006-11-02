cc4db534d33a65b142b582ec7bbaf9d25406178b
Matthias Schwarzott <zzam@gentoo.org>
2006-11-02 09:13:22 +0000

    Removed older versions. Package-Manager: portage-2.1.2_rc1-r2

--- media-plugins/vdr-pin/files/rc-addon-0.0.12.sh
+++
@@ -1,8 +0,0 @@
-# $Header: /var/cvsroot/gentoo-x86/media-plugins/vdr-pin/files/rc-addon-0.0.12.sh,v 1.1 2006/05/05 22:38:03 hd_brummy Exp $
-#
-# rc-addon-script for plugin pin
-#
-
-# DIR should dont exist on plugin start
-
-[[ -e /etc/vdr/plugins/pin ]] && rm -r /etc/vdr/plugins/pin
