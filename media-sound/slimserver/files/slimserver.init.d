5840969a94a5d173dbd091df8e121cd23d8d5b15
Tom William Payne <twp@gentoo.org>
2006-11-13 19:41:57 +0000

    Initial commit.  Bug # 73832.

---
+++ media-sound/slimserver/files/slimserver.init.d
@@ -0,0 +1,43 @@
+#!/sbin/runscript
+# Copyright 1999-2006 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/media-sound/slimserver/files/slimserver.init.d,v 1.1 2006/11/13 19:41:57 twp Exp $
+
+SLIMSERVER_PIDFILE="/var/run/slimserver/slimserver.pid"
+
+depend() {
+	#need mysql
+	need net
+}
+
+start() {
+	local myopts=""
+	[[ -n "${SLIMSERVER_AUDIODIR}" ]] \
+		&& myopts="${myopts} --audiodir ${SLIMSERVER_AUDIODIR}"
+	[[ -n "${SLIMSERVER_PLAYLISTDIR}" ]] \
+		&& myopts="${myopts} --playlistdir ${SLIMSERVER_PLAYLISTDIR}"
+	ebegin "Starting slimserver"
+	start-stop-daemon --start \
+		--env HOME=/opt/slimserver \
+		--exec /opt/slimserver/slimserver.pl \
+		--pidfile ${SLIMSERVER_PIDFILE} \
+		-- \
+		--cachedir /var/cache/slimserver \
+		--daemon \
+		--group slimserver \
+		--logfile /var/log/slimserver \
+		--pidfile ${SLIMSERVER_PIDFILE} \
+		--prefsfile "${SLIMSERVER_PREFSFILE:-/etc/slimserver.pref}" \
+		--user slimserver \
+		--quiet \
+		${myopts} ${SLIMSERVER_OPTS}
+	eend $?
+}
+
+stop() {
+	ebegin "Stopping slimserver"
+	start-stop-daemon --stop \
+		--quiet \
+		--pidfile ${SLIMSERVER_PIDFILE}
+	eend $?
+}
