449315dc1681e9ec4203b5ee1137216f1309cf76
Renat Lumpau <rl03@gentoo.org>
2006-11-21 22:36:05 +0000

    Housekeeping Package-Manager: portage-2.1.2_rc1-r5

--- www-misc/zoneminder/files/init.d
+++ www-misc/zoneminder/files/init.d
@@ -1,21 +1,16 @@
 #!/sbin/runscript
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/www-misc/zoneminder/files/init.d,v 1.1 2006/02/06 22:02:52 rl03 Exp $
-
-depend() {
-    need mysql
-    use net
-}
+# $Header: /var/cvsroot/gentoo-x86/www-misc/zoneminder/files/init.d,v 1.2 2006/11/21 22:36:05 rl03 Exp $
 
 start() {
 	ebegin "Starting zoneminder"
-	eval ${CMD_START}
+	${CMD_START}
 	eend $?
 }
 
 stop() {
 	ebegin "Stopping zoneminder"
-	eval ${CMD_STOP}
+	${CMD_STOP}
 	eend $?
 }
