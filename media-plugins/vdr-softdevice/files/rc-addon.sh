824fcd97282992e196afd9c111054e6682323236
Matthias Schwarzott <zzam@gentoo.org>
2006-11-23 12:44:17 +0000

    No longer kill ShmClient at vdr-restart, as it seems to work now without that. Package-Manager: portage-2.1.2_rc2-r1

--- media-plugins/vdr-softdevice/files/rc-addon.sh
+++ media-plugins/vdr-softdevice/files/rc-addon.sh
@@ -8,15 +8,3 @@ plugin_pre_vdr_start() {
 		add_plugin_param "-ao ${SOFTDEVICE_AUDIO_OUT}:${SOFTDEVICE_AUDIO_OUT_SUBOPTS}"
 	fi
 }
-
-plugin_pre_vdr_stop() {
-	if [[ ${SOFTDEVICE_VIDEO_OUT} == "shm" ]]; then
-		killall ShmClient 2>/dev/null
-	fi
-}
-
-# for compatibility
-if [[ ${SCRIPT_API:-1} -lt 2 ]]; then
-        plugin_pre_vdr_start
-fi
-
