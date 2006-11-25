76ffd852f76786a059239567f4d8c08fd9c7b535
Matthew Kennedy <mkennedy@gentoo.org>
2006-11-25 20:18:32 +0000

    Include autoloads in the site-init. Package-Manager: portage-2.1.2_rc2-r1

--- app-emacs/ebuild-mode/files/50ebuild-mode-gentoo.el
+++ app-emacs/ebuild-mode/files/50ebuild-mode-gentoo.el
@@ -2,6 +2,9 @@
 
 (add-to-list 'load-path "@SITELISP@")
 
+(autoload 'ebuild-mode "ebuild-mode" "Major mode for Portage .ebuild and .eclass files." t)
+(autoload 'eselect-mode "ebuild-mode" "Major mode for Portage .eselect files." t)
+
 (add-to-list 'auto-mode-alist '("\\.ebuild\\'" . ebuild-mode))
 (add-to-list 'auto-mode-alist '("\\.eclass\\'" . ebuild-mode))
 (add-to-list 'auto-mode-alist '("\\.eselect\\'" . eselect-mode))
