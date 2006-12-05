63d5e1b928938b52ffdcd85e639361a136f2089e
Fabian Groffen <grobian@gentoo.org>
2006-12-05 20:03:29 +0000

    Set Nesedah theme as default in the config script to avoid bugs like #154159 Package-Manager: portage-2.1.1-r2

--- gnustep-libs/camaelon/files/config-camaelon.sh
+++ gnustep-libs/camaelon/files/config-camaelon.sh
@@ -1,4 +1,5 @@
 echo "Applying Camaelon default theme..."
 echo "defaults write NSGlobalDomain GSAppKitUserBundles \"(${GNUSTEP_LOCAL_ROOT}/Library/Bundles/Camaelon.themeEngine)\""
 defaults write NSGlobalDomain GSAppKitUserBundles "(${GNUSTEP_LOCAL_ROOT}/Library/Bundles/Camaelon.themeEngine)"
-
+echo "defaults write Camaelon Theme Nesedah"
+defaults write Camaelon Theme Nesedah
