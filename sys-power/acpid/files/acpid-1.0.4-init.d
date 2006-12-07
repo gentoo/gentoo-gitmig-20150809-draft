2a1e076879ea853bbe2347bd2f2c34864d2d3d0d
Roy Marples <uberlord@gentoo.org>
2006-12-07 00:08:44 +0000

    Make acpid more robust for start-stop-daemon. Package-Manager: portage-2.1.2_rc2-r5

--- sys-power/acpid/files/acpid-1.0.4-init.d
+++ sys-power/acpid/files/acpid-1.0.4-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-power/acpid/files/acpid-1.0.4-init.d,v 1.4 2006/08/20 04:16:44 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-power/acpid/files/acpid-1.0.4-init.d,v 1.5 2006/12/07 00:08:44 uberlord Exp $
 
 opts="reload"
 
@@ -20,7 +20,7 @@ start() {
 	checkconfig || return 1
 
 	ebegin "Starting acpid"
-	acpid ${ACPID_OPTIONS}
+	start-stop-daemon --start --exec /usr/sbin/acpid -- ${ACPID_OPTIONS}
 	eend $?
 }
 
@@ -32,6 +32,6 @@ stop() {
 
 reload() {
 	ebegin "Reloading acpid configuration"
-	killall -HUP acpid
+	start-stop-daemon --stop --oknodo --exec /usr/sbin/acpid --signal HUP
 	eend $?
 }
