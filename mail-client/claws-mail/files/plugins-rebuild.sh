4fd0a4cfddcacaae8dc1323c4942a7078f938ce1
Andrej Kacian <ticho@gentoo.org>
2006-12-19 18:30:06 +0000

    Remove old plugins before merging new to prevent collision-protect issues. Also, do not merge new plugins again after merging them in place of old ones. Package-Manager: portage-2.1.2_rc3-r5

--- mail-client/claws-mail/files/plugins-rebuild.sh
+++ mail-client/claws-mail/files/plugins-rebuild.sh
@@ -1,6 +1,6 @@
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/mail-client/claws-mail/files/plugins-rebuild.sh,v 1.1 2006/12/08 11:07:55 genone Exp $
+# $Header: /var/cvsroot/gentoo-x86/mail-client/claws-mail/files/plugins-rebuild.sh,v 1.2 2006/12/19 18:30:06 ticho Exp $
 #!/bin/bash
 
 if [ -d ${ROOT}/var/db/pkg ]; then
@@ -17,11 +17,15 @@ if ls -d mail-client/sylpheed-claws-[a-z]* &> /dev/null; then
 	echo "Found old plugins for rename:"
 	echo "${OLDPLUGINS}"
 	echo
+	echo "Unmerging old plugins to prevent file collisions..."
+	echo
+	emerge -C $* ${OLDPLUGINS}
+	echo
 	echo "Installing renamed versions with given emerge options: $*"
+	echo
 	sleep 2
 	emerge $* ${OLDPLUGINS//sylpheed-claws/claws-mail}
-fi
-if ls -d mail-client/claws-mail-[a-z]* &> /dev/null; then
+elif ls -d mail-client/claws-mail-[a-z]* &> /dev/null; then
 	echo "Looking for claws-mail plugins to rebuild ..."
 	PLUGINS=$(for d in mail-client/claws-mail-[a-z]*; do /usr/lib/portage/bin/pkgname $d | cut -d' ' -f 1; done)
 	echo
