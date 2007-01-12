c450d1cc2d648456add0e587e47f75b11ccfd99b
Raúl Porcel <armin76@gentoo.org>
2007-01-12 14:23:20 +0000

    Remove broken ebuilds, version bump, fix bug 119416 Package-Manager: portage-2.1.1-r2

--- net-p2p/phex/files/phex-2.6.4.89.sh
+++
@@ -1,7 +0,0 @@
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-p2p/phex/files/phex-2.6.4.89.sh,v 1.1 2005/09/26 21:09:27 mkay Exp $
-
-#!/bin/sh
-
-java -classpath $(java-config -p commons-logging,commons-httpclient-3,phex,jgoodies-looks-1.3,jgoodies-forms) phex.Main
