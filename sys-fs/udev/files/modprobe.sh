645c11a476656846ab2f48a3cbf55721b57aadab
Matthias Schwarzott <zzam@gentoo.org>
2007-02-10 22:28:14 +0000

    Does not call /etc/init.d/net.IFACE for network devices which are registed after being up (like ppp, isdn), solving bug #165936. Added modprobe wrapper to make all modules blacklistable, Bug #130766, and to give console-output about loading modules. Package-Manager: portage-2.1.2-r9

---
+++ sys-fs/udev/files/modprobe.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+VERBOSE=no
+[[ -e /dev/.udev_populate ]] && VERBOSE=yes
+
+source /sbin/functions.sh </dev/console
+
+MODPROBE=/sbin/modprobe
+MODLIST=$("${MODPROBE}" -i --show-depends "${@}" | sed "s#^insmod /lib.*/\(.*\)\.ko.*#\1#g" | sed 's|-|_|g')
+
+[[ -z ${MODLIST} ]] && exit 0
+for m in ${MODLIST}; do
+	LAST=$m
+done
+
+# check for blacklisting
+if [[ -f /etc/modprobe.conf ]]; then
+	if grep -q 'blacklist.*[[:space:]]'"${LAST}"'\([[:space:]]\|$\)' /etc/modprobe.conf; then
+		# module blacklisted
+		exit 0
+	fi
+fi
+
+if [[ ${VERBOSE} != no ]]; then
+	# print line if not already loaded
+	if ! fgrep -w -q ${LAST} /proc/modules; then
+		einfo "  udev loading module ${LAST}" > /dev/console
+	fi
+fi
+
+# now do real loading
+exec "${MODPROBE}" "${@}"
