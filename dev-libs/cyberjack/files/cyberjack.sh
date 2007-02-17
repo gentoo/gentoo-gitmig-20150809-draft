e1c7f255c6c77faee0c519f42fec1bf4ddcd811f
Wolfram Schlich <wschlich@gentoo.org>
2007-02-17 23:44:33 +0000

    add hack for >=dev-libs/libusb-0.1.12 to udev script Package-Manager: portage-2.1.2-r9

--- dev-libs/cyberjack/files/cyberjack.sh
+++ dev-libs/cyberjack/files/cyberjack.sh
@@ -4,14 +4,24 @@ dev_mode=0664
 
 # using the cyberJack libusb driver
 if [ -n "${DEVICE}" ]; then
-  dev="${DEVICE}"
+	dev="${DEVICE}"
+	# hack for >=dev-libs/libusb-0.1.12
+	# which uses /dev/bus/usb/NNN/MMM
+	# instead of /proc/bus/usb/NNN/MMM
+	case "${dev}" in
+		/proc/bus/usb/*)
+			dev="${dev} ${dev/proc/dev}"
+			;;
+	esac
 fi
 
 # using the cyberJack kernel module
 if [ -n "${DEVNAME}" ]; then
-  dev="${DEVNAME}"
+	dev="${DEVNAME}"
 fi
 
 # set device group and permissions
-/bin/chgrp "${dev_group}" "${dev}"
-/bin/chmod "${dev_mode}" "${dev}"
+for d in ${dev}; do
+	/bin/chgrp "${dev_group}" "${d}"
+	/bin/chmod "${dev_mode}" "${d}"
+done
