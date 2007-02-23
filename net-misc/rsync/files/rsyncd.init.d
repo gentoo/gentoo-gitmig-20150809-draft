6a90a0a486dc64c2c351b7ac5d67ffd988609251
Roy Marples <uberlord@gentoo.org>
2007-02-23 11:33:59 +0000

    Use start-stop-daemon correctly. Package-Manager: portage-2.1.2-r10

--- net-misc/rsync/files/rsyncd.init.d
+++ net-misc/rsync/files/rsyncd.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/rsync/files/rsyncd.init.d,v 1.4 2004/10/21 01:10:48 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-misc/rsync/files/rsyncd.init.d,v 1.5 2007/02/23 11:33:59 uberlord Exp $
 
 depend() {
 	use net
@@ -9,12 +9,15 @@ depend() {
 
 start() {
 	ebegin "Starting rsyncd"
-	rsync --daemon ${RSYNC_OPTS}
+	start-stop-daemon --start --exec /usr/bin/rsync \
+	    --pidfile /var/run/rsyncd.pid \
+	    -- --daemon ${RSYNC_OPTS}
 	eend $?
 }
 
 stop() {
 	ebegin "Stopping rsyncd"
-	start-stop-daemon --stop --pidfile /var/run/rsyncd.pid
+	start-stop-daemon --stop --exec /usr/bin/rsync \
+	    --pidfile /var/run/rsyncd.pid
 	eend $?
 }
