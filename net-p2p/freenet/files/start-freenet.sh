89191812f2f1f9ccfa865d464e5bf6072bd385f9
Raúl Porcel <armin76@gentoo.org>
2007-03-28 09:13:55 +0000

    Remove, bug 72996

--- net-p2p/freenet/files/start-freenet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script is a companion script to the Gentoo freenet init script.
-# Logs freenet's stdout and stderr for debugging needs.
-#
-# Author: Brandon Low <lostlogic@gentoo.org>
-#
-${JAVA} ${JAVA_ARGS} freenet.node.Main -p /etc/freenet.conf \
-	> /var/freenet/freenet.stdout.log 2> /var/freenet/freenet.stderr.log &
-echo $!
