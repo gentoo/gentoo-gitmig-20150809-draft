9663c47839244e45bd0be9e8185175c8535b8961
Ulrich Müller <ulm@gentoo.org>
2007-04-23 15:49:17 +0000

    Autoload files. Redundant pkg_postinst and pkg_postrm removed. Package-Manager: portage-2.1.2.4

--- app-emacs/remember/files/50remember-gentoo.el
+++ app-emacs/remember/files/50remember-gentoo.el
@@ -1,4 +1,11 @@
 
-;;; remember configuation
+;;; remember site-lisp configuation
+
 (add-to-list 'load-path "@SITELISP@")
-(require 'remember)
+
+(autoload 'remember "remember" "Remember an arbitrary piece of data." t)
+(autoload 'remember-region "remember" "Remember the data from BEG to END." t)
+(autoload 'remember-clipboard "remember"
+  "Remember the contents of the current clipboard." t)
+(autoload 'remember-buffer "remember"
+  "Remember the contents of the current buffer." t)
