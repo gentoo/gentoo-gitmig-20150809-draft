c4868f2d8acc9b6973ee1d901492a04e04a2e723
Ulrich Müller <ulm@gentoo.org>
2007-04-27 19:28:36 +0000

    Use autoload for functions. Package-Manager: portage-2.1.2.4

--- app-emacs/u-vm-color/files/51u-vm-color-gentoo.el
+++ app-emacs/u-vm-color/files/51u-vm-color-gentoo.el
@@ -2,7 +2,10 @@
 ;;; u-vm-color site-lisp configuration
 
 (add-to-list 'load-path "@SITELISP@")
-(require 'u-vm-color)
+(autoload 'u-vm-color-summary-mode "u-vm-color"
+  "Configure `font-lock-keywords' and add some hooks for vm-buffers." t)
+(autoload 'u-vm-color-fontify-buffer "u-vm-color" "Fontifies mail-buffers." t)
+(autoload 'u-vm-color-fontify-buffer-even-more "u-vm-color")
+
 (add-hook 'vm-summary-mode-hook 'u-vm-color-summary-mode)
 (add-hook 'vm-select-message-hook 'u-vm-color-fontify-buffer)
-
