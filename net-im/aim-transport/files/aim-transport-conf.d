384f26dab23ff7a2d00a5280d7342648eb0fa8f9
Raúl Porcel <armin76@gentoo.org>
2007-05-18 13:50:07 +0000

    Remove, bug 145858

--- net-im/aim-transport/files/aim-transport-conf.d
+++
@@ -1,9 +0,0 @@
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License, v2 or later
-# $Header: /var/cvsroot/gentoo-x86/net-im/aim-transport/files/aim-transport-conf.d,v 1.1 2004/07/09 17:50:01 humpback Exp $
-
-CONFIG="/etc/jabber/aimtrans.xml"
-
-#Need to find a way to fix the "double" expansion 
-#PIDFILE= grep pid ${CONFIG} | sed -e 's/<[^>]*>//g' | sed s/' '//g
-PIDFILE="/var/log/jabber/aim.pid"
