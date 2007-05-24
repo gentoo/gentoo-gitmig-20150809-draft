115c2d848eed85474b326efcf05719083e089b8f
Christian Faulhammer <fauli@gentoo.org>
2007-05-24 06:06:26 +0000

    remove

--- app-emacs/cdi/files/50cdi-gentoo.el
+++
@@ -1,5 +0,0 @@
-
-;;; cdi site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'cdi-start "cdi" "Setup the cdi buffer and perhaps start a daemon." t)
--- app-emacs/erc-cvs/files/50erc-cvs-gentoo.el
+++
@@ -1,5 +0,0 @@
-
-;;; erc site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(require 'erc-auto)
