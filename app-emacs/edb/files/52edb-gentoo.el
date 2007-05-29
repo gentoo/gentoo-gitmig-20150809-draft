8af69fc2118ad9d527e3a0cecb55f73524642a9b
Ulrich Müller <ulm@gentoo.org>
2007-05-29 20:14:28 +0000

    Version bump. Package-Manager: portage-2.1.2.7

--- app-emacs/edb/files/52edb-gentoo.el
+++ app-emacs/edb/files/52edb-gentoo.el
@@ -3,4 +3,5 @@
 
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'db-find-file "database" "EDB database package" t)
-(autoload 'edb-EXPERIMENTAL-interact "database" "EDB database package" t)
+(autoload 'edb-interact "database" "EDB database package" t)
+(defalias 'edb-EXPERIMENTAL-interact 'edb-interact)
