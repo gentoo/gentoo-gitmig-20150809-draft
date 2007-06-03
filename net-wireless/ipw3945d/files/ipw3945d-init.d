d7f19d3d81d96aecc479857ef666382e41f91949
Doug Goldstein <cardoe@gentoo.org>
2007-06-03 05:19:35 +0000

    s-s-d start was wrong. Package-Manager: portage-2.1.2.9

--- net-wireless/ipw3945d/files/ipw3945d-init.d
+++ net-wireless/ipw3945d/files/ipw3945d-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-wireless/ipw3945d/files/ipw3945d-init.d,v 1.4 2006/12/22 10:11:26 phreak Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-wireless/ipw3945d/files/ipw3945d-init.d,v 1.5 2007/06/03 05:19:35 cardoe Exp $
 
 PIDFILE=/var/run/ipw3945d/ipw3945d.pid
 
@@ -23,7 +23,7 @@ start() {
 	ebegin "Starting ipw3945d"
 	chown ipw3945d /sys/bus/pci/drivers/ipw3945/00*/cmd
 	chmod a-w,u+rw /sys/bus/pci/drivers/ipw3945/00*/cmd
-	start-stop-daemon --start --exec /sbin/ipw3945d --pidfile ${PIDFILE} -- \
+	start-stop-daemon --start --exec /sbin/ipw3945d -- \
 		--pid-file=${PIDFILE} ${ARGS}
 	eend ${?}
 }
