e0dff6698cc577130906e77b7db83348edbbc9c5
Ulrich Müller <ulm@gentoo.org>
2007-06-27 07:25:47 +0000

    Disable rst-mode-lazy in case font-lock doesn't support it. Package-Manager: portage-2.1.3_rc5

--- app-emacs/rst/files/50rst-gentoo.el
+++ app-emacs/rst/files/50rst-gentoo.el
@@ -4,3 +4,7 @@
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'rst-mode "rst" "mode for editing reStructuredText documents" t)
 (add-to-list 'auto-mode-alist '("\\.re?st\\'" . rst-mode))
+
+;; disable rst-mode-lazy in case font-lock doesn't support it
+(or (fboundp 'lazy-lock-mode)
+    (setq rst-mode-lazy nil))
