10afc8053675cf207a79234b07235b670c9543a9
Christian Faulhammer <fauli@gentoo.org>
2007-07-04 09:29:29 +0000

    don't create autoloads as they are not sufficient; instead load doxymacs when a C mode is entered; kill the font-lock in the site-init; fixes bug 184175, reported by Anders Bach Nielsen <abachn@daimi.au.dk> Package-Manager: portage-2.1.2.9

--- app-emacs/doxymacs/files/50doxymacs-gentoo.el
+++ app-emacs/doxymacs/files/50doxymacs-gentoo.el
@@ -1,11 +1,5 @@
-
 ;;; doxymacs site-lisp configuration
 
 (add-to-list 'load-path "@SITELISP@")
-(load "doxymacs-autoloads" nil t)
-(add-hook 'c-mode-common-hook 'doxymacs-mode)
 
-(defun my-doxymacs-font-lock-hook ()
-  (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
-      (doxymacs-font-lock)))
-(add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)
+(add-hook 'c-mode-common-hook (load "doxymacs" nil t))
