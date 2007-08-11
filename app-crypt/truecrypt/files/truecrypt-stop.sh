4372f090b374be95a439e688a2a21f5d01ee9814
Alon Bar-Lev <alonbl@gentoo.org>
2007-08-11 08:47:21 +0000

    Don't fail if no volumes are mounted, bug#168691, thanks to Jakob Truelsen Package-Manager: portage-2.1.3.3

--- app-crypt/truecrypt/files/truecrypt-stop.sh
+++ app-crypt/truecrypt/files/truecrypt-stop.sh
@@ -4,6 +4,6 @@
 # Try to remove any dm-crypt mappings
 if [ -x /usr/bin/truecrypt ]; then
 	ebegin "Removing truecrypt mappings"
-	/usr/bin/truecrypt -d
+	! /usr/bin/truecrypt -l > /dev/null 2>&1  || /usr/bin/truecrypt -d
 	eend $?
 fi
