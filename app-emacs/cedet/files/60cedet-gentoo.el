5828627ae099b4114f86e2f1ff4bec7cb6697500
Ulrich Müller <ulm@gentoo.org>
2007-09-02 20:11:16 +0000

    Suppress startup message. Package-Manager: portage-2.1.3.7

--- app-emacs/cedet/files/60cedet-gentoo.el
+++ app-emacs/cedet/files/60cedet-gentoo.el
@@ -1,7 +1,7 @@
 
 ;;; cedet site-lisp configuration
 
-(load "@SITELISP@/common/cedet")
+(load "@SITELISP@/common/cedet" nil t)
 
 ;; If you wish to customize CEDET, you will need to follow the
 ;; directions in the INSTALL (installed in the documentation) file and
