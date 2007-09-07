e159cc9113d4b67f0e01b0dcadf258146bd972ee
Gunnar Wrobel <wrobel@gentoo.org>
2007-09-07 14:06:55 +0000

    Removed some obsolete files. Package-Manager: portage-2.1.2.12

--- www-apps/knowledgetree/files/config-hook-2.0.0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Sets installation directory and hostname when installing knowledgeTree
-
-if [ "x$1"="xinstall" ]; then
-    cd ${MY_INSTALLDIR}/config
-    sed -i -e "s#default->rootUrl  = \"\"#default->rootUrl  = \"${VHOST_APPDIR}\"#" environment.php
-fi
--- www-apps/knowledgetree/files/config-hook-3.0.3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Sets installation directory and hostname when installing knowledgeTree
-
-if [ $1 = "install" ]; then
-    cd ${MY_INSTALLDIR}/config
-    sed -i -e "s#rootUrl = default#rootUrl = \"${VHOST_APPDIR}\"#" config.ini
-fi
