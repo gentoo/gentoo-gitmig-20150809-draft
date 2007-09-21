07a0f8024237201eb1d0c2fe29e958d1c2811d91
Ulrich Müller <ulm@gentoo.org>
2007-09-21 09:15:45 +0000

    Move binding of keys to w3m-mode-hook. Fixes bug #193287. Package-Manager: portage-2.1.3.9

--- app-emacs/w3mnav/files/75w3mnav-gentoo.el
+++ app-emacs/w3mnav/files/75w3mnav-gentoo.el
@@ -7,7 +7,9 @@
 (autoload 'w3m-nav-go-prev "w3mnav" nil t)
 (autoload 'w3m-nav-go-next "w3mnav" nil t)
 
-(setq w3m-mode-map w3m-info-like-map)
-(define-key w3m-mode-map "t" 'w3m-nav-go-top)
-(define-key w3m-mode-map "[" 'w3m-nav-go-prev)
-(define-key w3m-mode-map "]" 'w3m-nav-go-next)
+(add-hook 'w3m-mode-hook
+	  (lambda ()
+	    (setq w3m-mode-map w3m-info-like-map)
+	    (define-key w3m-mode-map "t" 'w3m-nav-go-top)
+	    (define-key w3m-mode-map "[" 'w3m-nav-go-prev)
+	    (define-key w3m-mode-map "]" 'w3m-nav-go-next)))
--- app-emacs/w3mnav/w3mnav-0.5-r3.ebuild
+++ app-emacs/w3mnav/w3mnav-0.5-r3.ebuild
@@ -1,6 +1,6 @@
 # Copyright 1999-2007 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/app-emacs/w3mnav/w3mnav-0.5-r2.ebuild,v 1.1 2007/09/20 23:06:43 ulm Exp $
+# $Header: /var/cvsroot/gentoo-x86/app-emacs/w3mnav/w3mnav-0.5-r3.ebuild,v 1.1 2007/09/21 09:15:45 ulm Exp $
 
 inherit elisp
 
