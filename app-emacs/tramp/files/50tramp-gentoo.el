5617a35ff787afa86db50d24a08f745e2820ea1f
Ulrich Müller <ulm@gentoo.org>
2007-09-26 13:13:22 +0000

    Remove old versions. Package-Manager: portage-2.1.3.9

--- app-emacs/tramp/files/50tramp-gentoo.el
+++
@@ -1,8 +0,0 @@
-
-;;; tramp site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(setq auto-save-file-name-transforms
-      `(("\\`/[^/]*:\\(.+/\\)*\\(.*\\)"
-	 ,(expand-file-name "\\2" temporary-file-directory))))
-(require 'tramp)
