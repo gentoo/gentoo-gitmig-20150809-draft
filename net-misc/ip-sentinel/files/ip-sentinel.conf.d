928c379eda9dd1a6cf6af0518f69c6c6e5257a39
Peter Volkov <pva@gentoo.org>
2007-10-04 08:50:41 +0000

    Fixed typo. Package-Manager: portage-2.1.3.11

--- net-misc/ip-sentinel/files/ip-sentinel.conf.d
+++ net-misc/ip-sentinel/files/ip-sentinel.conf.d
@@ -4,7 +4,7 @@ IFACE="eth0"
 # Insert any other options needed
 OPTS=""
 
-# If you wish to run dhcp in a chroot, run:
+# If you wish to run ip-sentinel in a chroot, run:
 # emerge --config =net-misc/<ip-sentinel-version>
 # and un-comment the following line.
 # You can specify a different chroot directory but MAKE SURE it's empty.
