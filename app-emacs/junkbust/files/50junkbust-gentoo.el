4bc7ef55277e786d62652e05661b2a1721799cad
Christian Faulhammer <fauli@gentoo.org>
2007-10-19 21:51:19 +0000

    punt

--- app-emacs/junkbust/files/50junkbust-gentoo.el
+++
@@ -1,8 +0,0 @@
-
-;;; junkbust site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(require 'junkbust)
-(global-set-key   [f6] 'junkbust-block-url-edit-rule)
-(global-set-key [S-f6] 'junkbust-block-url-edit-url)
-(global-set-key [C-f6] 'junkbust-blocklist-file-visit)
