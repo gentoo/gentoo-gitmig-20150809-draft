0cb1826aa4e4b07100e5a42306e1fbdac2a2afec
Roy Marples <uberlord@gentoo.org>
2007-11-01 01:59:20 +0000

    Bump for a fixed init script, #196647 Package-Manager: portage-2.1.3.16 RepoMan-Options: --force

--- net-wireless/ipw3945d/files/ipw3945d-init.d
+++ net-wireless/ipw3945d/files/ipw3945d-init.d
@@ -1,12 +1,13 @@
 #!/sbin/runscript
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-wireless/ipw3945d/files/ipw3945d-init.d,v 1.5 2007/06/03 05:19:35 cardoe Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-wireless/ipw3945d/files/ipw3945d-init.d,v 1.6 2007/11/01 01:59:20 uberlord Exp $
 
 PIDFILE=/var/run/ipw3945d/ipw3945d.pid
 
 depend() {
 	before net
+	need localmount
 }
 
 check() {
@@ -23,7 +24,7 @@ start() {
 	ebegin "Starting ipw3945d"
 	chown ipw3945d /sys/bus/pci/drivers/ipw3945/00*/cmd
 	chmod a-w,u+rw /sys/bus/pci/drivers/ipw3945/00*/cmd
-	start-stop-daemon --start --exec /sbin/ipw3945d -- \
+	start-stop-daemon --start --exec /sbin/ipw3945d --pidfile ${PIDFILE} -- \
 		--pid-file=${PIDFILE} ${ARGS}
 	eend ${?}
 }
