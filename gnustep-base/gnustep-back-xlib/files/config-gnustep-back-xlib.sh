7ef78da4b448e8bc951f8bc5d94e463c967b0b45
Bernard Cafarelli <voyageur@gentoo.org>
2007-12-08 20:11:59 +0000

    Remove old versions Package-Manager: portage-2.1.4_rc9

--- gnustep-base/gnustep-back-xlib/files/config-gnustep-back-xlib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Switching to libgnustep-xlib backend ..."
-echo "defaults write NSGlobalDomain GSBackend libgnustep-xlib"
-defaults write NSGlobalDomain GSBackend libgnustep-xlib
-
