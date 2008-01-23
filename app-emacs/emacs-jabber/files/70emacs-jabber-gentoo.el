7752271617c62550b7dbdb14f7c2d7b162207d18
Ulrich Müller <ulm@gentoo.org>
2008-01-23 10:58:42 +0000

    Change autoload for jabber-connect to fix void variable errors. Remove old. Package-Manager: portage-2.1.4

--- app-emacs/emacs-jabber/files/70emacs-jabber-gentoo.el
+++ app-emacs/emacs-jabber/files/70emacs-jabber-gentoo.el
@@ -3,5 +3,5 @@
 
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'jabber-customize "jabber" "customize jabber options" t)
-(autoload 'jabber-connect "jabber-core"
+(autoload 'jabber-connect "jabber"
   "connect to the jabber server and start a jabber xml stream" t)
