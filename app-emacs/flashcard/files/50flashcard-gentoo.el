bd4fcfb6edbc617c2841e6e36ed527f707a0b462
Ulrich Müller <ulm@gentoo.org>
2008-02-17 16:08:26 +0000

    Fix site-init file. Package-Manager: portage-2.1.4.4

--- app-emacs/flashcard/files/50flashcard-gentoo.el
+++ app-emacs/flashcard/files/50flashcard-gentoo.el
@@ -3,7 +3,6 @@
 
 (add-to-list 'load-path "@SITELISP@")
 (add-to-list 'auto-mode-alist '("\\.deck\\'" . flashcard-mode))
-(autoload 'flashcard-import-from-colon-file
-  "flashcard"
-  "Autoload for `flashcard-import-from-colon-file'."
-  t)
+(autoload 'flashcard-mode "flashcard" nil t)
+(autoload 'flashcard-add-card "flashcard" nil t)
+(autoload 'flashcard-import-from-colon-file "flashcard" nil t)
