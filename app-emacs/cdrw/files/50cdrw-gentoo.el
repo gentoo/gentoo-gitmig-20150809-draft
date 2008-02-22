deb1b769fa02ba92279d616b3ed33d756ccfea58
Ulrich Müller <ulm@gentoo.org>
2008-02-22 09:07:29 +0000

    app-emacs/cdrw removal, bug 207133

--- app-emacs/cdrw/files/50cdrw-gentoo.el
+++
@@ -1,7 +0,0 @@
-
-;;; cdrw site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-
-(autoload 'cdrw "cdrw" "CDRW Creation" t)
-(autoload 'cdrw-from-file "cdrw" "Load previously saved mark-file." t)
