88d82794646f763f4d30c527c552e52860af567e
Ulrich Müller <ulm@gentoo.org>
2008-03-24 21:32:03 +0000

    Set the load-path during site initialisation. Package-Manager: portage-2.1.4.4

--- app-emacs/emacs-wget/files/65emacs-wget-gentoo.el
+++ app-emacs/emacs-wget/files/65emacs-wget-gentoo.el
@@ -1,6 +1,7 @@
 
 ;;; emacs-wget site-lisp configuration
 
+(add-to-list 'load-path "@SITELISP@")
 (autoload 'wget "wget" "wget interface for Emacs." t)
 (autoload 'wget-web-page "wget" "wget interface to download whole web page." t)
 (load "w3m-wget")
