b0e65cfdef782e644248e7d76c432d6642742099
Mike Frysinger <vapier@gentoo.org>
2008-05-10 10:38:41 +0000

    Handle just -d in wrapper script as this is how tar calls it #220899 by Ulrich Müller. Package-Manager: portage-2.2_pre5

--- app-arch/lzma-utils/files/lzma-nocxx.sh
+++ app-arch/lzma-utils/files/lzma-nocxx.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-if [ "$1" = "-dc" ] ; then
-	shift
-	cat "$@" | lzmadec
-else
-	(
-	echo "You've built lzma-utils without C++ support."
-	echo "If you want lzma support, rebuild with C++ support."
-	) 1>&2
-	exit 1
-fi
+case $1 in
+	-dc)
+		shift
+		cat "$@" | lzmadec
+		;;
+	-d)
+		shift
+		lzmadec
+		;;
+	*)
+		(
+		echo "You've built lzma-utils without C++ support."
+		echo "If you want lzma support, rebuild with C++ support."
+		) 1>&2
+		exit 1
+		;;
+esac
