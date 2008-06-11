1c0d9b7100e9acb26851f1f9575f6033ae32c2cd
Christian Faulhammer <fauli@gentoo.org>
2008-06-11 23:05:39 +0000

    make the rebuild script space safe Package-Manager: portage-2.1.4.4

--- mail-client/claws-mail/files/plugins-rebuild.sh
+++ mail-client/claws-mail/files/plugins-rebuild.sh
@@ -1,10 +1,10 @@
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/mail-client/claws-mail/files/plugins-rebuild.sh,v 1.2 2006/12/19 18:30:06 ticho Exp $
+# $Header: /var/cvsroot/gentoo-x86/mail-client/claws-mail/files/plugins-rebuild.sh,v 1.3 2008/06/11 22:48:14 opfer Exp $
 #!/bin/bash
 
-if [ -d ${ROOT}/var/db/pkg ]; then
-	cd ${ROOT}/var/db/pkg
+if [ -d "${ROOT}"/var/db/pkg ]; then
+	cd "${ROOT}"/var/db/pkg
 else
 	echo "ERROR: package database not found"
 	exit 1
@@ -12,7 +12,7 @@ fi
 
 if ls -d mail-client/sylpheed-claws-[a-z]* &> /dev/null; then
 	echo "Looking for sylpheed-claws plugins to rename ..."
-	OLDPLUGINS=$(for d in mail-client/sylpheed-claws-[a-z]*; do /usr/lib/portage/bin/pkgname $d | cut -d' ' -f 1; done)
+	OLDPLUGINS=$(for d in mail-client/sylpheed-claws-[a-z]*; do /usr/lib/portage/bin/pkgname ${d} | cut -d' ' -f 1; done)
 	echo 
 	echo "Found old plugins for rename:"
 	echo "${OLDPLUGINS}"
@@ -27,7 +27,7 @@ if ls -d mail-client/sylpheed-claws-[a-z]* &> /dev/null; then
 	emerge $* ${OLDPLUGINS//sylpheed-claws/claws-mail}
 elif ls -d mail-client/claws-mail-[a-z]* &> /dev/null; then
 	echo "Looking for claws-mail plugins to rebuild ..."
-	PLUGINS=$(for d in mail-client/claws-mail-[a-z]*; do /usr/lib/portage/bin/pkgname $d | cut -d' ' -f 1; done)
+	PLUGINS=$(for d in mail-client/claws-mail-[a-z]*; do /usr/lib/portage/bin/pkgname ${d} | cut -d' ' -f 1; done)
 	echo
 	echo "Found plugins for rebuilding:"
 	echo "${PLUGINS}"
