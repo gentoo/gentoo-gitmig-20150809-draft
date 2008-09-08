81aa8dfa9199d00df4cfd8789938adc3b04ad8de
Tiziano Müller <dev-zero@gentoo.org>
2008-09-08 08:57:09 +0000

    Removed bashism from start script Package-Manager: portage-2.2_rc8/cvs/Linux 2.6.27-rc4 x86_64

--- sys-fs/evms/files/evms2-start.sh
+++ sys-fs/evms/files/evms2-start.sh
@@ -1,9 +1,9 @@
 # /lib/rcscripts/addons/evms-start.sh:  Setup evms volumes at boot
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-fs/evms/files/evms2-start.sh,v 1.3 2007/08/31 05:16:34 dev-zero Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-fs/evms/files/evms2-start.sh,v 1.4 2008/09/08 08:57:09 dev-zero Exp $
 
-if [[ -z ${CDBOOT} ]] ; then
+if [ -z ${CDBOOT} ] ; then
 	ebegin "Activating EVMS"
 	evms_activate
 	retval=$?
