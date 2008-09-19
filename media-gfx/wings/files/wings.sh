6e2fb3271f1a5d8a486de7788a852c28a9906006
Markus Meier <maekke@gentoo.org>
2008-09-19 21:44:34 +0000

    bump to wings-0.99.03 by Ben Schwartz and Jyrki Launonen in bug #205090 Package-Manager: portage-2.2_rc8/cvs/Linux 2.6.27-rc6 i686

--- media-gfx/wings/files/wings.sh
+++ media-gfx/wings/files/wings.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ESDL_ROOT="/usr/lib/erlang/lib/esdl"
 WINGS_ROOT="/usr/lib/erlang/lib/wings"
-exec erl -noshell -pa $ESDL_ROOT/ebin $WINGS_ROOT/ebin -run wings_start start_halt
+exec erl -smp disable -noshell -pa $ESDL_ROOT/ebin $WINGS_ROOT/ebin -run wings_start start_halt
