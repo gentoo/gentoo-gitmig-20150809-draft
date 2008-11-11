9052a2c377a9cd871aa08d5046c6654265413274
Joe Peterson <lavajoe@gentoo.org>
2008-11-11 14:03:15 +0000

    Remove use of sudo and nice in init script; no rev bump Package-Manager: portage-2.2_rc13/cvs/Linux 2.6.27-gentoo-r2 i686

--- media-sound/squeezecenter/files/squeezecenter.init.d
+++ media-sound/squeezecenter/files/squeezecenter.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2008 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/media-sound/squeezecenter/files/squeezecenter.init.d,v 1.1 2008/08/03 04:35:29 lavajoe Exp $
+# $Header: /var/cvsroot/gentoo-x86/media-sound/squeezecenter/files/squeezecenter.init.d,v 1.2 2008/11/11 14:03:15 lavajoe Exp $
 
 # These fit the SqueezeCenter ebuild and so shouldn't need to be changed;
 # user-servicable parts go in /etc/conf.d/squeezecenter.
@@ -21,20 +21,20 @@ depend() {
 start() {
 	ebegin "Starting SqueezeCenter"
 
+        export SSD_NICELEVEL=${SC_NICENESS}
 	cd /
-	/usr/bin/nice --adjustment=${SC_NICENESS:-0} sudo -u ${scuser} \
-		start-stop-daemon \
-			--start --exec /usr/bin/perl /usr/sbin/${scname} \
-			-- \
-			--quiet --daemon \
-			--pidfile=${pidfile} \
-			--cachedir=${cachedir} \
-			--prefsfile=${prefsfile} \
-			--prefsdir=${prefsdir} \
-			--logdir=${logdir} \
-			--audiodir=${SC_MUSIC_DIR} \
-			--playlistdir=${SC_PLAYLISTS_DIR} \
-			${SC_OPTS}
+	start-stop-daemon \
+	    --start --exec /usr/bin/perl /usr/sbin/${scname} --chuid ${scuser} \
+	    -- \
+	    --quiet --daemon \
+	    --pidfile=${pidfile} \
+	    --cachedir=${cachedir} \
+	    --prefsfile=${prefsfile} \
+	    --prefsdir=${prefsdir} \
+	    --logdir=${logdir} \
+	    --audiodir=${SC_MUSIC_DIR} \
+	    --playlistdir=${SC_PLAYLISTS_DIR} \
+	    ${SC_OPTS}
 
 	eend $? "Failed to start SqueezeCenter"
 }
