ee5b287f496ebc4a8764123ba25ba83d4c4a6324
Tiago Cunha <tcunha@gentoo.org>
2008-11-17 00:00:46 +0000

    Improve init scripts by completing dependency information, and by adding the reload option Package-Manager: portage-2.1.4.5

--- net-analyzer/symon/files/symon-init.d
+++ net-analyzer/symon/files/symon-init.d
@@ -1,20 +1,31 @@
 #!/sbin/runscript
 # Copyright 1999-2007 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header:
+# $Header: /var/cvsroot/gentoo-x86/net-analyzer/symon/files/symon-init.d,v 1.2 2008/11/17 00:00:46 tcunha Exp $
+
+opts="${opts} reload"
 
 depend() {
-	need net
+	after bootmisc
+	need localmount net
+	use logger
+}
+
+reload() {
+	ebegin "Reloading symon"
+	start-stop-daemon --stop --pidfile /var/run/symon.pid \
+		--exec /usr/sbin/symon --oknodo --signal HUP
+	eend $?
 }
 
 start() {
 	ebegin "Starting symon"
-	start-stop-daemon --start --quiet --exec /usr/sbin/symon -- -u
-	eend ${?}
+	start-stop-daemon --start --exec /usr/sbin/symon -- -u
+	eend $?
 }
 
 stop() {
 	ebegin "Stopping symon"
-	start-stop-daemon --stop --quiet --pidfile /var/run/symon.pid
-	eend ${?}
+	start-stop-daemon --stop --pidfile /var/run/symon.pid
+	eend $?
 }
--- net-analyzer/symon/files/symux-init.d
+++ net-analyzer/symon/files/symux-init.d
@@ -1,20 +1,31 @@
 #!/sbin/runscript
 # Copyright 1999-2007 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header:
+# $Header: /var/cvsroot/gentoo-x86/net-analyzer/symon/files/symux-init.d,v 1.2 2008/11/17 00:00:46 tcunha Exp $
+
+opts="${opts} reload"
 
 depend() {
-	need net
+	after bootmisc
+	need localmount net
+	use logger
+}
+
+reload() {
+	ebegin "Reloading symux"
+	start-stop-daemon --stop --pidfile /var/run/symux.pid \
+		--exec /usr/sbin/symux --oknodo --signal HUP
+	eend $?
 }
 
 start() {
 	ebegin "Starting symux"
-	start-stop-daemon --start --quiet --exec /usr/sbin/symux
-	eend ${?}
+	start-stop-daemon --start --exec /usr/sbin/symux
+	eend $?
 }
 
 stop() {
 	ebegin "Stopping symux"
-	start-stop-daemon --stop --quiet --pidfile /var/run/symux.pid
-	eend ${?}
+	start-stop-daemon --stop --pidfile /var/run/symux.pid
+	eend $?
 }
