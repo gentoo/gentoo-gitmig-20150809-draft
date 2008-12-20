1aae5377f3b7fa25bea061c606dd4e22146aa0d7
Joe Peterson <lavajoe@gentoo.org>
2008-12-20 01:24:43 +0000

    Add new version 7.3.0 Package-Manager: portage-2.1.6.1/cvs/Linux 2.6.27-gentoo-r4 i686

--- media-sound/squeezecenter/files/squeezecenter.conf.d
+++ media-sound/squeezecenter/files/squeezecenter.conf.d
@@ -1,6 +1,6 @@
 # /etc/conf.d/squeezecenter
 
-# Niceness level for the SqueezeCenter process. If not specified then the
+# Niceness level for the SqueezeCenter process.  If not specified then the
 # default is to run at standard priority.  Uncomment the following to run at a
 # high priority (in order to try to minimise drop-outs due to audio starvation
 # of the players).  Note that this requires "OpenRC", which will become the
@@ -15,7 +15,7 @@
 
 # The following contains any other options you want to specify, such as default
 # logging options.  The example below will prevent the discovery and display of
-# UPNP devices within your players.
+# UPnP devices within your players.
 #
 # See "squeezecenter-server --help" for a full list of possible options,
 # but note that many of them are supplied by /etc/init.d/squeezecenter
--- media-sound/squeezecenter/files/squeezecenter.init.d
+++ media-sound/squeezecenter/files/squeezecenter.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2008 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/media-sound/squeezecenter/files/squeezecenter.init.d,v 1.2 2008/11/11 14:03:15 lavajoe Exp $
+# $Header: /var/cvsroot/gentoo-x86/media-sound/squeezecenter/files/squeezecenter.init.d,v 1.3 2008/12/20 01:24:43 lavajoe Exp $
 
 # These fit the SqueezeCenter ebuild and so shouldn't need to be changed;
 # user-servicable parts go in /etc/conf.d/squeezecenter.
@@ -21,20 +21,24 @@ depend() {
 start() {
 	ebegin "Starting SqueezeCenter"
 
-        export SSD_NICELEVEL=${SC_NICENESS}
+	export SSD_NICELEVEL=${SC_NICENESS}
 	cd /
 	start-stop-daemon \
-	    --start --exec /usr/bin/perl /usr/sbin/${scname} --chuid ${scuser} \
-	    -- \
-	    --quiet --daemon \
-	    --pidfile=${pidfile} \
-	    --cachedir=${cachedir} \
-	    --prefsfile=${prefsfile} \
-	    --prefsdir=${prefsdir} \
-	    --logdir=${logdir} \
-	    --audiodir=${SC_MUSIC_DIR} \
-	    --playlistdir=${SC_PLAYLISTS_DIR} \
-	    ${SC_OPTS}
+		--start --exec /usr/bin/perl /usr/sbin/${scname} \
+		--pidfile ${pidfile} \
+		--startas /usr/sbin/${scname} \
+		--chuid ${scuser} \
+		-- \
+		--quiet --daemon \
+		--pidfile=${pidfile} \
+		--cachedir=${cachedir} \
+		--prefsfile=${prefsfile} \
+		--prefsdir=${prefsdir} \
+		--logdir=${logdir} \
+		--audiodir=${SC_MUSIC_DIR} \
+		--playlistdir=${SC_PLAYLISTS_DIR} \
+		${SC_OPTS}
+
 
 	eend $? "Failed to start SqueezeCenter"
 }
