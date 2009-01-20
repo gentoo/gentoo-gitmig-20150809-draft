1f58a10a7b58f0e29e74726123b079a0a2b59e3c
Matthias Schwarzott <zzam@gentoo.org>
2009-01-20 11:20:22 +0000

    Change comments. Package-Manager: portage-2.1.6.6/cvs/Linux 2.6.27-gentoo-r1 i686

--- sys-fs/udev/files/shell-compat-118-r3.sh
+++ sys-fs/udev/files/shell-compat-118-r3.sh
@@ -6,6 +6,7 @@ cmd_exist()
 	type "$1" >/dev/null 2>&1
 }
 
+# does not exist in baselayout-1, does exist in openrc
 if ! cmd_exist yesno; then
 	yesno() {
 		[ -z "$1" ] && return 1
@@ -16,6 +17,8 @@ if ! cmd_exist yesno; then
 	}
 fi
 
+# does exist in baselayout-1
+# does not exist in openrc, but is added by openrc-ebuild since some time
 if ! cmd_exist KV_to_int; then
 	KV_to_int() {
 		[ -z $1 ] && return 1
@@ -36,6 +39,7 @@ if ! cmd_exist KV_to_int; then
 	}
 fi
 
+# same as KV_to_int
 if ! cmd_exist get_KV; then
 	_RC_GET_KV_CACHE=""
 	get_KV() {
@@ -48,7 +52,7 @@ if ! cmd_exist get_KV; then
 	}
 fi
 
-# only for baselayout-1, does exist in all openrc versions
+# does not exist in baselayout-1, does exist in openrc
 if ! cmd_exist fstabinfo; then
 	fstabinfo() {
 		[ "$1" = "--quiet" ] && shift
@@ -67,4 +71,5 @@ if ! cmd_exist fstabinfo; then
 		"${ret}"
 	}
 fi
+
 
