77216cff193270f69042f12333e6d08072f4662a
Ulrich Müller <ulm@gentoo.org>
2009-03-28 10:17:13 +0000

    Use add-to-list in Emacs site-init file. Remove unused file. Package-Manager: portage-2.2_rc27/cvs/Linux i686

--- app-office/magicpoint/files/50magicpoint-gentoo.el
+++ app-office/magicpoint/files/50magicpoint-gentoo.el
@@ -1,5 +1,6 @@
+
 ;;; magicpoint site-lisp configuration
 
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'mgp-mode "mgp-mode" "MagicPoint editor mode" t)
-(setq auto-mode-alist (cons '("\\.mgp$" . mgp-mode) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.mgp\\'" . mgp-mode))
--- app-office/magicpoint/files/50mgp-mode-gentoo.el
+++
@@ -1,4 +0,0 @@
-;;; mgp-mode site-lisp configuration
-
-(autoload 'mgp-mode "mgp-mode" "MagicPoint editor mode" t)
-(setq auto-mode-alist (cons '("\\.mgp$" . mgp-mode) auto-mode-alist))
