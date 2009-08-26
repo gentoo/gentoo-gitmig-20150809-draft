af17b754c04607c4d73aefbdb67a83280ab151b4
Sebastien Fabbro <bicatali@gentoo.org>
2009-08-26 18:28:40 +0000

    Version bump. Now gmp and gf2x are forced. Fixed parallel building, and hopefully as-needed issues (bug #282174). Simplified the shared lib patch. Added sci-mathematics herd as co-maintainer. Package-Manager: portage-2.2_rc38/cvs/Linux x86_64

--- dev-libs/ntl/files/linux.mk
+++
@@ -1,3 +0,0 @@
-shared: DIRNAME $(SHOBJ)
-	$(LINK_CXX) $(PICFLAG) -shared -Wl,-soname,lib`cat DIRNAME`.so -o lib`cat DIRNAME`.so $(SHOBJ) $(GMP_OPT_LIBDIR) $(GMP_OPT_LIB)
-	ln -s lib`cat DIRNAME`.so libntl.so
