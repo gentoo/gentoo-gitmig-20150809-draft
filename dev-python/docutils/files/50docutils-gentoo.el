5fcb77a471e5677da9a1b9ec51c43a6354de8407
Arfrever Frehtes Taifersar Arahesis <arfrever@gentoo.org>
2009-09-12 21:21:04 +0000

    Delete older ebuilds.

--- dev-python/docutils/files/50docutils-gentoo.el
+++
@@ -1,8 +0,0 @@
-
-;; site-lisp configuration for docutils
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'rst-mode "rst-mode" "mode for editing reStructuredText documents" t)
-(setq auto-mode-alist
-      (append '(("\\.rst$" . rst-mode)
-                ("\\.rest$" . rst-mode)) auto-mode-alist))
