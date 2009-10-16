7051f95f24fc83a86bc7c5e1813a5ef52488ba10
Ulrich Müller <ulm@gentoo.org>
2009-10-16 20:43:36 +0000

    Version bump, new snapshot from upstream repository. Package-Manager: portage-2.2_rc46/cvs/Linux i686

--- app-emacs/folding/files/70folding-gentoo.el
+++ app-emacs/folding/files/70folding-gentoo.el
@@ -1,6 +1,3 @@
-
-;;; folding site-lisp configuration
-
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'folding-mode          "folding" "Folding mode" t)
 (autoload 'turn-off-folding-mode "folding" "Folding mode" t)
