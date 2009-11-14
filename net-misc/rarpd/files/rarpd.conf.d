055d2e0931ff2e2896bea17deae4d21f218efeda
Tomas Chvatal <scarabeus@gentoo.org>
2009-11-14 17:08:18 +0000

    Drop package masked for removal.

--- net-misc/rarpd/files/rarpd.conf.d
+++
@@ -1,8 +0,0 @@
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/rarpd/files/rarpd.conf.d,v 1.2 2004/07/15 00:10:49 agriffis Exp $
-
-# Config file for /etc/init.d/rarpd
-
-# see the rarpd(8) manpage for more info
-RARPD_OPTS=""
