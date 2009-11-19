3afe5545eb3f8aec8a212d13283d6000dda0c9df
Samuli Suominen <ssuominen@gentoo.org>
2009-11-19 21:42:28 +0000

    Remove KDE 3.5.10.

--- kde-base/kdvi/files/50kdvi-gentoo.el
+++
@@ -1,3 +0,0 @@
-(require 'kdvi-search)
-(add-hook 'LaTeX-mode-hook (lambda () (local-set-key "\C-x\C-j" 'kdvi-jump-to-line)))
-(add-hook 'tex-mode-hook (lambda () (local-set-key "\C-x\C-j" 'kdvi-jump-to-line)))
