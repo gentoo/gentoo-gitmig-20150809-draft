14ea5687eb96c8a30653f51bf3ca6eb2690848c9
Mike Frysinger <vapier@gentoo.org>
2009-12-14 03:09:13 +0000

    update man pages to 4.2.6

--- net-misc/ntp/files/man-pages/genmans.sh
+++ net-misc/ntp/files/man-pages/genmans.sh
@@ -16,8 +16,9 @@ fi
 SRCDIR=${0%/*}
 HTMLDIR=${SRCDIR}/${VERSION}/html
 DISTFILE=/usr/portage/distfiles/${VERSION}.tar.gz
-MANDIR=man
+MANDIR=${SRCDIR}/man
 
+rm -rf ${SRCDIR}/${VERSION}
 if [[ ! -d ${HTMLDIR} ]] ; then
 	if [[ -f ${DISTFILE} ]] ; then
 		tar zxf ${DISTFILE} -C ${SRCDIR} || exit 1
@@ -33,7 +34,7 @@ processfile() {
     MANFILE=$2
     echo -n "Processing $HTMLFILE ..."
 	sed -e "s:<csobj.*<:csobj>/:" $HTMLDIR/$HTMLFILE > .$HTMLFILE
-	xsltproc --html --stringparam version $VERSION ntp.xsl .$HTMLFILE > $MANDIR/$MANFILE || exit 1
+	xsltproc --html --stringparam version $VERSION ${SRCDIR}/ntp.xsl .$HTMLFILE > $MANDIR/$MANFILE || exit 1
 	rm -f .$HTMLFILE
     echo "Done."
 }
@@ -49,9 +50,9 @@ read
 # Apply patch if still needed
 (
 cd $HTMLDIR
-patch -p2 --dry-run < ../../ntpdc.html.patch -sf || exit 0
-patch -p2 < ../../ntpdc.html.patch
-)
+patch -F0 -p2 --dry-run < ../../ntpdc.html.patch -sf || exit 0
+patch -F0 -p2 < ../../ntpdc.html.patch
+) || exit 1
 
 # Process HTML files
 rm -rf ${MANDIR}
@@ -61,8 +62,10 @@ for f in ntp{date,dc,d,q,time,trace,dsim} keygen tickadj ; do
 done
 echo
 
-cp -vi /usr/local/src/freebsd/src/usr.sbin/ntp/doc/*.5 man/
-cp -vi *.patch genmans.sh ntp.xsl man/
+cp -vi /usr/local/src/freebsd/src/usr.sbin/ntp/doc/*.5 ${MANDIR}
+cp -vi ${SRCDIR}/*.patch ${SRCDIR}/genmans.sh ${SRCDIR}/ntp.xsl ${MANDIR}
 
-tar -jcf ${VERSION}-manpages.tar.bz2 ${MANDIR}
+tar -jcf ${VERSION}-manpages.tar.bz2 -C ${MANDIR}/.. man
 du -b ${VERSION}-manpages.tar.bz2
+
+rm -rf ${MANDIR} ${SRCDIR}/${VERSION}
