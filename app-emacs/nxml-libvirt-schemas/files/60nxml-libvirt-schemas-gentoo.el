2d83a32cf6c468bccb07f4cda3e988540ae4da60
Diego Elio Pettenò <flameeyes@gentoo.org>
2009-12-16 19:12:59 +0000

    Initial import of nxml-mode schemas for libvirt; it requires patches backported from the upstream tree for the files to properly validate. Package-Manager: portage-2.2_rc59/cvs/Linux x86_64

---
+++ app-emacs/nxml-libvirt-schemas/files/60nxml-libvirt-schemas-gentoo.el
@@ -0,0 +1,8 @@
+
+;;; nxml-svg-schemas site-lisp configuration
+
+;; This must be executed after rng-schema-locating-files
+;; is set in rng-loc (which is part of nxml-mode).
+(eval-after-load "rng-loc"
+  '(add-to-list 'rng-schema-locating-files
+		"@SITEETC@/schemas.xml"))
