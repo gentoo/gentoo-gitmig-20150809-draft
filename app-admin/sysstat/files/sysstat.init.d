b6e9bd7441476ecdba6b3bd3a912eaafaf05bb90
Jeroen Roovers <jer@gentoo.org>
2010-08-02 16:17:38 +0000

    Describe more accurately what the init.d script is doing and not doing. Package-Manager: portage-2.2_rc67/cvs/Linux i686

--- app-admin/sysstat/files/sysstat.init.d
+++ app-admin/sysstat/files/sysstat.init.d
@@ -1,26 +1,26 @@
 #!/sbin/runscript
-# Copyright 1999-2006 Gentoo Foundation
+# Copyright 1999-2010 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/app-admin/sysstat/files/sysstat.init.d,v 1.1 2007/05/31 10:08:50 jokey Exp $
+# $Header: /var/cvsroot/gentoo-x86/app-admin/sysstat/files/sysstat.init.d,v 1.2 2010/08/02 16:17:38 jer Exp $
 
 depend() {
     use hostname
 }
 
 start() {
-    ebegin "Calling the system activity data collector (sadc)..."
+    ebegin "Writing a dummy startup record using sadc (see sadc(8))..."
     /usr/lib/sa/sadc -F -L -
     eend $?
 }
 
 stop() {
-    ebegin "Stopping..."
+    ebegin "Cannot stop writing a dummy startup record (see sadc(8))..."
     eend $?
 }
 
 # restart emulation
 # svc_start svc_stop
 restart() {
-    ebegin "Restart..."
+    ebegin "Not writing yet another dummy startup record (see sadc(8))..."
     eend $?
 }
