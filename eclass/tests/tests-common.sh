9b5df37cb0db185405505f97dce71d9c306cdb7e
Michał Górny <mgorny@gentoo.org>
2010-10-18 07:18:14 +0000

    Implement debug-print*() family of functions for tests.

--- eclass/tests/tests-common.sh
+++ eclass/tests/tests-common.sh
@@ -9,5 +9,23 @@ inherit() {
 	done
 }
 
+debug-print() {
+	[[ ${#} -eq 0 ]] && return
+		
+	if [[ ${ECLASS_DEBUG_OUTPUT} == on ]]; then
+		printf 'debug: %s\n' "${@}" >&2
+	elif [[ -n ${ECLASS_DEBUG_OUTPUT} ]]; then
+		printf 'debug: %s\n' "${@}" >> "${ECLASS_DEBUG_OUTPUT}"
+	fi
+}
+
+debug-print-function() {
+	debug-print "${1}, parameters: ${*:2}"
+}
+
+debug-print-section() {
+	debug-print "now in section ${*}"
+}
+
 hasq() { [[ " ${*:2} " == *" $1 "* ]]; }
 has() { hasq "$@"; }
