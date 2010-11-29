3cbec2c8a68f1ea529141787c7a7f2c1ea69cafe
Ulrich Müller <ulm@gentoo.org>
2010-11-29 21:44:14 +0000

    Remove comment and empty lines from Emacs site-init file; elisp-common.eclass now generates them automatically.
    Package-Manager: portage-2.1.9.25/cvs/Linux x86_64

--- sci-visualization/gri/files/50gri-gentoo.el
+++ sci-visualization/gri/files/50gri-gentoo.el
@@ -1,7 +1,3 @@
-
-;; site-lisp configuration for sci-misc/gri
-
 (add-to-list 'load-path "@SITELISP@")
-
 (autoload 'gri-mode "gri-mode" "Enter Gri-mode." t)
 (add-to-list 'auto-mode-alist '("\\.gri\\'" . gri-mode))
