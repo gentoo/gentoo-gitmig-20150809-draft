7cf033110f9649e2cf8829f615a743b9d08b02f9
Joerg Bornkessel <hd_brummy@gentoo.org>
2010-12-28 18:44:25 +0000

    use shared tinyxml lib now, see bug #349507; some minor fixes in source code
    Package-Manager: portage-2.1.9.24/cvs/Linux i686

--- media-plugins/vdr-audiorecorder/files/rc-addon.sh
+++ media-plugins/vdr-audiorecorder/files/rc-addon.sh
@@ -1,4 +1,4 @@
-# $Header: /var/cvsroot/gentoo-x86/media-plugins/vdr-audiorecorder/files/rc-addon.sh,v 1.1 2006/07/05 16:57:35 zzam Exp $
+# $Header: /var/cvsroot/gentoo-x86/media-plugins/vdr-audiorecorder/files/rc-addon.sh,v 1.2 2010/12/28 18:44:25 hd_brummy Exp $
 #
 # rc-addon-script for plugin audiorecorder
 #
@@ -8,5 +8,6 @@
 
 plugin_pre_vdr_start() {
 	add_plugin_param "--recdir=${AUDIORECORDER_DIR}"
+	add_plugin_param "--debug=0"
 }
 
