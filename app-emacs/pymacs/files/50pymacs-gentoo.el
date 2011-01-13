8d5b1a07672768b1923ea62186dcd80f2e3c5836
Ulrich Müller <ulm@gentoo.org>
2011-01-13 20:24:43 +0000

    Remove old.
    Package-Manager: portage-2.1.9.30/cvs/Linux x86_64

--- app-emacs/pymacs/files/50pymacs-gentoo.el
+++ app-emacs/pymacs/files/50pymacs-gentoo.el
@@ -1,8 +1,4 @@
-
-;;; pymacs site-lisp configuration
-
 (add-to-list 'load-path "@SITELISP@")
-
 (autoload 'pymacs-load "pymacs" nil t)
 (autoload 'pymacs-eval "pymacs" nil t)
 (autoload 'pymacs-apply "pymacs")
