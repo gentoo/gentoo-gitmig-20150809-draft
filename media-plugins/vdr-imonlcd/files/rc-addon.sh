dc4cc65ca61208b5775e7f3b12f39eb664d76e26
Christian Ruppert <idl0r@gentoo.org>
2011-01-28 23:14:02 +0000

    Remove useless source call.
    Package-Manager: portage-2.2.0_alpha19/cvs/Linux x86_64

--- media-plugins/vdr-imonlcd/files/rc-addon.sh
+++ media-plugins/vdr-imonlcd/files/rc-addon.sh
@@ -1,6 +1,4 @@
-# $Header: /var/cvsroot/gentoo-x86/media-plugins/vdr-imonlcd/files/rc-addon.sh,v 1.2 2011/01/28 23:11:47 idl0r Exp $
-
-[ -e "/etc/conf.d/vdr.imonlcd" ] && source /etc/conf.d/vdr.imonlcd
+# $Header: /var/cvsroot/gentoo-x86/media-plugins/vdr-imonlcd/files/rc-addon.sh,v 1.3 2011/01/28 23:14:02 idl0r Exp $
 
 plugin_pre_vdr_start() {
 	add_plugin_param "${IMONLCD_DEVICE:+--device ${IMONLCD_DEVICE}}"
