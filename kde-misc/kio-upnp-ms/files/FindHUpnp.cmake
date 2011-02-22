2325e744606f175c30138a315252a43ae739d3f5
Christoph Junghans <ottxor@gentoo.org>
2011-02-22 21:43:27 +0000

    import from cj-overlay (dep for bug #351916)
    Package-Manager: portage-2.1.9.25/cvs/Linux i686

---
+++ kde-misc/kio-upnp-ms/files/FindHUpnp.cmake
@@ -0,0 +1,22 @@
+# - Find HUpnp
+# HUpnp is a Universal Plug and Play (UPnP) library
+# used by the UPnP collection.
+# Defines:
+# HUPNP_INCLUDE_DIR
+# HUPNP_LIBS
+# HUPNP_FOUND
+
+find_path(HUPNP_INCLUDE_DIR HUpnp HINTS ${KDE4_INCLUDE_DIR})
+
+find_library(HUPNP_LIBS HUpnp PATHS ${KDE4_LIB_DIR})
+
+if(HUPNP_INCLUDE_DIR AND HUPNP_LIBS)
+  set(HUPNP_FOUND TRUE)
+  message(STATUS "Found HUpnp")
+else(HUPNP_INCLUDE_DIR and HUPNP_LIBS)
+  set(HUPNP_FOUND FALSE)
+  if(HUPNP_FIND_REQUIRED)
+    message(FATAL_ERROR "Could NOT find required package HUpnp: <http://herqq.org>")
+  endif(HUPNP_FIND_REQUIRED)
+endif(HUPNP_INCLUDE_DIR AND HUPNP_LIBS)
+
