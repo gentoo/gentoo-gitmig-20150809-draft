fdac8c50310237d3bc64ce187c9f0a77d1aa9402
Diego Elio Pettenò <flameeyes@gentoo.org>
2011-04-08 01:50:53 +0000

    Make the init script provide the entropy service.
    Package-Manager: portage-2.2.0_alpha29/cvs/Linux x86_64

--- sys-apps/haveged/files/haveged-init.d
+++ sys-apps/haveged/files/haveged-init.d
@@ -1,11 +1,15 @@
 #!/sbin/runscript
 # Copyright 1999-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-apps/haveged/files/haveged-init.d,v 1.1 2011/01/05 03:30:30 robbat2 Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-apps/haveged/files/haveged-init.d,v 1.2 2011/04/08 01:50:53 flameeyes Exp $
 
 DAEMON=haveged
 HAVEGED_OPTS="-r 0 ${HAVEGED_OPTS}"
 PIDFILE=/var/run/${DAEMON}.pid
+
+depend() {
+	provide entropy
+}
 
 start() {
 	ebegin "Starting ${DAEMON}"
