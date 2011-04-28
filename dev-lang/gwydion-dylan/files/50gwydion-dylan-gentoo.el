fcd7808b824e1e49e036fbac22ab9f28809a097a
Tomas Chvatal <scarabeus@gentoo.org>
2011-04-28 15:38:21 +0000

    Drop per bug #316501 and #247043.

--- dev-lang/gwydion-dylan/files/50gwydion-dylan-gentoo.el
+++
@@ -1,7 +0,0 @@
-
-;;; gwydion-dylan site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-
-(autoload 'dylan-mode "dylan-mode" "Dylan-mode" t)
-(add-to-list 'auto-mode-alist '("\\.dylan\\'" . dylan-mode))
