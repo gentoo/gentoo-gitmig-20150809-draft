edf5e857c4ff1b1da0337c4146a4d47cabbea623
Ulrich Müller <ulm@gentoo.org>
2011-05-18 18:21:48 +0000

    Remove package app-emacs/table, bug 358351.

--- app-emacs/table/files/50table-gentoo.el
+++
@@ -1,8 +0,0 @@
-
-;;; table site-lisp configuration
-
-;; Emacs 22 and later include the table package
-(unless (fboundp 'table-insert)
-  (add-to-list 'load-path "@SITELISP@")
-  (load "table-autoloads" nil t)
-  (add-hook 'text-mode-hook 'table-recognize))
