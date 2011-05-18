f8b9bfbf6727730630b8021f46c9f1030f6f3a3e
Ulrich Müller <ulm@gentoo.org>
2011-05-18 18:24:35 +0000

    Remove package app-emacs/htmlfontify, bug 358351.

--- app-emacs/htmlfontify/files/51htmlfontify-gentoo.el
+++
@@ -1,9 +0,0 @@
-
-;;; htmlfontify site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'htmlfontify-buffer "htmlfontify" nil t)
-(autoload 'htmlfontify-run-etags "htmlfontify" nil t)
-(autoload 'htmlfontify-copy-and-link-dir "htmlfontify" nil t)
-(autoload 'htmlfontify-load-rgb-file "hfy-cmap" nil t)
-(autoload 'htmlfontify-unload-rgb-file "hfy-cmap" nil t)
