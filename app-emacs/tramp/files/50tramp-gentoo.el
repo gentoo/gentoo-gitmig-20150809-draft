55ebf270442358f087ab889972c1401319d2b04b
Ulrich Müller <ulm@gentoo.org>
2011-05-18 18:22:47 +0000

    Remove package app-emacs/tramp, bug 358351.

--- app-emacs/tramp/files/50tramp-gentoo.el
+++
@@ -1,5 +0,0 @@
-(add-to-list 'load-path "@SITELISP@")
-(setq auto-save-file-name-transforms
-      `(("\\`/[^/]*:\\(.+/\\)*\\(.*\\)"
-	 ,(expand-file-name "\\2" temporary-file-directory))))
-(load "tramp-autoloads" nil t)
