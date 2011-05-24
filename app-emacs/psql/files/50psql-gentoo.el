662498cccd751e5c01d3111eafe60a02c470e461
Ulrich Müller <ulm@gentoo.org>
2011-05-24 16:16:04 +0000

    Remove package app-emacs/psql, bug 364775.
    Package-Manager: portage-2.1.9.49/cvs/Linux x86_64
    RepoMan-Options: --force

--- app-emacs/psql/files/50psql-gentoo.el
+++
@@ -1,7 +0,0 @@
-
-;;; psql site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'psql-mode "psql-mode" "Mode for editing postgress sql." t)
-(autoload 'psql-run  "psql-mode" "Mode for editing postgress sql." t)
-(add-to-list 'auto-mode-alist '("\\.p?sql$" . psql-mode ))
