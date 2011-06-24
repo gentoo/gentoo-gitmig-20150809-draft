823f8c35b07f530e70dda7ef0ed11ccd7b63ada6
Nirbheek Chauhan <nirbheek@gentoo.org>
2011-06-24 09:55:00 +0000

    Bump to 5.0, major release as well as security bump. XULRunner has been merged back into Firefox with this release, and there is no corresponding net-libs/xulrunner release since upstream has dropped official support for it. Packages that want libmozjs should use dev-lang/spidermonkey, which will be updated soon.
    Package-Manager: portage-2.2.0_alpha39_p14/cvs/Linux x86_64

--- www-client/firefox/files/gentoo-default-prefs.js
+++ www-client/firefox/files/gentoo-default-prefs.js
@@ -10,3 +10,5 @@ pref("browser.urlbar.hideGoButton",        true);
 pref("accessibility.typeaheadfind",        true);
 pref("browser.shell.checkDefaultBrowser",  false); 
 pref("browser.EULA.override",              true); 
+pref("general.useragent.vendor", "Gentoo");
+pref("intl.locale.matchOS", true);
