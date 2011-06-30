d0ca2282d3a2de91c4eba8ce3f9d9e7afbd5fa2e
Bjarke Istrup Pedersen <gurligebis@gentoo.org>
2011-06-30 20:02:18 +0000

    Bumping to 1.6_pre20110623
    Package-Manager: portage-2.2.0_alpha41/cvs/Linux i686

--- net-misc/miniupnpd/files/miniupnpd-init.d
+++ net-misc/miniupnpd/files/miniupnpd-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
-# Copyright 1999-2008 Gentoo Foundation
+# Copyright 1999-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/miniupnpd/files/miniupnpd-init.d,v 1.1 2010/02/28 19:36:06 gurligebis Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-misc/miniupnpd/files/miniupnpd-init.d,v 1.2 2011/06/30 20:02:18 gurligebis Exp $
 
 depend() {
 	need net iptables
@@ -10,7 +10,7 @@ depend() {
 start() {
 	ebegin "Starting miniupnpd"
 	/etc/miniupnpd/iptables_init.sh
-	start-stop-daemon --start --pidfile /var/run/miniupnpd.pid --startas /usr/sbin/miniupnpd -- ${ARGS}
+	start-stop-daemon --start --pidfile /var/run/miniupnpd.pid --exec /usr/sbin/miniupnpd -- ${ARGS}
 	eend $?
 }
 
