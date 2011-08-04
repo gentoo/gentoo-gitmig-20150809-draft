5c32774c19ae8989a4cf77401eeac768643874c4
Samuli Suominen <ssuominen@gentoo.org>
2011-08-04 17:30:01 +0000

    Rename opts="reload" as extra_commands="reload" for baselayout-2.
    Package-Manager: portage-2.2.0_alpha47/cvs/Linux x86_64

--- sys-power/acpid/files/acpid-2.0.11-init.d
+++ sys-power/acpid/files/acpid-2.0.11-init.d
@@ -1,9 +1,9 @@
 #!/sbin/runscript
 # Copyright 1999-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-power/acpid/files/acpid-2.0.11-init.d,v 1.2 2011/08/04 17:07:33 ssuominen Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-power/acpid/files/acpid-2.0.11-init.d,v 1.3 2011/08/04 17:30:01 ssuominen Exp $
 
-opts="reload"
+extra_commands="reload"
 
 depend() {
 	need localmount
