9de336f3551746875c71f64d705915b1908e07a1
Samuli Suominen <ssuominen@gentoo.org>
2011-08-04 17:01:47 +0000

    Version bump. Install systemd unit file wrt #375269 by Michał Górny. Remove deprecated --oknodo from initd script wrt #377761. Remove support for sys-apps/hal and drop "before hald" from initd script.
    Package-Manager: portage-2.2.0_alpha47/cvs/Linux x86_64

---
+++ sys-power/acpid/files/acpid-2.0.11-init.d
@@ -0,0 +1,29 @@
+#!/sbin/runscript
+# Copyright 1999-2011 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/sys-power/acpid/files/acpid-2.0.11-init.d,v 1.1 2011/08/04 17:01:47 ssuominen Exp $
+
+opts="reload"
+
+depend() {
+	need localmount
+	use logger
+}
+
+start() {
+	ebegin "Starting acpid"
+	start-stop-daemon --start --quiet --exec /usr/sbin/acpid -- ${ACPID_OPTIONS}
+	eend $?
+}
+
+stop() {
+	ebegin "Stopping acpid"
+	start-stop-daemon --stop --exec /usr/sbin/acpid
+	eend $?
+}
+
+reload() {
+	ebegin "Reloading acpid configuration"
+	start-stop-daemon --stop --exec /usr/sbin/acpid --signal HUP
+	eend $?
+}
