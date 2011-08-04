b5579ac679fc5429b167666452c93bdf584da67d
Samuli Suominen <ssuominen@gentoo.org>
2011-08-04 18:01:50 +0000

    Improve init.d script more for baselayout-2.
    Package-Manager: portage-2.2.0_alpha47/cvs/Linux x86_64

--- sys-power/acpid/files/acpid-2.0.11-init.d
+++ sys-power/acpid/files/acpid-2.0.11-init.d
@@ -1,29 +1,27 @@
 #!/sbin/runscript
 # Copyright 1999-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-power/acpid/files/acpid-2.0.11-init.d,v 1.3 2011/08/04 17:30:01 ssuominen Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-power/acpid/files/acpid-2.0.11-init.d,v 1.4 2011/08/04 18:01:50 ssuominen Exp $
 
 extra_commands="reload"
+command="/usr/sbin/acpid"
+command_args="${ACPID_OPTIONS}"
+start_stop_daemon_args="--quiet"
+description="Daemon for Advanced Configuration and Power Interface"
 
 depend() {
+	if [ ! -f /etc/init.d/sysfs ]; then
+		eerror "The $SVCNAME init-script is written for baselayout-2!"
+		eerror "Please do not use it with baselayout-1!".
+		return 1
+        fi
+
 	need localmount
 	use logger
 }
 
-start() {
-	ebegin "Starting acpid"
-	start-stop-daemon --start --quiet --exec /usr/sbin/acpid -- ${ACPID_OPTIONS}
-	eend $?
-}
-
-stop() {
-	ebegin "Stopping acpid"
-	start-stop-daemon --stop --exec /usr/sbin/acpid
-	eend $?
-}
-
 reload() {
 	ebegin "Reloading acpid configuration"
-	start-stop-daemon --exec /usr/sbin/acpid --signal HUP
+	start-stop-daemon --exec $command --signal HUP
 	eend $?
 }
