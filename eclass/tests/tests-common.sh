eba26e229d6505e5922ca3f0d69e61c476d21df4
Mike Frysinger <vapier@gentoo.org>
2011-09-23 04:03:12 +0000

    add die and has_version helpers

--- eclass/tests/tests-common.sh
+++ eclass/tests/tests-common.sh
@@ -11,7 +11,7 @@ inherit() {
 
 debug-print() {
 	[[ ${#} -eq 0 ]] && return
-		
+
 	if [[ ${ECLASS_DEBUG_OUTPUT} == on ]]; then
 		printf 'debug: %s\n' "${@}" >&2
 	elif [[ -n ${ECLASS_DEBUG_OUTPUT} ]]; then
@@ -36,4 +36,13 @@ has() {
 		[ "${x}" = "${needle}" ] && return 0
 	done
 	return 1
+}
+
+die() {
+	echo "die: $*" 1>&2
+	exit 1
+}
+
+has_version() {
+	portageq has_version / "$@"
 }
