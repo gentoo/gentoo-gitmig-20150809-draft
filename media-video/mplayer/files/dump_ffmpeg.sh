54b97519ffdb9960f2f449aa707593c11353272a
Alexis Ballier <aballier@gentoo.org>
2011-11-02 14:20:13 +0000

    riff.h isnt needed anymore
    Package-Manager: portage-2.2.0_alpha72/cvs/Linux x86_64

--- media-video/mplayer/files/dump_ffmpeg.sh
+++ media-video/mplayer/files/dump_ffmpeg.sh
@@ -61,8 +61,7 @@ move_file() {
 
 # HEADERS (order matters for the consistency checks: leaves come first)
 FILES=" libavutil/x86_cpu.h \
-        libavformat/internal.h \
-        libavformat/riff.h"
+        libavformat/internal.h "
 # Files that are sed'ed but not compiled, used to check for availability of
 # some codecs
 FILES="${FILES} libavcodec/allcodecs.c libavformat/allformats.c"
