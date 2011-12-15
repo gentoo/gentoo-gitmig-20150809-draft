0ca14e881db0fabfb09c595d8816a03cd7f91688
Alexis Ballier <aballier@gentoo.org>
2011-12-15 12:29:06 +0000

    update the prepare_mplayer script to use ffmpeg instead of libav and use dump_ffmpeg.sh for dumping unused files
    Package-Manager: portage-2.2.0_alpha81/cvs/Linux x86_64

--- media-video/mplayer/files/prepare_mplayer.sh
+++ media-video/mplayer/files/prepare_mplayer.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 VERSION=$(date +%Y%m%d)
 PACKAGE="mplayer-1.0_rc4_p${VERSION}"
+DUMP_FFMPEG="$(dirname $0)/dump_ffmpeg.sh"
 
 svn checkout svn://svn.mplayerhq.hu/mplayer/trunk ${PACKAGE}
 
 pushd ${PACKAGE} > /dev/null
 	# ffmpeg is in git now so no svn external anymore
 	rm -rf ffmpeg
-	git clone git://git.libav.org/libav.git ffmpeg/
+	git clone git://git.videolan.org/ffmpeg.git ffmpeg/
+        sh "$DUMP_FFMPEG"
 	STORE_VERSION=$(svn log -r HEAD -q |grep ^r |cut -d' ' -f1)
 	echo "*** Remember to adjust mplayer ebuild with revision: \"SVN-${STORE_VERSION}\" ***"
 popd > /dev/null
