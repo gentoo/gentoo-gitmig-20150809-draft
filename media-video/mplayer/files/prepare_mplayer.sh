405800bbbdb73ac561366c4f4c70ccad873327f0
Alexis Ballier <aballier@gentoo.org>
2011-12-16 13:24:10 +0000

    generate snapshot_version the same way as upstream version.sh and use printf which should be more portable
    Package-Manager: portage-2.2.0_alpha81/cvs/Linux x86_64

--- media-video/mplayer/files/prepare_mplayer.sh
+++ media-video/mplayer/files/prepare_mplayer.sh
@@ -10,8 +10,8 @@ pushd ${PACKAGE} > /dev/null
 	rm -rf ffmpeg
 	git clone git://git.videolan.org/ffmpeg.git ffmpeg/
         sh "$DUMP_FFMPEG"
-	STORE_VERSION=$(svn log -r HEAD -q |grep ^r |cut -d' ' -f1)
-	echo "$STORE_VERSION" > snapshot_version
+	STORE_VERSION=$(LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2)
+	printf "$STORE_VERSION" > snapshot_version
 popd > /dev/null
 
 find "${PACKAGE}" -type d -name '.svn' -prune -print0 | xargs -0 rm -rf
