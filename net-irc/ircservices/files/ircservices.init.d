27630d7dae0640dfb2845d237cf77bfe84240745
Nathan Phillip Brink <binki@gentoo.org>
2011-12-17 04:41:25 +0000

    Use --user option instead of deprecated --chuid option for start-stop-daemon, reported by ago.
    Package-Manager: portage-2.2.0_alpha78-r1/cvs/Linux x86_64

--- net-irc/ircservices/files/ircservices.init.d
+++ net-irc/ircservices/files/ircservices.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
-# Copyright 1999-2005 Gentoo Foundation
+# Copyright 1999-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License, v2 or later
-# $Header: /var/cvsroot/gentoo-x86/net-irc/ircservices/files/ircservices.init.d,v 1.5 2006/11/05 22:23:08 swegener Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-irc/ircservices/files/ircservices.init.d,v 1.6 2011/12/17 04:41:25 binki Exp $
 
 depend() {
 	if [[ ${LOCALIRCD} = true ]]
@@ -15,7 +15,7 @@ depend() {
 
 start() {
 	ebegin "Starting IRC Services"
-	start-stop-daemon --start --quiet --chuid ircservices --exec /usr/bin/ircservices -- \
+	start-stop-daemon --start --quiet --user ircservices --exec /usr/bin/ircservices -- \
 		-dir=/var/lib/ircservices \
 		-log=/var/log/ircservices/ircservices.log \
 		&>/dev/null
