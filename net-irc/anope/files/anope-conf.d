38c5a22f6a8e1104e6d44703b011ece1fa94793f
Bjarke Istrup Pedersen <gurligebis@gentoo.org>
2011-12-28 20:25:49 +0000

    Bumping to 1.8.7, removing old version  and moving out of /opt. Thanks to everybody who helped out. Fixing bug #395089
    Package-Manager: portage-2.2.0_alpha83/cvs/Linux i686

--- net-irc/anope/files/anope-conf.d
+++ net-irc/anope/files/anope-conf.d
@@ -1,6 +1,6 @@
-# Copyright 1999-2005 Gentoo Foundation
+# Copyright 1999-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-irc/anope/files/anope.confd,v 1.1 2006/08/29 22:54:17 gurligebis Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-irc/anope/files/anope-conf.d,v 1.1 2011/12/28 20:25:48 gurligebis Exp $
 
 # user to run ultimate-ircd as
 ANOPE_USER="anope"
