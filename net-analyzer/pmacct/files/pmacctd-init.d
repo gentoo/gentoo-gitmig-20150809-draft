5c13b758193c3d5febf7972ef9955d3caa62e407
Jeroen Roovers <jer@gentoo.org>
2012-04-13 15:54:38 +0000

    Fix run time configuration directory.
    Package-Manager: portage-2.2.0_alpha100/cvs/Linux x86_64

--- net-analyzer/pmacct/files/pmacctd-init.d
+++ net-analyzer/pmacct/files/pmacctd-init.d
@@ -1,9 +1,9 @@
 #!/sbin/runscript
-# Copyright 1999-2010 Gentoo Foundation
+# Copyright 1999-2012 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-analyzer/pmacct/files/pmacctd-init.d,v 1.5 2010/09/30 16:43:05 jer Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-analyzer/pmacct/files/pmacctd-init.d,v 1.6 2012/04/13 15:54:38 jer Exp $
 
-PMACCTDDIR=${PMACCTDDIR:-/etc/pmacct}
+PMACCTDDIR=${PMACCTDDIR:-/etc/pmacctd}
 if [ ${SVCNAME} != "pmacctd" ]; then
 	PMACCTDPID="/var/run/${SVCNAME}.pid"
 else
