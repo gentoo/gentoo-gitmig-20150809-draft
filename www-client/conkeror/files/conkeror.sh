60e96f92409f8e6973d5228c3847ea0db7171bf4
Ulrich Müller <ulm@gentoo.org>
2012-04-19 05:37:13 +0000

    Remove old. Don't look for xulrunner in wrapper script.
    Package-Manager: portage-2.1.10.56/cvs/Linux x86_64

--- www-client/conkeror/files/conkeror.sh
+++ www-client/conkeror/files/conkeror.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # Wrapper script for conkeror
 
-for cmd in xulrunner-2.0 xulrunner-1.9.2 firefox; do
-    xr=$(type -p ${cmd})
-    if [[ -n ${xr} ]]; then
-	[[ ${cmd} = firefox ]] && xr="${xr} -app"
-	exec ${xr} /usr/share/conkeror/application.ini "$@"
-    fi
-done
-
-echo "$0: xulrunner or firefox required, but not found." >&2
+xr=$(type -p firefox)
+[[ -n ${xr} ]] && exec "${xr}" -app /usr/share/conkeror/application.ini "$@"
+echo "$0: firefox required, but not found." >&2
 exit 1
