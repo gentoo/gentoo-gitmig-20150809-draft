8de2fec1e5f8e8d3bd3938999ba0bb5d26c2682d
Ulrich Müller <ulm@gentoo.org>
2012-05-31 21:33:02 +0000

    Remove spurious comment from site-init file for Emacs.
    Package-Manager: portage-2.1.10.63/cvs/Linux x86_64

--- sci-mathematics/cvc3/files/50cvc3-gentoo.el
+++ sci-mathematics/cvc3/files/50cvc3-gentoo.el
@@ -1,5 +1,3 @@
-;;; teyjus site-lisp configuration
-
 (add-to-list 'load-path "@SITELISP@")
 (add-to-list 'auto-mode-alist '("\\.cvc\\'" . cvc-mode))
 (autoload 'cvc-mode "cvc-mode" "CVC specifications editing mode." t)
