920dfe3668693737f1498d294d5be3a5e8efe82a
Pacho Ramos <pacho@gentoo.org>
2012-06-16 11:57:30 +0000

    Remove package.

--- net-misc/batman-vis/files/batman-vis-conf.d
+++
@@ -1,9 +0,0 @@
-#/etc/conf.d/batman-vis: config file for /etc/init.d/batmand
-
-# Configure wich interface or interfaces to for batman to use.
-VIS_BATMAN_INTERFACES="eth0"
-VIS_DOT_INTERFACES="eth0"
-
-# Insert any other batman options
-VIS_OPTIONS=""
-
