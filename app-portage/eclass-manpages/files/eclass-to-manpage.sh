62539eeccad2a795587dae4a5457130efde52fea
Mike Frysinger <vapier@gentoo.org>
2012-07-18 14:23:17 +0000

    Add a -d (debugger) flag to quickly run dgawk.

--- app-portage/eclass-manpages/files/eclass-to-manpage.sh
+++ app-portage/eclass-manpages/files/eclass-to-manpage.sh
@@ -4,10 +4,12 @@
 : ${ECLASSDIR:=${0%/*}/../../../eclass}
 : ${FILESDIR:=${ECLASSDIR}/../app-portage/eclass-manpages/files}
 
+AWK="gawk"
 while [[ $# -gt 0 ]] ; do
 	case $1 in
 	-e) ECLASSDIR=$2; shift;;
 	-f) FILESDIR=$2; shift;;
+	-d) AWK="dgawk";;
 	*) break;;
 	esac
 	shift
@@ -21,8 +23,14 @@ fi
 [[ $# -eq 0 ]] && set -- "${ECLASSDIR}"/*.eclass
 
 for e in "$@" ; do
-	gawk \
+	set -- \
+	${AWK} \
 		-vPORTDIR="${PORTDIR}" \
 		-f "${FILESDIR}"/eclass-to-manpage.awk \
-		${e} > ${e##*/}.5 || rm -f ${e##*/}.5
+		${e}
+	if [[ ${AWK} == "gawk" ]] ; then
+		"$@" > ${e##*/}.5 || rm -f ${e##*/}.5
+	else
+		"$@"
+	fi
 done
