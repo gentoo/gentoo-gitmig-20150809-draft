84f661f97ea02d5b2e0ef1b03702c0b722f85919
Sergey Popov <pinkbyte@gentoo.org>
2012-09-13 07:07:53 +0000

    Change license of init.d scripts to BSD-2 wrt bug #426002
    Package-Manager: portage-2.2.0_alpha127/cvs/Linux x86_64

--- net-analyzer/symon/files/symon-init.d
+++ net-analyzer/symon/files/symon-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
-# Copyright 1999-2007 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-analyzer/symon/files/symon-init.d,v 1.2 2008/11/17 00:00:46 tcunha Exp $
+# Copyright 1999-2012 Gentoo Foundation
+# Distributed under the terms of the 2-clause BSD license
+# $Header: /var/cvsroot/gentoo-x86/net-analyzer/symon/files/symon-init.d,v 1.3 2012/09/13 07:07:53 pinkbyte Exp $
 
 opts="${opts} reload"
 
