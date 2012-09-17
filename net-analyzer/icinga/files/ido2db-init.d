c09fe125bc16f21c7f4c9850711bad5d8b5470af
Matt Thode <prometheanfire@gentoo.org>
2012-09-17 19:53:22 +0000

    part 2 of previous commit
    Package-Manager: portage-2.1.11.9/cvs/Linux x86_64

--- net-analyzer/icinga/files/ido2db-init.d
+++ net-analyzer/icinga/files/ido2db-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2012 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-analyzer/icinga/files/ido2db-init.d,v 1.3 2012/09/17 19:48:49 prometheanfire Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-analyzer/icinga/files/ido2db-init.d,v 1.4 2012/09/17 19:53:22 prometheanfire Exp $
 
 get_config() {
     if [ -e "${IDO2DBCFG}" ]; then
