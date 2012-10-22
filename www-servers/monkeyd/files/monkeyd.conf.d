a7d78d28e1dbaf180c8114d2f8c6b1f0ada6526b
Anthony G. Basile <blueness@gentoo.org>
2012-10-22 23:24:46 +0000

    Remove vulnerable version, bug #439340
    Package-Manager: portage-2.1.11.30/cvs/Linux x86_64
    Manifest-Sign-Key: 0xD0455535

--- www-servers/monkeyd/files/monkeyd.conf.d
+++
@@ -1,6 +0,0 @@
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/www-servers/monkeyd/files/monkeyd.conf.d,v 1.2 2004/09/28 02:02:09 swegener Exp $
-
-# this should be fine
-MONKEY_PID=/var/run/monkey.pid
