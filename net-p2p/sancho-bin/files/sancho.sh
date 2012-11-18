5dc558fd2c384605c355c4898ed25af97487085e
Markos Chandras <hwoarang@gentoo.org>
2012-11-18 11:33:30 +0000

    net-p2p/sancho-bin removal. Bug #421971

--- net-p2p/sancho-bin/files/sancho.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-p2p/sancho-bin/files/sancho.sh,v 1.4 2006/07/04 14:28:43 squinky86 Exp $
-
-cd /opt/sancho
-./sancho-bin ${*}
