d6d9f2aa7cfcaef3394f3179ca30d7e7bff22fa6
Christian Faulhammer <fauli@gentoo.org>
2013-01-04 10:50:45 +0000

    version bump and also associate with .pyx files as lined out in bug 445366 by Michael Orlitzky
    Package-Manager: portage-2.1.11.31/cvs/Linux i686
    Manifest-Sign-Key: 0x2B859DE3

--- app-emacs/python-mode/files/50python-mode-gentoo.el
+++ app-emacs/python-mode/files/50python-mode-gentoo.el
@@ -6,5 +6,6 @@
   "Start an interactive Python interpreter in another window." t)
 
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+(add-to-list 'auto-mode-alist '("\\.pyx$" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (add-to-list 'interpreter-mode-alist '("jython" . jython-mode))
